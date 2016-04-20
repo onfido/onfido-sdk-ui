98ef185d01af5ac5a37096d50638fde5a8a8b32e
Lawrence Gosset <lawrencegosset@gmail.com>
2016-04-20 11:43:02 +0100

    make redux actions pure

--- src/store/reducers/globals.js
+++ src/store/reducers/globals.js
@@ -8,16 +8,16 @@ const initialState = {
 function data(state = initialState, action) {
   switch (action.type) {
     case 'SET_TOKEN':
-      state.jwt = action.payload
-      return {state}
+      state = Object.assign({}, state, {jwt: action.payload})
+      return state
     case 'SET_WEBSOCKET_SUPPORT':
-      state.supportsWebSockets = action.payload
+      state = Object.assign({}, state, {supportsWebSockets: action.payload})
       return state
     case 'SET_DOCUMENT_CAPTURED':
-      state.hasDocumentCaptured = action.payload
+      state = Object.assign({}, state, {hasDocumentCaptured: action.payload})
       return state
     case 'SET_FACE_CAPTURED':
-      state.hasFaceCaptured = action.payload
+      state = Object.assign({}, state, {hasFaceCaptured: action.payload})
       return state
     default:
       return state
