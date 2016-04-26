f47e265c5bffc2871bd1f4cb90c73531de292280
Lawrence Gosset <lawrencegosset@gmail.com>
2016-04-26 11:23:34 +0100

    Use object-assign polyfill for object creation

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -1,9 +1,10 @@
 const {bindActionCreators} = require('redux')
+const objectAssign = require('object-assign')
 const store = require('../store')
 const globals = require('./globals')
 const captures = require('./captures')
 
-const unboundActions = Object.assign({}, globals, captures)
+const unboundActions = objectAssign({}, globals, captures)
 const actions = bindActionCreators(unboundActions, store.dispatch)
 
 module.exports = {
