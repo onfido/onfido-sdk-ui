ce67b3b2ec9a51bd7e0e88529e1424c017c17a55
Lawrence Gosset <lawrencegosset@gmail.com>
2016-05-03 18:19:22 +0100

    Change buttons to links to provide router state

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,11 +1,14 @@
 import { h, Component } from 'preact';
+import { Link } from 'preact-router';
 import classNames from 'classnames';
 
 const methods = [{
+	route: 'document',
 	type: 'documentCapture',
 	hint: 'First, we need to capture a document via your camera or file upload.',
 	captureType: 'Document capture'
 },{
+	route: 'face',
 	type: 'faceCapture',
 	hint: 'Next, we need you to take a photo of your face to match with the document.',
 	captureType: 'Face capture'
@@ -15,18 +18,20 @@ export default class Home {
 	renderMethod = (method, index) => {
 		const classes = `onfido-method onfido-method-${method.type}`;
 		const { complete } = this.props;
+		const route = `verify/${method.route}`;
 		return (
 			<div className={classes}>
 				<h1>{index + 1}</h1>
 				{complete && <span className='onfido-verified'>Complete</span>}
 				<p>{method.hint}</p>
-				<button
+				<Link
+					href={route}
 					className='onfido-method-selector'
-					onClick={this.props.toggleCamera}
+					onClick={this.props.toggleCamera.bind(method.captureType)}
 					disabled={complete}
 				>
 					{method.captureType}
-				</button>
+				</Link>
 			</div>
 		);
 	}
