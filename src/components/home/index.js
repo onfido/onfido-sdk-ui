6ec205d308616591b13a63af4937654fea53d0f2
Lawrence Gosset <lawrencegosset@gmail.com>
2016-05-05 12:16:42 +0100

    Use router links to provide logic from the home screen

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,43 +1,49 @@
 import { h, Component } from 'preact';
 import { Link } from 'preact-router';
-import classNames from 'classnames';
-
-const methods = [{
-	route: 'document',
-	type: 'documentCapture',
-	hint: 'First, we need to capture a document via your camera or file upload.',
-	captureType: 'Document capture'
-},{
-	route: 'face',
-	type: 'faceCapture',
-	hint: 'Next, we need you to take a photo of your face to match with the document.',
-	captureType: 'Face capture'
-}];
 
 export default class Home {
+	renderState (method, route) {
+		if (method.complete) {
+			return (<span class='btn btn-complete'>Complete</span>);
+		} else {
+			return (
+				<Link href={route} className='onfido-method-selector'>
+					{method.title}
+				</Link>
+			);
+		}
+	}
+
 	renderMethod = (method, index) => {
 		const classes = `onfido-method onfido-method-${method.type}`;
-		const { complete } = this.props;
 		const route = `verify/${method.route}`;
 		return (
 			<div className={classes}>
 				<h1>{index + 1}</h1>
-				{complete && <span className='onfido-verified'>Complete</span>}
 				<p>{method.hint}</p>
-				<Link
-					href={route}
-					className='onfido-method-selector'
-					onClick={this.props.toggleCamera.bind(method.captureType)}
-					disabled={complete}
-				>
-					{method.captureType}
-				</Link>
+				{this.renderState(method, route)}
 			</div>
 		);
 	}
+
 	render () {
+		const { hasDocumentCaptured, hasFaceCaptured } = this.props;
+		const methods = [{
+			route: 'document',
+			type: 'documentCapture',
+			hint: 'First, we need to capture a document via your camera or file upload.',
+			title: 'Document capture',
+			complete: hasDocumentCaptured
+		},{
+			route: 'face',
+			type: 'faceCapture',
+			hint: 'Next, we need you to take a photo of your face to match with the document.',
+			title: 'Face capture',
+			complete: hasFaceCaptured
+		}];
 		return (
 			<div className='onfido-methods'>
+				<a rel='modal:close' className='onfido-close'>×&nbsp;Close</a>
 				{methods.map(this.renderMethod)}
 			</div>
 		);
