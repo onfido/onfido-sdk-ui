d77613a34002d21c8d7c8cdca9312223c487e385
Lawrence Gosset <lawrencegosset@gmail.com>
2016-05-05 12:16:02 +0100

    Add logic to handle file uploads on non-getusermedia browsers

--- src/components/camera/index.js
+++ src/components/camera/index.js
@@ -1,101 +1,193 @@
 import { h, Component } from 'preact';
 import { Link, route } from 'preact-router';
 import getUserMedia from 'getusermedia';
-import { connect, events } from '../../../../javascript-sdk-core/src'
+import Dropzone from 'react-dropzone';
+import CountUp from 'countup.js';
+import { connect, events } from '../../../../javascript-sdk-core/src';
+import { filterImage } from '../utils';
+
+const Countdown = () => (<span className='onfido-countdown'></span>)
 
 export default class Camera extends Component {
 
 	componentWillReceiveProps (nextProps) {
-		const { hasDocumentCaptured } = this.props;
-		if (hasDocumentCaptured) {
-			console.log('documentCapture');
+		const { method } = this.props;
+		const { hasDocumentCaptured, hasFaceCaptured } = nextProps;
+		const validDocumentCapture = (hasDocumentCaptured && method === 'document');
+		const validFaceCapture = (hasFaceCaptured && method === 'face');
+		if (validDocumentCapture || validFaceCapture) {
 			route('/', true);
+		};
+	}
+
+	captureImage = () => {
+		this.ctx = this.canvas.getContext('2d');
+		this.canvas.width = this.video.clientWidth;
+		this.canvas.height = this.video.clientHeight;
+		this.ctx.drawImage(this.video, 0, 0);
+		const image = this.canvas.toDataURL('image/webp');
+		this.handleImage(image);
+	}
+
+	handleFiles (files) {
+		const { method } = this.props;
+		const { faceCapture, documentCapture } = this.props.actions;
+		switch (method) {
+			case 'document':
+				return files.map((file) => documentCapture(file));
+			case 'face':
+				return files.map((file) => faceCapture(file));
+			default:
+				return false;
 		}
 	}
 
-	getBase64 = () => {
-		this.ctx = this.canvas.getContext('2d')
-		this.canvas.width = this.video.clientWidth
-		this.canvas.height = this.video.clientHeight
-		this.ctx.drawImage(this.video, 0, 0)
-		const image = this.canvas.toDataURL('image/webp')
-		const base64 = image.split(',')[1]
+	handleImage (image) {
+		const { method, actions, socket } = this.props;
 		const payload = {
 			id: 1,
-			messageType: 'face',
-			image
+			messageType: method,
+			image: filterImage(image)
 		}
-		// console.log(payload)
-		this.socket.sendMessage(base64)
+		console.log(payload);
+		switch (method) {
+			case 'document':
+				return (() => {
+					socket.sendMessage(payload.image);
+					actions.documentCapture(payload);
+				})();
+			case 'face':
+				return (() => {
+					actions.faceCapture(payload);
+					setTimeout(() => actions.setFaceCaptured(true), 1000);
+				})();
+			default:
+				return false;
+		}
 	}
 
 	capture = {
-		start: () => this.interval = setInterval(() => this.getBase64(), 1000),
-		stop: () => clearInterval(this.interval)
+		once: () => {
+			const options = {
+			  useEasing : false,
+			  useGrouping : false
+			};
+			const countdown = new CountUp(this.countdown, 3, 0, 0, 3, options);
+			countdown.start(() => {
+				this.video.pause();
+				this.captureImage();
+			});
+		},
+		start: () => {
+			this.interval = setInterval(() => this.captureImage(), 1000);
+		},
+		stop: () => {
+			clearInterval(this.interval);
+		}
 	}
 
-	state = {
-		something: false
-	}
-
-	componentWillMount () {
-		this.socket = connect('something');
-	}
-
-	// gets called when this route is navigated to
-	componentDidMount() {
-		console.log(this.props);
-		const { toggleCamera, hasDocumentCaptured, method } = this.props;
-		getUserMedia((err, stream) => {
-			if (err) {
-				console.log('failed');
-			} else {
-				const video = this.video
-				const canvas = document.createElement('canvas');
-				video.src = window.URL.createObjectURL(stream);
-				video.onloadedmetadata = (e) => video.play();
-				this.video = video;
-				this.canvas = canvas;
-			}
-		});
+	createStream (stream) {
+		const { hasDocumentCaptured, method } = this.props;
+		const video = this.video;
+		const canvas = document.createElement('canvas');
+		video.src = window.URL.createObjectURL(stream);
+		video.onloadedmetadata = (e) => video.play();
+		this.video = video;
+		this.canvas = canvas;
 		if (method === 'document' && !hasDocumentCaptured) {
 			this.capture.start();
 		};
-		events.once('documentCapture', () => {
+	}
 
+	componentDidMount () {
+		getUserMedia((err, stream) => {
+			if (err) {
+				// TODO handle the manual photo method when getUserMedia unsupported
+				console.log('failed');
+			} else {
+				this.createStream(stream);
+				this.bindEvents();
+			}
 		});
 	}
 
-	// gets called just before navigating away from the route
-	componentWillUnmount() {
-		console.log('componentWillUnmount')
-		this.capture.stop()
+	bindEvents () {
+		events.on('modalOpen', () => this.capture.start());
+		events.on('modalClose', () => this.capture.stop());
 	}
 
-	// Note: `user` comes from the URL, courtesy of our router
-	render() {
+	componentWillUnmount() {
+		console.log('componentWillUnmount');
+		this.capture.stop();
+	}
+
+	renderCaptureButton () {
 		return (
-			<div id='onfido-camera' className='onfido-camera'>
+			<button id='onfido-capture' className='btn' onClick={this.capture.once}>
+				Take photo
+			</button>
+		);
+	}
+
+	renderPreviews () {
+		const { documentCaptures, faceCaptures, method } = this.props;
+		switch (method) {
+			case 'document':
+				return documentCaptures.map((file) => <img src={file.preview} />)
+			case 'face':
+				return faceCaptures.map((file) => <img src={file.preview} />)
+			default:
+				return false;
+		}
+	}
+
+	renderUploader () {
+		return (
+			<div className='onfido-upload'>
+				{this.renderPreviews()}
+				<Dropzone onDrop={::this.handleFiles}>
+					<div>Try dropping some files here, or click to select files to upload.</div>
+				</Dropzone>
+			</div>
+		)
+	}
+
+	renderVideo () {
+		return (
+			<video
+				id='onfido-video'
+				className='onfido-video'
+				autoplay='autoplay'
+				controls='true'
+				muted
+				controls='false'
+				ref={(video) => { this.video = video }}
+			/>
+		);
+	}
+
+	renderNav () {
+		return (
+			<div>
 				<Link href='/' id='onfido-back' className='onfido-back'>
 					<span className='sans-serif'>&larr;</span>&nbsp;Back
 				</Link>
-				<button
-					id='onfido-capture'
-					className='onfido-capture btn'
-					onClick={this.capture.start}
-				>
-					Take photo
-				</button>
-				<video
-					id='onfido-video'
-					className='onfido-video'
-					autoplay='autoplay'
-					controls='true'
-					muted
-					controls='false'
-					ref={(video) => { this.video = video }}
-				/>
+				<a rel='modal:close' className='onfido-close white'>×&nbsp;Close</a>
 			</div>
-		);
+		)
 	}
+
+	render() {
+		const { method, supportsGetUserMedia } = this.props;
+		const showFaceCapture = (!supportsGetUserMedia && method === 'face');
+		return (
+			<div id='onfido-camera' className='onfido-camera'>
+				{this.renderNav()}
+				{showFaceCapture && this.renderCaptureButton()}
+				{showFaceCapture && <Countdown ref={(c) => { this.countdown = c }}/>}
+				{!supportsGetUserMedia && this.renderVideo() || this.renderUploader()}
+			</div>
+		)
+	}
+
 }
