b2afae4334b342734b908182817fd47aa0c0ca3c
Lawrence Gosset <lawrencegosset@gmail.com>
2016-05-16 20:47:08 +0100

    Vanilla implementation of modal

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,26 +1,49 @@
-import { VanillaModal } from 'vanilla-modal';
-import { events } from '../../../../javascript-sdk-core/src';
+import { VanillaModal } from 'vanilla-modal'
+import { events } from '../../../../onfido-sdk-core'
 
-const options = {
-	modal: '.onfido-modal',
+const Modal = {}
+
+Modal.template = `
+  <div class='onfido-modal-inner'>
+    <div class='onfido-modal-content'></div>
+  </div>
+`
+
+Modal.create = (options) => {
+  const modal = document.createElement('div')
+  modal.className = 'onfido-modal'
+  modal.innerHTML = Modal.template
+  options.mount.parentNode.insertBefore(modal, options.mount.nextSibling)
+  events.emit('modalMounted', options)
+}
+
+Modal.options = {
+  modal: '.onfido-modal',
   modalInner: '.onfido-modal-inner',
   modalContent: '.onfido-modal-content',
-	onBeforeClose: () => {
-		events.emit('modalClose')
-	},
-	onBeforeOpen: () => {
-		events.emit('modalOpen')
-	}
-};
+  onOpen: () => {
+    events.emit('onOpen')
+  },
+  onClose: () => {
+    events.emit('onClose')
+  },
+  onBeforeOpen: () => {
+    events.emit('onBeforeOpen')
+  },
+  onBeforeClose: () => {
+    events.emit('onBeforeClose')
+  }
+}
 
-const modal = new VanillaModal(options);
+events.on('modalMounted', (options) => {
+  const modal = new VanillaModal(Modal.options)
+  const button = document.getElementById(options.buttonId)
+  button.disabled = false
+  const id = `#${options.mount.getAttribute('id')}`
+  button.addEventListener('click', () => modal.open(id))
+  events.on('onOpen', () => options.mount.style.display = 'block')
+  events.on('onClose', () => options.mount.style.display = 'none')
+  events.on('closeModal', () => modal.close())
+})
 
-events.once('ready', () => {
-
-	const button = document.getElementById('onfido-button');
-	button.disabled = false;
-	button.addEventListener('click', () => {
-		modal.open('#onfido-mount')
-	});
-
-});
+export default Modal
