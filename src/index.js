c763a35bd756799342d4a1be342fdd91f95f8229
Lawrence Gosset <lawrencegosset@gmail.com>
2016-05-16 20:48:13 +0100

    Create proper init script with options and defaults for UI

--- src/index.js
+++ src/index.js
@@ -1,37 +1,33 @@
-// import 'lie';
-// import 'isomorphic-fetch';
-import { h, render } from 'preact';
-import { Provider } from 'react-redux';
-import { store } from '../../javascript-sdk-core/src'
+import { h, render } from 'preact'
+import { Provider } from 'react-redux'
+import { store, events } from '../../onfido-sdk-core'
+import Modal from './components/modal'
+import App from './components/app'
+import { route } from 'preact-router'
+import objectAssign from 'object-assign'
 
-let root;
-function init() {
-	let App = require('./components/app').default;
-	root = render(
-		<Provider store={store}>
-			<App />
-		</Provider>,
-		document.getElementById('onfido-mount'), root);
+const Onfido = {}
+
+const defaults = {
+  token: 'some token',
+  buttonId: 'onfido-button',
+  containerId: 'onfido-mount',
+  onReady: null,
+  onDocumentCapture: null,
+  onFaceCapture: null,
+  onComplete: null
 }
 
-init();
-
-if (module.hot) {
-	module.hot.accept('./components/app', () => requestAnimationFrame( () => {
-		flushLogs();
-		init();
-	}) );
-
-	// optional: mute HMR/WDS logs
-	let log = console.log,
-		logs = [];
-	console.log = (t, ...args) => {
-		if (typeof t==='string' && t.match(/^\[(HMR|WDS)\]/)) {
-			if (t.match(/(up to date|err)/i)) logs.push(t.replace(/^.*?\]\s*/m,''), ...args);
-		}
-		else {
-			log.call(console, t, ...args);
-		}
-	};
-	let flushLogs = () => console.log(`%c🚀 ${logs.splice(0,logs.length).join(' ')}`, 'color:#888;');
+Onfido.init = (opts) => {
+  route('/', true)
+  const options = objectAssign({}, defaults, opts)
+  options.mount = document.getElementById(options.containerId)
+  Modal.create(options)
+  render((
+    <Provider store={store}>
+      <App options={options}/>
+    </Provider>
+  ), options.mount)
 }
+
+export default Onfido
