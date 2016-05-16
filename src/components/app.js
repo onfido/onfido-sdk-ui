7b4b8a7f9e8dbcdc07a953e4b5734dd18394b30d
Lawrence Gosset <lawrencegosset@gmail.com>
2016-05-16 20:44:49 +0100

    Use core package from npm

--- src/components/app.js
+++ src/components/app.js
@@ -1,59 +1,61 @@
-import { h, Component } from 'preact';
-import { Router } from 'preact-router';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import './modal';
-
-import Home from './home';
-import Camera from './camera';
-
+import { h, Component } from 'preact'
+import { Router } from 'preact-router'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 import {
-	unboundActions,
-	store,
-	events,
-	connect as onfido
-} from '../../../javascript-sdk-core/src';
+  unboundActions,
+  store,
+  events,
+  connect as ws
+} from 'onfido-sdk-core'
+
+import Home from './home'
+import Camera from './camera'
+
+import styles from '../style/style.css'
 
 class App extends Component {
-	handleRoute = e => {
-		this.currentUrl = e.url;
-	};
+  handleRoute = e => {
+    this.currentUrl = e.url
+  }
 
-	componentWillMount () {
-		this.socket = onfido('jwt');
-	}
+  componentWillMount () {
+    const { options } = this.props
+    const { token } = options
+    this.socket = ws(token)
+  }
 
-	render() {
-		return (
-			<div id="app" className='onfido-verify'>
-				<Router onChange={this.handleRoute}>
-					<Home
-						path='/'
-						{...this.state}
-						{...this.props}
-					/>
-					<Camera
-						path='/verify/:method'
-						socket={this.socket}
-						{...this.state}
-						{...this.props}
-					/>
-				</Router>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div id="app" className='onfido-verify'>
+        <Router onChange={this.handleRoute}>
+          <Home
+            path='/'
+            {...this.state}
+            {...this.props}
+          />
+          <Camera
+            path='/verify/:method'
+            socket={this.socket}
+            {...this.state}
+            {...this.props}
+          />
+        </Router>
+      </div>
+    )
+  }
 }
 
 function mapStateToProps(state) {
   return {
-		documentCaptures: state.documentCaptures,
-		faceCaptures: state.faceCaptures,
-		...state.globals
-	};
+    documentCaptures: state.documentCaptures,
+    faceCaptures: state.faceCaptures,
+    ...state.globals
+  }
 }
 
 function mapDispatchToProps(dispatch) {
-  return { actions: bindActionCreators(unboundActions, dispatch) };
+  return { actions: bindActionCreators(unboundActions, dispatch) }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App)
