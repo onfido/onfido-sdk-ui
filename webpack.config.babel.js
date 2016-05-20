878d91cc3153fa3dc92da6cf8274504d69c810dd
Lawrence Gosset <lawrencegosset@gmail.com>
2016-05-20 13:33:58 +0100

    Update postCSS plugins in webpack build

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,125 +2,142 @@ import webpack from 'webpack';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import autoprefixer from 'autoprefixer';
+import customMedia from 'postcss-custom-media';
+import url from 'postcss-url';
 
 const ENV = process.env.NODE_ENV || 'development';
 
 const CSS_MAPS = ENV!=='production';
 
-module.exports = {
-	context: `${__dirname}/src`,
-	entry: './index.js',
+const config = {
+  context: `${__dirname}/src`,
+  entry: './index.js',
 
-	output: {
-		path: `${__dirname}/build`,
-		publicPath: '/',
-		filename: 'bundle.js'
-	},
+  output: {
+    library: 'Onfido',
+    libraryTarget: 'umd',
+    path: `${__dirname}/dist`,
+    publicPath: '/',
+    filename: 'onfido.min.js'
+  },
 
-	resolve: {
-		extensions: ['', '.jsx', '.js', '.json', '.less'],
-		modulesDirectories: [
-			`${__dirname}/src/lib`,
-			`${__dirname}/node_modules`,
-			'node_modules'
-		],
-		alias: {
-			components: `${__dirname}/src/components`,		// used for tests
-			style: `${__dirname}/src/style`,
-			'react': 'preact-compat',
-			'react-dom': 'preact-compat'
-		}
-	},
+  resolve: {
+    extensions: ['', '.jsx', '.js', '.json', '.less'],
+    modulesDirectories: [
+      `${__dirname}/src/lib`,
+      `${__dirname}/node_modules`,
+      `${__dirname}/src`,
+      'node_modules'
+    ],
+    alias: {
+      components: `${__dirname}/src/components`,    // used for tests
+      style: `${__dirname}/src/style`,
+      'react': 'preact-compat',
+      'react-dom': 'preact-compat'
+    }
+  },
 
-	module: {
-		loaders: [
-			{
-				test: /\.jsx?$/,
-				exclude: /node_modules/,
-				loader: 'babel'
-			},
-			{
-				test: /\.(less|css)$/,
-				include: /src\/components\//,
-				loader: ExtractTextPlugin.extract('style?singleton', [
-					`css?sourceMap=${CSS_MAPS}&modules&importLoaders=1&localIdentName=[local]${process.env.CSS_MODULES_IDENT || '_[hash:base64:5]'}`,
-					'postcss',
-					`less?sourceMap=${CSS_MAPS}`
-				].join('!'))
-			},
-			{
-				test: /\.(less|css)$/,
-				exclude: /src\/components\//,
-				loader: ExtractTextPlugin.extract('style?singleton', [
-					`css?sourceMap=${CSS_MAPS}`,
-					`postcss`,
-					`less?sourceMap=${CSS_MAPS}`
-				].join('!'))
-			},
-			{
-				test: /\.json$/,
-				loader: 'json'
-			},
-			{
-				test: /\.(xml|html|txt|md)$/,
-				loader: 'raw'
-			},
-			{
-				test: /\.(svg|woff2?|ttf|eot|jpe?g|png|gif)(\?.*)?$/i,
-				loader: ENV==='production' ? 'file?name=[path][name]_[hash:base64:5].[ext]' : 'url'
-			}
-		]
-	},
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+      {
+        test: /\.(less|css)$/,
+        include: /src\/components\//,
+        loader: ExtractTextPlugin.extract('style?singleton', [
+          `css?sourceMap=${CSS_MAPS}`,
+          'postcss',
+          `less?sourceMap=${CSS_MAPS}`
+        ].join('!'))
+      },
+      {
+        test: /\.(less|css)$/,
+        exclude: /src\/components\//,
+        loader: ExtractTextPlugin.extract('style?singleton', [
+          `css?sourceMap=${CSS_MAPS}`,
+          `postcss`,
+          `less?sourceMap=${CSS_MAPS}`
+        ].join('!'))
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      },
+      {
+        test: /\.(xml|html|txt|md)$/,
+        loader: 'raw'
+      },
+      {
+        test: /\.(svg|woff2?|ttf|eot|jpe?g|png|gif)(\?.*)?$/i,
+        loader: ENV==='production' ? 'file?name=[path][name]_[hash:base64:5].[ext]' : 'url'
+      }
+    ]
+  },
 
-	postcss: () => [
-		autoprefixer({ browsers: 'last 2 versions' })
-	],
+  postcss: () => [
+    customMedia(),
+    autoprefixer({ browsers: 'last 2 versions' }),
+    url({
+      url: "inline"
+    })
+  ],
 
-	plugins: ([
-		new webpack.NoErrorsPlugin(),
-		new ExtractTextPlugin('style.css', {
-			allChunks: true,
-			disable: ENV!=='production'
-		}),
-		new webpack.optimize.DedupePlugin(),
-		new webpack.DefinePlugin({
-			'process.env': JSON.stringify({ NODE_ENV: ENV })
-		}),
-		new HtmlWebpackPlugin({
-			template: './index.html',
-			minify: { collapseWhitespace: true }
-		})
-	]).concat(ENV==='production' ? [
-		new webpack.optimize.OccurenceOrderPlugin()
-	] : []),
+  plugins: ([
+    new webpack.NoErrorsPlugin(),
+    new ExtractTextPlugin('style.css', {
+      allChunks: true,
+      disable: ENV!=='production'
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(ENV)
+    }),
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      minify: { collapseWhitespace: true }
+    })
+  ]).concat(ENV==='production' ? [
+    new webpack.optimize.OccurenceOrderPlugin()
+  ] : []),
 
-	stats: { colors: true },
+  stats: { colors: true },
 
-	node: {
-		global: true,
-		process: false,
-		Buffer: false,
-		__filename: false,
-		__dirname: false,
-		setImmediate: false
-	},
+  node: {
+    global: true,
+    process: false,
+    Buffer: false,
+    __filename: false,
+    __dirname: false,
+    setImmediate: false
+  },
 
-	devtool: ENV==='production' ? 'source-map' : 'cheap-module-eval-source-map',
+  devtool: ENV==='production' ? 'source-map' : 'cheap-module-eval-source-map',
 
-	devServer: {
-		port: process.env.PORT || 8080,
-		host: '0.0.0.0',
-		colors: true,
-		publicPath: '/',
-		contentBase: './src',
-		historyApiFallback: true,
-		proxy: [
-			// OPTIONAL: proxy configuration:
-			// {
-			// 	path: '/optional-prefix/**',
-			// 	target: 'http://target-host.com',
-			// 	rewrite: req => { req.url = req.url.replace(/^\/[^\/]+\//, ''); }   // strip first path segment
-			// }
-		]
-	}
+  devServer: {
+    port: process.env.PORT || 8080,
+    host: '0.0.0.0',
+    colors: true,
+    publicPath: '/',
+    contentBase: './src',
+    historyApiFallback: true
+  }
 };
+
+if (ENV === 'production') {
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        screw_ie8: true,
+        warnings: false
+      }
+    })
+  )
+}
+
+export default config
