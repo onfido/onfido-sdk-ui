3205bdc5b265a0da22362d374a58d700b7e75bdd
Lawrence Gosset <lawrencegosset@gmail.com>
2016-05-22 18:00:30 +0100

    Extract base64 image creator into separate utility function

--- src/components/utils/createBase64.js
+++ src/components/utils/createBase64.js
@@ -1,9 +1,12 @@
-const createBase64 = (canvas, video, width, height) => {
+export const createBase64 = (canvas, video, dimensions, callback) => {
+  if (!video || !dimensions) return
+  const { ratio } = dimensions
   const ctx = canvas.getContext('2d')
+  const width = 960
+  const height = (960 / ratio)
   canvas.width = width
   canvas.height = height
   ctx.drawImage(video, 0, 0, width, height)
-  return canvas.toDataURL('image/webp')
+  const image = canvas.toDataURL('image/webp')
+  callback(image)
 }
-
-export default createBase64
