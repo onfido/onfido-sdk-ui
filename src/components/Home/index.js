4db68c0fa42b4db4921aca4fec057efbdb030b53
Lawrence Gosset <lawrencegosset@gmail.com>
2016-05-22 22:59:29 +0100

    Add in conditional click handlers for transitioning view state

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,37 +1,52 @@
 import { h, Component } from 'preact'
 import classNames from 'classnames'
 import { events } from 'onfido-sdk-core'
+import NativeListener from 'react-native-listener'
 
 import HomeComplete from '../HomeComplete'
+import DocumentSelector from '../DocumentSelector'
 
 export default class Home {
 
+  handleClick = (method) => {
+    const { changeView, documentType } = this.props
+    const methods = {
+      'document': () => {
+        if (documentType) {
+          changeView(true, method)
+        }
+      },
+      'face': () => changeView(true, method),
+      'home': () => null
+    }
+    return (methods[method] || methods['home'])()
+  }
+
   renderMethod (method, index) {
-    const { transition } = this.props
+    const { type, route, complete, renderDropdown, hint } = method
+    const { setDocumentType } = this.props.actions
     const classes = classNames({
       'onfido-method': true,
       'onfido-method--double': true,
-      [`onfido-method-${method.type}`]: true
+      [`onfido-method-${type}`]: true
     })
     const iconClass = classNames({
       'onfido-icon': true,
-      'onfido-icon--complete': method.complete,
-      [`onfido-icon--${method.type}`]: !method.complete
+      'onfido-icon--complete': complete,
+      [`onfido-icon--${type}`]: !complete
     })
-    const route = `verify/${method.route}`
     return (
       <div className={classes}>
-        <a onClick={transition.bind(this, true, method.route)} className='onfido-method-selector'>
+        <a
+          onClick={this.handleClick.bind(this, route)}
+          className='onfido-method-selector'
+        >
           <span className={iconClass}></span>
-          <p className='onfido-instructions'>{method.hint}</p>
-          {/*this.renderState(method, route)*/}
+          {renderDropdown && <DocumentSelector setDocumentType={setDocumentType} />}
+          <p className='onfido-instructions'>{hint}</p>
         </a>
       </div>
     )
-  }
-
-  closeModal () {
-    events.emit('closeModal')
   }
 
   renderMethods (methods) {
@@ -53,13 +68,15 @@ export default class Home {
       type: 'documentCapture',
       hint: 'Take a capture of your passport or national identity card, which will be used to verify your identity.',
       title: 'Document capture',
-      complete: hasDocumentCaptured
+      complete: hasDocumentCaptured,
+      renderDropdown: true
     },{
       route: 'face',
       type: 'faceCapture',
       hint: 'Take a photo of your face, which will be automatically matched with the photo from your document.',
       title: 'A photo of you',
-      complete: hasFaceCaptured
+      complete: hasFaceCaptured,
+      renderDropdown: false
     }]
     return (
       <div className='onfido-wrapper'>
@@ -67,7 +84,7 @@ export default class Home {
           <span></span>
           <a rel='modal:close' className='onfido-btn-nav onfido-btn-nav--right'>× Close</a>
         </div>
-        {complete && <HomeComplete handleClick={this.closeModal} /> || this.renderMethods(methods)}
+        {complete && <HomeComplete handleClick={() => events.emit('closeModal')} /> || this.renderMethods(methods)}
       </div>
     )
   }
