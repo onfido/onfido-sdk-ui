494cb2e0f74e345df63e30e208ed81ab4ec492ee
Lawrence Gosset <lawrencegosset@gmail.com>
2016-05-23 14:35:53 +0100

    Add ready callback into modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -21,29 +21,21 @@ Modal.options = {
   modal: '.onfido-modal',
   modalInner: '.onfido-modal-inner',
   modalContent: '.onfido-modal-content',
-  onOpen: () => {
-    events.emit('onOpen')
-  },
-  onClose: () => {
-    events.emit('onClose')
-  },
-  onBeforeOpen: () => {
-    events.emit('onBeforeOpen')
-  },
-  onBeforeClose: () => {
-    events.emit('onBeforeClose')
-  }
+  onOpen: () => events.emit('onOpen'),
+  onClose: () => events.emit('onClose'),
+  onBeforeOpen: () => events.emit('onBeforeOpen'),
+  onBeforeClose: () => events.emit('onBeforeClose')
 }
 
 events.on('modalMounted', (options) => {
   const modal = new VanillaModal(Modal.options)
   const button = document.getElementById(options.buttonId)
-  button.disabled = false
   const id = `#${options.mount.getAttribute('id')}`
   button.addEventListener('click', () => modal.open(id))
   events.on('onOpen', () => options.mount.style.display = 'block')
   events.on('onClose', () => options.mount.style.display = 'none')
   events.on('closeModal', () => modal.close())
+  events.once('ready', () => button.disabled = false)
 })
 
 export default Modal
