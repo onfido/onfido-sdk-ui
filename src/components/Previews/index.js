9269ece5c1ecfe7f79d9b69e57186adc4ef5aada
Lawrence Gosset <lawrencegosset@gmail.com>
2016-05-23 16:21:07 +0100

    Render out one preview only

--- src/components/Previews/index.js
+++ src/components/Previews/index.js
@@ -1,14 +1,49 @@
 import { h, Component } from 'preact'
 
-const Previews = ({ documentCaptures, faceCaptures, method }) =>  {
-  switch (method) {
-    case 'document':
-      return documentCaptures.map((file) => <img src={file.image} />)
-    case 'face':
-      return faceCaptures.map((file) => <img src={file.image} />)
-    default:
-      return false
+const Retake = ({ action }) => {
+  return (
+    <p className='onfido-retake'>
+      <button
+        onClick={() => action(false)}
+        className='onfido-btn onfido-btn-outline'
+      >
+        Retake?
+      </button>
+    </p>
+  )
+}
+
+const Captures = ({ captures }) => {
+  const [ capture ] = captures
+  return (
+    <div class='onfido-captures'>
+      <img src={capture.image} className='onfido-image' />
+    </div>
+  )
+}
+
+const Previews = (props) =>  {
+  const { documentCaptures, faceCaptures, method, actions } = props
+  const methods = {
+    'document': () => {
+      return (
+        <div className='onfido-previews'>
+          <Captures captures={documentCaptures} />
+          <Retake action={actions.setDocumentCaptured} />
+        </div>
+      )
+    },
+    'face': () => {
+      return (
+        <div className='onfido-previews'>
+          <Captures captures={faceCaptures} />
+          <Retake action={actions.setFaceCaptured} />
+        </div>
+      )
+    },
+    'home': () => null
   }
+  return (methods[method] || methods['home'])()
 }
 
 export default Previews
