cc00f817bc40ae543c4bbbdad31fcbe7aaca308c
Lawrence Gosset <lawrencegosset@gmail.com>
2016-05-25 14:57:25 +0100

    Add reducer for valid captures

--- src/store/reducers/captures.js
+++ src/store/reducers/captures.js
@@ -1,10 +1,17 @@
 import * as constants from '../../constants'
+import objectAssign from 'object-assign'
 
 export function documentCaptures(state = [], action) {
   switch (action.type) {
     case constants.DOCUMENT_CAPTURE:
-      const arr = state.slice(0, 2)
+      const arr = state.slice(0, 4)
       return [ action.payload, ...arr ]
+    case constants.CAPTURE_IS_VALID:
+      return state.map(capture => {
+        return capture.id === action.payload
+          ? objectAssign({}, capture, { isValid: true })
+          : capture
+      })
     default:
       return state
   }
@@ -13,7 +20,7 @@ export function documentCaptures(state = [], action) {
 export function faceCaptures(state = [], action) {
   switch (action.type) {
     case constants.FACE_CAPTURE:
-      const arr = state.slice(0, 2)
+      const arr = state.slice(0, 4)
       return [ action.payload, ...arr ]
     default:
       return state
