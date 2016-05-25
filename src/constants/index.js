cd32c55741feb1bdbf5695b11ed2b0e4bdb11cf1
Lawrence Gosset <lawrencegosset@gmail.com>
2016-05-25 14:56:14 +0100

    Add constants for capture valid

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,5 @@
 export const SOCKET_URL = 'wss://ws.onfido.com:9876'
+export const SOCKET_URL_DEV = 'wss://document-check-staging.onfido.co.uk:9876'
 export const XHR_URL = 'https://api.onfido.com'
 
 export const DOCUMENT_CAPTURE = 'DOCUMENT_CAPTURE'
@@ -11,3 +12,5 @@ export const SET_GUM_SUPPORT = 'SET_GUM_SUPPORT'
 export const SET_DOCUMENT_CAPTURED = 'SET_DOCUMENT_CAPTURED'
 export const SET_FACE_CAPTURED = 'SET_FACE_CAPTURED'
 export const SET_DOCUMENT_TYPE = 'SET_DOCUMENT_TYPE'
+
+export const CAPTURE_IS_VALID = 'CAPTURE_IS_VALID'
