ebce42740d5f11912401987d2e266a1cf564818e
Lawrence Gosset <lawrencegosset@gmail.com>
2016-06-02 00:29:56 +0100

    Create welcome view component

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,21 +1,19 @@
 import { h, Component } from 'preact'
+import { Link } from 'preact-router'
 
-const Welcome = (props) => {
-  const { changeView } = props
-  return (
-    <div className='onfido-wrapper'>
-      <div class='onfido-center'>
-        <h1>Welcome</h1>
-        <p>To open a bank account we will need to verify your identity. Please have your Passport, Identity Card or Drivers License ready.</p>
-        <button
-          onClick={changeView}
-          className='onfido-btn onfido-btn-primary'
-        >
-          Next
-        </button>
-      </div>
+const Welcome = (props) => (
+  <div className='onfido-wrapper'>
+    <div class='onfido-center'>
+      <h1>Welcome</h1>
+      <p>To open a bank account we will need to verify your identity. Please have your Passport, Identity Card or Drivers License ready.</p>
+      <Link
+        href="/step/1"
+        className='onfido-btn onfido-btn-primary'
+      >
+        Step 1
+      </Link>
     </div>
-  )
-}
+  </div>
+)
 
 export default Welcome
