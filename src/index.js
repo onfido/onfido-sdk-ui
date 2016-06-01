13889e3a5ef836b1718d92a7ba43383754a6133c
Lawrence Gosset <lawrencegosset@gmail.com>
2016-06-02 00:30:59 +0100

    Refactor container to use routes single child in render method

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import { h, render } from 'preact'
+import { h, render, Component } from 'preact'
 import { Provider } from 'react-redux'
-import { store, events } from 'onfido-sdk-core'
+import { store, events, connect as ws } from 'onfido-sdk-core'
 import Modal from './components/Modal'
 import App from './components/app'
+import { Router, route } from 'preact-router'
 
 import objectAssign from 'object-assign'
 
@@ -18,17 +19,37 @@ const defaults = {
   onComplete: null
 }
 
-const Everything = ({ options }) => (
-  <Provider store={store}>
-    <App options={options}/>
-  </Provider>
-)
+class Container extends Component {
+
+  componentWillMount () {
+    const { token } = this.props.options
+    this.socket = ws(token)
+  }
+
+  handleRoute = (e) => {
+    this.currentUrl = e.url
+  }
+
+  render () {
+    const { options } = this.props
+    return (
+      <Provider store={store}>
+        <Router onChange={this.handleRoute}>
+          <App options={options} socket={this.socket} path='/' />
+          <App options={options} socket={this.socket} path='/step/:step' />
+        </Router>
+      </Provider>
+    )
+  }
+
+}
 
 Onfido.init = (opts) => {
+  route('/', true)
   const options = objectAssign({}, defaults, opts)
   options.mount = document.getElementById(options.containerId)
   Modal.create(options)
-  render((<Everything options={options}/>), options.mount)
+  render(<Container options={options}/>, options.mount)
 }
 
 Onfido.getCaptures = () => events.getCaptures()
