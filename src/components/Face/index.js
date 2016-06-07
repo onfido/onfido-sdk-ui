e02b56544dd6db14b3bdc1cdff47044bbc4365f1
Lawrence Gosset <lawrencegosset@gmail.com>
2016-06-07 18:18:34 +0100

    Update face capture instructions

--- src/components/Face/index.js
+++ src/components/Face/index.js
@@ -2,10 +2,15 @@ import { h, Component } from 'preact'
 
 export const FaceInstructions = ({ handeClick }) => {
   return (
-    <div className='onfido-capture-ui'>
-      <button id='onfido-capture' className='onfido-btn onfido-btn-primary onfido-btn-capture' onClick={handeClick}>
-        Take photo
-      </button>
+    <div className='onfido-face-overlay'>
+      <span className='onfido-face-circle' />
+      <div className='onfido-capture-ui'>
+        <p>We need to take a capture of your face to match with your document</p>
+        <button id='onfido-capture' className='onfido-btn onfido-btn-primary onfido-btn-capture' onClick={handeClick}>
+          Take photo
+        </button>
+      </div>
     </div>
+
   )
 }
