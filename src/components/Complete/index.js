cbf9ff15daf2cc8e5c71967181624c3b9a367864
Lawrence Gosset <lawrencegosset@gmail.com>
2016-06-07 15:23:06 +0100

    Add completed view as component

--- src/components/Complete/index.js
+++ src/components/Complete/index.js
@@ -1,18 +1,18 @@
 import { h, Component } from 'preact'
+import ActionBar from '../ActionBar'
 
-const HomeComplete = () => {
-  return (
-    <div className='onfido-complete'>
-      <div className='onfido-header'>Complete</div>
-      <div className='onfido-method-selector onfido-method'>
-        <span className='onfido-icon onfido-icon--complete'></span>
-        <p className='onfido-complete-text'>
-          Everything is complete, thank you.<br/>
-          You can now close this window.
-        </p>
-      </div>
+const Complete = (props) => (
+  <div>
+    <ActionBar {...props} />
+    <div className='onfido-step'>
+      <h1 className='onfido-title onfido-center'>Complete</h1>
+      <span className='onfido-icon onfido-icon--complete'></span>
+      <p className='onfido-center'>
+        Everything is complete, thank you.<br/>
+        You can now close this window.
+      </p>
     </div>
-  )
-}
+  </div>
+)
 
-export default HomeComplete
+export default Complete
