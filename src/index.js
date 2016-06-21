570456212788140b10e3c779ce9ecec8b770c8b6
Lawrence Gosset <lawrencegosset@gmail.com>
2016-06-21 18:31:25 +0100

    Export selectors from core

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import connect from './connect'
 import store from './store/store'
 import events from './core/events'
 import { actions, unboundActions } from './store/actions'
+import * as selectors from './store/selectors'
 
 export {
   connect,
   store,
   actions,
   events,
-  unboundActions
+  unboundActions,
+  selectors
 }
