c2e3ac215dfdfb7e2d6aadebec56d13c9e4f7e0f
Lawrence Gosset <lawrencegosset@gmail.com>
2016-06-21 18:34:30 +0100

    Refactor action creators

--- src/store/actions/captures.js
+++ src/store/actions/captures.js
@@ -14,9 +14,23 @@ export function captureIsValid(payload) {
   }
 }
 
+export function clearDocuments(payload) {
+  return {
+    type: constants.CLEAR_DOCUMENTS,
+    payload
+  }
+}
+
 export function faceCapture(payload) {
   return {
     type: constants.FACE_CAPTURE,
+    payload
+  }
+}
+
+export function clearFaces(payload) {
+  return {
+    type: constants.CLEAR_FACES,
     payload
   }
 }
