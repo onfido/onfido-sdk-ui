958ac03895f40e4f376f4354cec13f0cfacfc37a
Lawrence Gosset <lawrencegosset@gmail.com>
2016-06-21 18:45:01 +0100

    Bind event callbacks on initialisation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { h, render, Component } from 'preact'
 import { Provider } from 'react-redux'
-import { store, events, connect as ws } from 'onfido-sdk-core'
+import { store, events, connect as ws } from '../../onfido-sdk-core/src/index'
 import Modal from './components/Modal'
 import App from './components/app'
 import { Router, route } from 'preact-router'
@@ -39,12 +39,20 @@ class Container extends Component {
 
 }
 
+function bindEvents (options) {
+  events.once('ready', () => options.onReady())
+  events.once('documentCapture', data => options.onDocumentCapture(data))
+  events.once('faceCapture', data => options.onFaceCapture(data))
+  events.once('complete', data => options.onComplete(data))
+}
+
 Onfido.init = (opts) => {
   // route('/', true)
   const options = objectAssign({}, defaults, opts)
+  bindEvents(options)
   options.mount = document.getElementById(options.containerId)
   Modal.create(options)
-  render(<Container options={options}/>, options.mount)
+  render( <Container options={options}/>, options.mount )
 }
 
 Onfido.getCaptures = () => events.getCaptures()
