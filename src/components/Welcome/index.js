55632372781fc21080e08df48a24911220372595
Ricardo Freitas <derfreitas@gmail.com>
2016-07-19 14:30:17 +0100

    made complete and welcome screen more parameterizable

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,26 +1,33 @@
 import { h, Component } from 'preact'
 import { Link } from 'preact-router'
 
-const Welcome = (props) => {
+const Welcome = ({title, descriptions, nextButton, nextLink}) => {
 
   return (
     <div>
       <div className='onfido-step'>
-        <h1 className='onfido-title'>Open your new bank account</h1>
+        <h1 className='onfido-title'>{title}</h1>
         <div class='onfido-mtop-large onfido-mbottom-large'>
-          <p>To open a bank account, we will need to verify your identity.</p>
-          <p>It will only take a couple of minutes.</p>
+          {descriptions.map(description => <p>{description}</p>)}
         </div>
         <Link
-          href={props.nextLink}
-          className='onfido-btn onfido-btn-centered onfido-btn-primary'
-        >
-          Verify Identity
+          href={nextLink}
+          className='onfido-btn onfido-btn-centered onfido-btn-primary'>
+          {nextButton}
         </Link>
       </div>
     </div>
 
   )
+}
+
+Welcome.defaultProps =  {
+  title: 'Open your new bank account',
+  descriptions: [
+    'To open a bank account, we will need to verify your identity.',
+    'It will only take a couple of minutes.'
+  ],
+  nextButton:'Verify Identity'
 }
 
 export default Welcome
