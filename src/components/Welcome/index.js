98b9859a684a0bece1f5bcab95d3bd80f51fabaf
Ricardo Freitas <derfreitas@gmail.com>
2016-08-18 11:17:38 +0100

    fully refactored welcome component to have modular CSS

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,25 +1,23 @@
 import { h, Component } from 'preact'
 import { Link } from 'preact-router'
 import theme from '../../style/refactor.css'
-import style from '../../style/style.css'
+import style from './style.css'
 
 const Welcome = ({title, descriptions, nextButton, nextLink}) => {
-
   return (
     <div>
       <div className={theme.step}>
         <h1 className={theme.title}>{title}</h1>
-        <div className='onfido-mtop-large onfido-mbottom-large'>
+        <div className={`${style['mtop-large']} ${theme["mbottom-large"]}`}>
           {descriptions.map(description => <p>{description}</p>)}
         </div>
         <Link
           href={nextLink}
-          className='onfido-btn onfido-btn-centered onfido-btn-primary'>
+          className={`${theme.btn} ${theme["btn-centered"]} ${theme["btn-primary"]}`}>
           {nextButton}
         </Link>
       </div>
     </div>
-
   )
 }
 
