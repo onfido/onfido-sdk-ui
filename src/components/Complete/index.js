29f8a665db54f9cd6b9f2ee0aeafbce4716299a4
Ricardo Freitas <derfreitas@gmail.com>
2016-08-22 15:20:55 +0100

    refactored Complete component style to be modular

--- src/components/Complete/index.js
+++ src/components/Complete/index.js
@@ -1,13 +1,14 @@
 import { h, Component } from 'preact'
 import { Link } from 'preact-router'
 import theme from '../../style/refactor.css'
+import style from './style.css'
 
 const Complete = ({message, submessage}) => (
   <div>
-    <div className='onfido-step'>
-      <span className='onfido-icon onfido-icon--complete'></span>
-      <h1 className={`onfido-title ${theme.center}`}>{message}</h1>
-      <p className={`onfido-mbottom-large ${theme.center}`}>{submessage}</p>
+    <div className={theme.step}>
+      <span className={`${theme.icon}  ${style.icon}`}></span>
+      <h1 className={`${theme.title} ${theme.center}`}>{message}</h1>
+      <p className={`${theme["mbottom-large"]} ${theme.center}`}>{submessage}</p>
     </div>
   </div>
 )
