f68dc06b2eacf6bfd0f3655f86cf2126afadaa4a
Ricardo Freitas <derfreitas@gmail.com>
2016-08-22 11:22:05 +0100

    removed unused div ID

--- src/components/Face/index.js
+++ src/components/Face/index.js
@@ -7,7 +7,7 @@ export const FaceTitle = ({ useCapture }) => {
     'Place your face in the circle' :
     'Upload a picture of your face'
 
-  return <div className='onfido-title'>{titleString}</div>
+  return <div className={theme.title}>{titleString}</div>
 }
 
 export const FaceOverlay = () => {
@@ -22,7 +22,6 @@ export const FaceInstructions = ({ handeClick }) => {
   return (
     <div className='onfido-face-instructions'>
       <button
-        id='onfido-capture'
         className='onfido-btn onfido-btn-primary onfido-btn-capture onfido-btn-centered'
         onClick={handeClick}
       >
