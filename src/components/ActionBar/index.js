f02701213d0c1c75ebec9bb110db6baf189cba56
Ricardo Freitas <derfreitas@gmail.com>
2016-08-22 14:56:23 +0100

    refactored ActionBar component style to be modular

--- src/components/ActionBar/index.js
+++ src/components/ActionBar/index.js
@@ -1,18 +1,20 @@
 import { h, Component } from 'preact'
 import { Link } from 'preact-router'
+import theme from '../../style/refactor.css'
+import style from './style.css'
 
 const ActionBar = (props) => {
   const firstStep = (props.step < 2 || !props.step)
   return (
-    <div className='onfido-actions onfido-actions--top'>
+    <div className={`${theme.actions} ${style.base}`}>
       {!firstStep && <Link
         href={props.prevLink}
-        className='onfido-btn-nav'>
+        className={style.btn}>
           <span>&larr;&nbsp;Back</span>
       </Link> || <span />}
       <a
         rel='modal:close'
-        className='onfido-btn-nav onfido-btn-nav--right'
+        className={style.btn}
       >
         × Close
       </a>
