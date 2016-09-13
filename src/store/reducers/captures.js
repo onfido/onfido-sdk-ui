d9f9722159b8570ce09c59c2a53f7b0e610cd819
Ricardo Freitas <derfreitas@gmail.com>
2016-09-13 15:34:52 +0100

    small refactor

--- src/store/reducers/captures.js
+++ src/store/reducers/captures.js
@@ -6,7 +6,7 @@ const initialState = {
   face: []
 }
 
-const changeCaptures = (captures, validator, newCaptureDiffState) => captures.map( capture =>
+const changeCapturesState = (captures, validator, newCaptureDiffState) => captures.map( capture =>
   validator(capture) ? objectAssign({}, capture, newCaptureDiffState) : capture
 )
 
@@ -16,16 +16,17 @@ export function captures(state = initialState, action) {
   const captures = state[captureType]
 
   const newStateWithCaptureState = newCaptureState => objectAssign({}, state, { [captureType]: newCaptureState })
+  const changeCapturesStateBinded = changeCapturesState.bind(this, captures)
 
   switch (action.type) {
     case constants.CAPTURE_CREATE:
       const oldCaptures = captures.slice(0, 2)
-      return newStateWithCaptureState([ payload.data, ...oldCaptures ])
+      return newStateWithCaptureState([payload.data, ...oldCaptures])
     case constants.CAPTURE_VALID:
-      const valid = changeCaptures(captures, capture => capture.id === payload.data, { valid: true })
+      const valid = changeCapturesStateBinded(capture => capture.id === payload.data, { valid: true })
       return newStateWithCaptureState(valid)
     case constants.CAPTURE_CONFIRM:
-      const confirmed = changeCaptures(captures, capture => capture.id === payload.data.id, { confirmed: true })
+      const confirmed = changeCapturesStateBinded(capture => capture.id === payload.data.id, { confirmed: true })
       return newStateWithCaptureState(confirmed)
     case constants.CAPTURE_DELETE:
       return newStateWithCaptureState([])
