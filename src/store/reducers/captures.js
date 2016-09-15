80cc9decc53abeb85c4887da381f4c424b2c50a4
Ricardo Freitas <derfreitas@gmail.com>
2016-09-15 17:38:45 +0100

    fixed typo on private function name

--- src/store/reducers/captures.js
+++ src/store/reducers/captures.js
@@ -16,17 +16,17 @@ export function captures(state = initialState, action) {
   const captures = state[captureType]
 
   const newStateWithCaptureState = newCaptureState => objectAssign({}, state, { [captureType]: newCaptureState })
-  const changeCapturesStateBinded = changeCapturesState.bind(this, captures)
+  const changeCapturesStateBound = changeCapturesState.bind(this, captures)
 
   switch (action.type) {
     case constants.CAPTURE_CREATE:
       const oldCaptures = captures.slice(0, 2)
       return newStateWithCaptureState([payload.data, ...oldCaptures])
     case constants.CAPTURE_VALID:
-      const valid = changeCapturesStateBinded(capture => capture.id === payload.data, { valid: true })
+      const valid = changeCapturesStateBound(capture => capture.id === payload.data, { valid: true })
       return newStateWithCaptureState(valid)
     case constants.CAPTURE_CONFIRM:
-      const confirmed = changeCapturesStateBinded(capture => capture.id === payload.data.id, { confirmed: true })
+      const confirmed = changeCapturesStateBound(capture => capture.id === payload.data.id, { confirmed: true })
       return newStateWithCaptureState(confirmed)
     case constants.CAPTURE_DELETE:
       return newStateWithCaptureState([])
