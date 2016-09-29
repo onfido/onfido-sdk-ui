ebf8652b39a152118b1b8b8695d9c9f108d429e5
Ricardo Freitas <derfreitas@gmail.com>
2016-09-29 14:08:07 +0100

    Travis: added support for TAGs; added support for multiple deploy domains 	1. The latest tag will be pushed in addition to latest.OWNER.REPO.surge.sh

--- deploy.sh
+++ deploy.sh
@@ -1,37 +1,55 @@
 #!/bin/bash
 
-# -f 2 trims the first two chars
-# ref http://stackoverflow.com/a/37813003/689223 ;
-BRANCH_NAME=$TRAVIS_BRANCH
-
-# replaces "/" with "."
-# sed -r is only supported in linux, ref http://stackoverflow.com/a/2871217/689223
-BRANCH_SUBDOMAIN=`echo "$BRANCH_NAME" | sed -r 's/[\/]+/\./g'`
-
-PRNAME=""
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]
-then
-  PRNAME=PR${TRAVIS_PULL_REQUEST}.
-fi
-
 # Split on "/", ref: http://stackoverflow.com/a/5257398/689223
 REPO_SLUG_ARRAY=(${TRAVIS_REPO_SLUG//\// })
 REPO_OWNER=${REPO_SLUG_ARRAY[0]}
 REPO_NAME=${REPO_SLUG_ARRAY[1]}
 
-DEPLOY_DOMAIN=https://${PRNAME}${BRANCH_SUBDOMAIN}.${REPO_NAME}.${REPO_OWNER}.surge.sh
 DEPLOY_PATH=./dist
 
-# Using the Issues api instead of the PR api
-# Done so because every PR is an issue, and the issues api allows to post general comments,
-# while the PR api requires that comments are made to specific files and specific commits
-GITHUB_PR_COMMENTS=https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments
 
-echo $BRANCH_NAME $BRANCH_SUBDOMAIN $DEPLOY_DOMAIN
-
-surge --project ${DEPLOY_PATH} --domain $DEPLOY_DOMAIN;
+DEPLOY_SUBDOMAIN_UNFORMATTED_LIST=()
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]
 then
-  curl -H "Authorization: token ${GITHUB_API_TOKEN}" --request POST ${GITHUB_PR_COMMENTS} --data '{"body":"Travis automatic deployment: '${DEPLOY_DOMAIN}'"}'
+  DEPLOY_SUBDOMAIN_UNFORMATTED_LIST+=(${TRAVIS_PULL_REQUEST}.pr)
+elif [ -n "${TRAVIS_TAG// }" ] #TAG is not empty
+then
+  LATEST_TAG=`git tag | sort -r | sed -n 1p`
+
+  if [ "$TRAVIS_TAG" == "$LATEST_TAG" ]
+  then
+    DEPLOY_SUBDOMAIN_UNFORMATTED_LIST+=(latest)
+  fi
+
+  DEPLOY_SUBDOMAIN_UNFORMATTED_LIST+=(${TRAVIS_TAG}.tag)
+else
+  DEPLOY_SUBDOMAIN_UNFORMATTED_LIST+=(${TRAVIS_BRANCH}.branch)
 fi
+
+
+for DEPLOY_SUBDOMAIN_UNFORMATTED in "${DEPLOY_SUBDOMAIN_UNFORMATTED_LIST[@]}"
+do
+  echo $DEPLOY_SUBDOMAIN_UNFORMATTED
+
+  # replaces "/" with "."
+  # sed -r is only supported in linux, ref http://stackoverflow.com/a/2871217/689223
+  # Domain names follow the RFC1123 spec [a-Z] [0-9] [-]
+  # The length is limited to 253 characters
+  # https://en.wikipedia.org/wiki/Domain_Name_System#Domain_name_syntax
+  DEPLOY_SUBDOMAIN=`echo "$DEPLOY_SUBDOMAIN_UNFORMATTED" | sed -r 's/[\/]+/\./g'`
+
+  DEPLOY_DOMAIN=https://${DEPLOY_SUBDOMAIN}.${REPO_NAME}.${REPO_OWNER}.surge.sh
+
+  surge --project ${DEPLOY_PATH} --domain $DEPLOY_DOMAIN;
+
+  if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+  then
+    # Using the Issues api instead of the PR api
+    # Done so because every PR is an issue, and the issues api allows to post general comments,
+    # while the PR api requires that comments are made to specific files and specific commits
+    GITHUB_PR_COMMENTS=https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments
+
+    curl -H "Authorization: token ${GITHUB_API_TOKEN}" --request POST ${GITHUB_PR_COMMENTS} --data '{"body":"Travis automatic deployment: '${DEPLOY_DOMAIN}'"}'
+  fi
+done
