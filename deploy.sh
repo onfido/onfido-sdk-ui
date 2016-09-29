dd591428778bad8c660896f4d46e0a1fef64b77d
Ricardo Freitas <derfreitas@gmail.com>
2016-09-29 11:56:16 +0100

    Travis: added comments and improved formatting; made the domain's owner/repo subdomain section not hardcoded

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,37 @@
-BRANCH_NAME=$TRAVIS_BRANCH # ref http://stackoverflow.com/a/37813003/689223 ; -f 2 trims the first two chars
-BRANCH_SUBDOMAIN=`echo "$BRANCH_NAME" | sed -r 's/[\/]+/\./g'` # sed -r is only supported in linux, ref http://stackoverflow.com/a/2871217/689223
+#!/bin/bash
+
+# -f 2 trims the first two chars
+# ref http://stackoverflow.com/a/37813003/689223 ;
+BRANCH_NAME=$TRAVIS_BRANCH
+
+# replaces "/" with "."
+# sed -r is only supported in linux, ref http://stackoverflow.com/a/2871217/689223
+BRANCH_SUBDOMAIN=`echo "$BRANCH_NAME" | sed -r 's/[\/]+/\./g'`
+
 PRNAME=""
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then PRNAME=PR${TRAVIS_PULL_REQUEST}.; fi
-BRANCH_DOMAIN=https://${PRNAME}${BRANCH_SUBDOMAIN}.onfido-sdk-ui.onfido.surge.sh
-GITHUB_PR_COMMENTS=https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments #every PR is an issue, and the issues api allows to post general comments, while the PR api requires that comments are made to specific files
-echo $BRANCH_NAME $BRANCH_SUBDOMAIN $BRANCH_DOMAIN
-surge --project ./dist --domain $BRANCH_DOMAIN;
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then curl -H "Authorization: token ${GITHUB_API_TOKEN}" --request POST ${GITHUB_PR_COMMENTS} --data '{"body":"Travis automatic deployment: '${BRANCH_DOMAIN}'"}'; fi
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+then
+  PRNAME=PR${TRAVIS_PULL_REQUEST}.
+fi
+
+# Split on "/", ref: http://stackoverflow.com/a/5257398/689223
+REPO_SLUG_ARRAY=(${TRAVIS_REPO_SLUG//\// })
+REPO_OWNER=${REPO_SLUG_ARRAY[0]}
+REPO_NAME=${REPO_SLUG_ARRAY[1]}
+
+DEPLOY_DOMAIN=https://${PRNAME}${BRANCH_SUBDOMAIN}.${REPO_NAME}.${REPO_OWNER}.surge.sh
+DEPLOY_PATH=./dist
+
+# Using the Issues api instead of the PR api
+# Done so because every PR is an issue, and the issues api allows to post general comments,
+# while the PR api requires that comments are made to specific files and specific commits
+GITHUB_PR_COMMENTS=https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments
+
+echo $BRANCH_NAME $BRANCH_SUBDOMAIN $DEPLOY_DOMAIN
+
+surge --project ${DEPLOY_PATH} --domain $DEPLOY_DOMAIN;
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+then
+  curl -H "Authorization: token ${GITHUB_API_TOKEN}" --request POST ${GITHUB_PR_COMMENTS} --data '{"body":"Travis automatic deployment: '${DEPLOY_DOMAIN}'"}'
+fi
