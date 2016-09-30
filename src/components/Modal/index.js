a593244deeb624621c625d1accf32b6a7cc40a83
Ricardo Freitas <derfreitas@gmail.com>
2016-09-30 16:59:39 +0100

    fixed bug of css classes not being correctly passed to vanilla

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -4,32 +4,42 @@ import style from './style.css'
 
 const Modal = {}
 
-Modal.template = `
+const template = `
   <div class='${style.inner}'>
     <div class='${style.content}'></div>
   </div>
 `
 
-Modal.create = (options) => {
+const generateModal = ({useModal}) => {
   const modal = document.createElement('div')
-  modal.className = options.useModal ? style.modal : style.inline
-  modal.innerHTML = Modal.template
+  modal.className = useModal ? style.modal : style.inline
+  modal.innerHTML = template
+  return modal;
+}
+
+Modal.create = (options) => {
+  const modal = generateModal(options)
+
+  const containerEl = document.getElementById(options.containerId)
 
   if (options.useModal) {
+    options.mount = containerEl
     options.mount.parentNode.insertBefore(modal, options.mount.nextSibling)
   }
   else {
-    options.mountRoot = options.mount
+    options.mountRoot = containerEl
     options.mount = modal.getElementsByClassName(style.content)[0]
     options.mountRoot.appendChild(modal)
+    options.mountRoot.className += " "+style.root
   }
 
-  options.mountRoot.className += " "+style.root
 
   events.emit('modalMounted', options)
 }
 
-Modal.options = {
+const vanillaOptions = {
+  loadClass: style.vanillaModal,
+  class: style.modalVisible,
   modal: '.'+style.modal,
   modalInner: '.'+style.inner,
   modalContent: '.'+style.content,
@@ -38,12 +48,13 @@ Modal.options = {
   onBeforeOpen: () => events.emit('onBeforeOpen'),
   onBeforeClose: () => events.emit('onBeforeClose')
 }
+console.log(vanillaOptions)
 
 events.on('modalMounted', (options) => {
   if (options.useModal) {
-    const modal = new VanillaModal(Modal.options)
+    const modal = new VanillaModal(vanillaOptions)
     const button = document.getElementById(options.buttonId)
-    const id = `#${options.mount.getAttribute('id')}`
+    const id = "#"+options.containerId
     button.addEventListener('click', () => modal.open(id))
     events.on('onOpen', () => options.mount.style.display = 'block')
     events.on('onClose', () => options.mount.style.display = 'none')
