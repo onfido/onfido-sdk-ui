76136472589a23ea32633452d1304f124a210f6a
Stefania Cardenas <stefania.cardenas@onfido.com>
2017-06-26 17:57:28 +0100

    Added Error component and basic style

--- src/components/Confirm/index.js
+++ src/components/Confirm/index.js
@@ -5,6 +5,7 @@ import style from './style.css'
 import { isOfFileType } from '../utils/file'
 import {preventDefaultOnClick} from '../utils'
 import PdfViewer from './PdfPreview'
+import ApiUploadError from '../Error'
 
 const CaptureViewerPure = ({capture:{blob, base64, previewUrl}}) =>
   <div className={style.captures}>
@@ -57,35 +58,48 @@ class CaptureViewer extends Component {
   }
 }
 
-
-const Previews = ({capture, retakeAction, confirmAction} ) =>
-  <div className={`${theme.previews} ${theme.step}`}>
+const PreviewHeader = () =>
+  <div>
     <h1 className={theme.title}>Confirm capture</h1>
     <p>Please confirm that you are happy with this photo.</p>
-    <CaptureViewer capture={capture} />
-    <div className={`${theme.actions} ${style.actions}`}>
-      <button
-        onClick={retakeAction}
-        className={`${theme.btn} ${style["btn-outline"]}`}
-      >
-        Take again
-      </button>
-      <a
-        href=''
-        className={`${theme.btn} ${theme["btn-primary"]}`}
-        onClick={preventDefaultOnClick(confirmAction)}
-      >
-        Confirm
-      </a>
+  </div>
+
+const RetakeAction = ({retakeAction}) =>
+  <button onClick={retakeAction}
+    className={`${theme.btn} ${style["btn-outline"]}`}>
+    Take again
+  </button>
+
+const ConfirmAction = ({confirmAction, error}) => {
+  if (!error) return (
+    <a href='' className={`${theme.btn} ${theme["btn-primary"]}`}
+      onClick={preventDefaultOnClick(confirmAction)}>
+      Confirm
+    </a>
+  )
+}
+
+const Actions = ({retakeAction, confirmAction, error}) =>
+  <div>
+    <div className={`${theme.actions} ${style.actions} ${error ? style.error : ''}`}>
+      <RetakeAction retakeAction={retakeAction} />
+      <ConfirmAction confirmAction={confirmAction} error={error}/>
     </div>
   </div>
 
+const Previews = ({capture, retakeAction, confirmAction, error} ) =>
+  <div className={`${theme.previews} ${theme.step}`}>
+    {error ? <ApiUploadError error={error} /> : <PreviewHeader /> }
+    <CaptureViewer capture={capture} />
+    <Actions retakeAction={retakeAction} confirmAction={confirmAction} error={error} />
+  </div>
 
-const Confirm = ({method, side, validCaptures:[capture], onConfirm, actions: {deleteCaptures}}) =>
+const Confirm = ({method, side, validCaptures:[capture], onConfirm, error, actions: {deleteCaptures}}) =>
   <Previews
     capture={capture}
     retakeAction={() => deleteCaptures({method, side})}
     confirmAction={onConfirm}
+    error={error}
   />
 
 export default Confirm
