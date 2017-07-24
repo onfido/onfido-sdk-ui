8cc27847f61dabf43c9ac841a59e92bad24b9483
Stefania Cardenas <stefania.cardenas@onfido.com>
2017-07-24 16:22:02 +0100

    Refactored error copy and added style

--- src/components/strings/errors.js
+++ src/components/strings/errors.js
@@ -1,9 +1,9 @@
 export const errors = {
-  'INVALID_CAPTURE': 'No document detected. Make sure all the document is in the picture.',
-  'CORRUPTED_FILE': 'File not uploading. Try using another file type',
-  'UNSUPPORTED_FILE': 'Unsupported file type. Try using a .jpg or .png file.',
-  'INVALID_SIZE': 'File size too large. Size needs to be smaller than 10MB.',
-  'NO_FACE_ERROR': 'No face found. Your face is needed in the picture.',
-  'MULTIPLE_FACES_ERROR': 'Multiple faces found. Only your face can be in the picture.',
-  'SERVER_ERROR': 'Connection lost. Please try again.'
+  'INVALID_CAPTURE': { message:'No document detected', instruction: 'Make sure the document is in the picture'},
+  'CORRUPTED_FILE': {message: 'File not uploading', instruction: 'Try using another file type'},
+  'UNSUPPORTED_FILE': {message: 'Unsupported file type', instruction: 'Try using a .jpg or .png file'},
+  'INVALID_SIZE': {message: 'File size too large', instruction: 'Size needs to be smaller than 10MB'},
+  'NO_FACE_ERROR': {message: 'No face found', instruction: 'Your face is needed in the picture'},
+  'MULTIPLE_FACES_ERROR': {message: 'Multiple faces found', instruction: 'Only your face can be in the picture'},
+  'SERVER_ERROR': {message: 'Connection lost', instruction: 'Please try again'}
 }
