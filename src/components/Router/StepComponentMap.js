40326b0cfa74b2bf3f500161dc03d03e0b6394aa
Ricardo Freitas <derfreitas@gmail.com>
2017-07-27 18:02:55 +0100

    added screen tracks to route level components

--- src/components/Router/StepComponentMap.js
+++ src/components/Router/StepComponentMap.js
@@ -12,10 +12,10 @@ export const createComponentList = (steps, documentType) => {
 const createComponent = ({type, options}, documentType) => {
   const wrapComponentB = wrapComponent(options)
   const stepMap = {
-    welcome: () => wrapComponentB(Welcome),
-    face: () => wrapComponentB(FaceCapture),
+    welcome: () => wrapComponentB(Welcome, "welcome"),
+    face: () => wrapComponentB(FaceCapture, "face_capture"),
     document: () => createDocumentComponents({type, options}, documentType),
-    complete: () => wrapComponentB(Complete)
+    complete: () => wrapComponentB(Complete, "complete")
   }
   if (!(type in stepMap)) { console.error('No such step: ' + type) }
   return stepMap[type]()
@@ -25,13 +25,13 @@ const createDocumentComponents = ({type, options}, documentType) => {
   const wrapComponentB = wrapComponent(options)
   const double_sided_docs = ['driving_licence', 'national_identity_card']
   const frontDocumentFlow = [
-    wrapComponentB(Select),
-    wrapComponentB(FrontDocumentCapture)]
+    wrapComponentB(Select, "document_select"),
+    wrapComponentB(FrontDocumentCapture, "document_capture_front")]
 
   if (double_sided_docs.indexOf(documentType) !== -1) {
     return [
       ...frontDocumentFlow,
-      wrapComponentB(BackDocumentCapture)]
+      wrapComponentB(BackDocumentCapture, "document_capture_back")]
   }
   return frontDocumentFlow
 }
