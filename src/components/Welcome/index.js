9db09e38f60c0bb897131e08fd2f624394106e4b
Ricardo Freitas <derfreitas@gmail.com>
2017-07-31 17:02:06 +0100

    removed console log from welcome component

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -3,25 +3,21 @@ import theme from '../Theme/style.css'
 import style from './style.css'
 import {preventDefaultOnClick, triggerOnMount} from '../utils'
 
-const Welcome = ({title, descriptions, nextButton, nextStep}) => {
-  console.log("Welcome")
-  return (
-    <div>
-      <div className={theme.step}>
-        <h1 className={theme.title}>{title}</h1>
-        <div className={`${style['mtop-large']} ${theme["mbottom-large"]}`}>
-          {descriptions.map(description => <p>{description}</p>)}
-        </div>
-        <a
-          href=''
-          className={`${theme.btn} ${theme["btn-centered"]} ${theme["btn-primary"]}`}
-          onClick={preventDefaultOnClick(nextStep)}>
-          {nextButton}
-        </a>
+const Welcome = ({title, descriptions, nextButton, nextStep}) =>
+  <div>
+    <div className={theme.step}>
+      <h1 className={theme.title}>{title}</h1>
+      <div className={`${style['mtop-large']} ${theme["mbottom-large"]}`}>
+        {descriptions.map(description => <p>{description}</p>)}
       </div>
+      <a
+        href=''
+        className={`${theme.btn} ${theme["btn-centered"]} ${theme["btn-primary"]}`}
+        onClick={preventDefaultOnClick(nextStep)}>
+        {nextButton}
+      </a>
     </div>
-  )
-}
+  </div>
 
 Welcome.defaultProps =  {
   title: 'Open your new bank account',
