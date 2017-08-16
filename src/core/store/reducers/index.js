5a4ab5f1014b325512e7acd18490782794295ae9
Ricardo Freitas <derfreitas@gmail.com>
2017-08-16 16:04:19 +0100

    added reset store action

--- src/core/store/reducers/index.js
+++ src/core/store/reducers/index.js
@@ -1,8 +1,16 @@
 import { combineReducers } from 'redux'
 import { captures } from './captures'
 import globals from './globals'
+import * as constants from '../../constants'
 
-export default combineReducers({
+const appReducer = combineReducers({
   captures,
   globals
 })
+
+export default (state, action) => {
+  if (action.type === constants.RESET_STORE) {
+    return appReducer({}, {})
+  }
+  return appReducer(state, action)
+}
