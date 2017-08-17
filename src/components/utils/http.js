eccc48667caafbaa5f27b48daf49cc8fe0aca5b7
Henry Heath <henry.heath@onfido.com>
2017-08-17 17:48:12 +0100

    Read steps from query string  and util to shorten URLs

--- src/components/utils/http.js
+++ src/components/utils/http.js
@@ -17,3 +17,21 @@ export const performHttpReq = ({payload, endpoint, contentType, token}, onSucces
 
   request.send(payload)
 }
+
+export const shortenUrl = (longUrl, onSuccess, onError) => {
+  const request = new XMLHttpRequest()
+  const key = process.env.URL_SHORTENER_KEY
+  const url = `https://www.googleapis.com/urlshortener/v1/url?key=${key}`
+  request.open('POST', url)
+  request.setRequestHeader('Content-Type', 'application/json')
+
+  request.onload = () => {
+    if (request.status === 200 || request.status === 201) {
+      onSuccess(JSON.parse(request.response))}
+    else {
+      onError(request)
+    }
+  }
+  request.onerror = () => onError(request)
+  request.send({longUrl})
+}
