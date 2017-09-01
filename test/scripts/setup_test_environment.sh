16e02b8966d8e8d6a43521a80e6a8adbc6fc7e0f
Dawid Dylowicz <dawid.dylowicz@onfido.com>
2017-09-01 14:16:09 +0100

    Fix openssl command

--- test/scripts/setup_test_environment.sh
+++ test/scripts/setup_test_environment.sh
@@ -4,7 +4,6 @@ set -ev
 
 chmod ugo+x ./test/scripts/install_chromedriver_travis.sh
 sudo ./test/scripts/install_chromedriver_travis.sh
-openssl aes-256-cbc -K $encrypted_1688a3f2e0a8_key -iv $encrypted_1688a3f2e0a8_iv
-  -in ../monster_rsa.enc -out ~/.ssh/monster_rsa -d
+openssl aes-256-cbc -K $encrypted_1688a3f2e0a8_key -iv $encrypted_1688a3f2e0a8_iv -in ../monster_rsa.enc -out ~/.ssh/monster_rsa -d
 chmod 600 ~/.ssh/monster_rsa
 gem install bundler
