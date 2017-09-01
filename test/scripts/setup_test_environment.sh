f9945f428ba77548ede1ba4debe842f26def07d4
Dawid Dylowicz <dawid.dylowicz@onfido.com>
2017-09-01 14:33:07 +0100

    Check what's inside ssh

--- test/scripts/setup_test_environment.sh
+++ test/scripts/setup_test_environment.sh
@@ -6,4 +6,5 @@ chmod ugo+x ./test/scripts/install_chromedriver_travis.sh
 sudo ./test/scripts/install_chromedriver_travis.sh
 openssl aes-256-cbc -K $encrypted_1688a3f2e0a8_key -iv $encrypted_1688a3f2e0a8_iv -in test/monster_rsa.enc -out ~/.ssh/monster_rsa -d
 chmod 600 ~/.ssh/monster_rsa
+ls -la ~/.ssh
 gem install bundler
