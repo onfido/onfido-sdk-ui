7b035458387d996a1782e85e947d79ae2ed78ff7
Dawid Dylowicz <dawid.dylowicz@onfido.com>
2017-09-06 10:41:20 +0100

    Remove Chromium and Google Chrome browsers before installing new one

--- test/scripts/install_chromedriver_travis.sh
+++ test/scripts/install_chromedriver_travis.sh
@@ -2,8 +2,15 @@
 
 set -ev
 
-CHROME_VERSION="google-chrome-beta"
+CHROME_VERSION="google-chrome-stable"
 CHROME_DRIVER_VERSION=2.31
+
+echo "Uninstalling current Chromium from `which chromium-browser`..."
+sudo apt-get purge chromium-browser
+rm ~/.config/chromium/ -rf
+echo "Uninstalling current Google Chrome from `which google-chrome`..."
+sudo apt-get purge google-chrome-stable
+rm ~/.config/google-chrome/ -rf
 
 echo "Installing $CHROME_VERSION..."
 sudo wget --no-verbose -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
