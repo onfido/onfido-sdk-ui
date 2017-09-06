e26846673ffe8f26086892fc0b8bdd42a69062e6
Dawid Dylowicz <dawid.dylowicz@onfido.com>
2017-09-06 17:48:11 +0100

    Bring back sources for Chrome

--- test/scripts/install_chromedriver_travis.sh
+++ test/scripts/install_chromedriver_travis.sh
@@ -14,6 +14,8 @@ sudo apt-get purge google-chrome-stable
 rm ~/.config/google-chrome/ -rf
 
 echo "Installing $CHROME_VERSION..."
+sudo wget --no-verbose -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
 sudo apt-get update -qqy
 sudo apt-get -qqy install ${CHROME_VERSION}
 echo "Using `google-chrome --version` from `which google-chrome`"
@@ -23,3 +25,5 @@ sudo wget --no-verbose -O /tmp/chromedriver_linux64.zip https://chromedriver.sto
 sudo unzip /tmp/chromedriver_linux64.zip -d /opt
 chmod 755 /opt/chromedriver
 sudo ln -fs /opt/chromedriver /usr/bin/chromedriver
+
+ruby --version
