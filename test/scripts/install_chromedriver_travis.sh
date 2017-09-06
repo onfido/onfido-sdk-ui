c2e2fd4a7671a3e7a7d3235e7993ce4840b2a4dc
Dawid Dylowicz <dawid.dylowicz@onfido.com>
2017-09-06 18:06:38 +0100

    Remove unnecessary linking and removing sources from install_chromedriver_travis.sh script

--- test/scripts/install_chromedriver_travis.sh
+++ test/scripts/install_chromedriver_travis.sh
@@ -8,6 +8,7 @@ CHROME_DRIVER_VERSION=2.31
 echo "Uninstalling current Chromium from `which chromium-browser`..."
 sudo apt-get purge chromium-browser
 rm ~/.config/chromium/ -rf
+
 echo "Uninstalling current Google Chrome from `which google-chrome`..."
 sudo apt-get purge google-chrome-stable
 rm ~/.config/google-chrome/ -rf
@@ -17,10 +18,6 @@ sudo wget --no-verbose -q -O - https://dl-ssl.google.com/linux/linux_signing_key
 echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
 sudo apt-get update -qqy
 sudo apt-get -qqy install ${CHROME_VERSION}
-sudo rm /etc/apt/sources.list.d/google-chrome.list
-sudo rm -rf /var/lib/apt/lists/* /var/cache/apt/*
-sudo ln -fs /opt/google/chrome-beta/google-chrome /usr/bin/google-chrome
-sudo ln -fs /opt/google/chrome-beta/google-chrome /usr/local/bin/google-chrome
 echo "Using `google-chrome --version` from `which google-chrome`"
 
 echo "Installing chromedriver $CHROME_DRIVER_VERSION..."
