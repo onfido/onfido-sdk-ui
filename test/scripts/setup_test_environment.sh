47ffb4b3fe5ef75fe5331488978c6009e015ce26
Dawid Dylowicz <dawid.dylowicz@onfido.com>
2017-09-07 12:03:01 +0100

    Run only on pull requests

--- test/scripts/setup_test_environment.sh
+++ test/scripts/setup_test_environment.sh
@@ -5,7 +5,5 @@ set -ev
 chmod ugo+x ./test/scripts/install_chromedriver_travis.sh
 sudo ./test/scripts/install_chromedriver_travis.sh
 
-rvm install 2.3.0
-
 chmod 600 ~/.ssh/monster_rsa
 gem install bundler
