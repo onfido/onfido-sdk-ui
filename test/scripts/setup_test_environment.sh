c3d4cdb906fd7f0ec622c346475277f429eea6dd
Dawid Dylowicz <dawid.dylowicz@onfido.com>
2017-09-07 11:51:58 +0100

    Install ruby 2.3.0

--- test/scripts/setup_test_environment.sh
+++ test/scripts/setup_test_environment.sh
@@ -5,5 +5,7 @@ set -ev
 chmod ugo+x ./test/scripts/install_chromedriver_travis.sh
 sudo ./test/scripts/install_chromedriver_travis.sh
 
+rvm install 2.3.0
+
 chmod 600 ~/.ssh/monster_rsa
 gem install bundler
