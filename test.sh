5fcf6523cf41fecea1cf3431d9f32472382359e4
Dawid Dylowicz <dawid.dylowicz@onfido.com>
2017-09-07 11:47:06 +0100

    Run tests only against production

--- test.sh
+++ test.sh
@@ -2,27 +2,29 @@
 
 set -e
 
-# set path for test directory
-TESTS_PATH=./test
+if [ ${NODE_ENV} = "production" ]; then
+  # set path for test directory
+  TESTS_PATH=./test
 
-# setup to run browser GUI
-export DISPLAY=:99.0
-sh -e /etc/init.d/xvfb start &
-sleep 3 # give xvfb some time to start
+  # setup to run browser GUI
+  export DISPLAY=:99.0
+  sh -e /etc/init.d/xvfb start &
+  sleep 3 # give xvfb some time to start
 
-# run local server in the background and wait until it starts
-# ref: https://stackoverflow.com/a/21002153
-echo "Running local server..."
-exec 3< <(npm run travis)
-sed '/webpack: Compiled successfully.$/q' <&3 ; cat <&3 &
+  # run local server in the background and wait until it starts
+  # ref: https://stackoverflow.com/a/21002153
+  echo "Running local server..."
+  exec 3< <(npm run travis)
+  sed '/webpack: Compiled successfully.$/q' <&3 ; cat <&3 &
 
-# go to test directory
-cd $TESTS_PATH
+  # go to test directory
+  cd $TESTS_PATH
 
-# install gem dependencies using monster_rsa private key to fetch the monster gem
-GIT_SSH_COMMAND="ssh -i ~/.ssh/monster_rsa" bundle install
+  # install gem dependencies using monster_rsa private key to fetch the monster gem
+  GIT_SSH_COMMAND="ssh -i ~/.ssh/monster_rsa" bundle install
 
-# run cucumber tests against deployed domain
-SDK_URL="https://localhost:8080/?async=false"
-echo "Running Cucumber tests on ${SDK_URL}"
-bundle exec cucumber BROWSER=chrome SDK_URL=${SDK_URL} USE_SECRETS=false DEBUG=false --retry 2
+  # run cucumber tests against deployed domain
+  SDK_URL="https://localhost:8080/?async=false"
+  echo "Running Cucumber tests on ${SDK_URL}"
+  bundle exec cucumber BROWSER=chrome SDK_URL=${SDK_URL} USE_SECRETS=false DEBUG=false --retry 2
+fi
