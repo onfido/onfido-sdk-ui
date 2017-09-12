a19a2765eaab4425b88108cad4288c4fe7e3a56c
Ricardo Freitas <derfreitas@gmail.com>
2017-09-12 10:23:53 +0100

    fixed surge deploy script to support branches with #

--- deploy.sh
+++ deploy.sh
@@ -38,12 +38,13 @@ for DEPLOY_SUBDOMAIN_UNFORMATTED in "${DEPLOY_SUBDOMAIN_UNFORMATTED_LIST[@]}"
 do
   echo $DEPLOY_SUBDOMAIN_UNFORMATTED
 
-  # replaces "/" or "." with "-"
+  # replaces non alphanumeric symbols with "-"
   # sed -r is only supported in linux, ref http://stackoverflow.com/a/2871217/689223
   # Domain names follow the RFC1123 spec [a-Z] [0-9] [-]
   # The length is limited to 253 characters
   # https://en.wikipedia.org/wiki/Domain_Name_System#Domain_name_syntax
-  DEPLOY_SUBDOMAIN=`echo "$DEPLOY_SUBDOMAIN_UNFORMATTED" | sed -r 's/[\/|\.]+/\-/g'`
+  DEPLOY_SUBDOMAIN=`echo "$DEPLOY_SUBDOMAIN_UNFORMATTED" | sed -r 's/[^A-Za-z0-9]+/\-/g'`
+  echo $DEPLOY_SUBDOMAIN
 
   if [ "$NODE_ENV" == "production" ]
   then
