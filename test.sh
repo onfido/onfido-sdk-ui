9786f5dfc1c77a83236bfed1b131878c991d2ee4
Dawid Dylowicz <dawid.dylowicz@onfido.com>
2017-09-13 13:04:43 +0100

    Refactor tests with steps making them more concise

--- test.sh
+++ test.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
-set -e
+set -ev
 
-if [[ ${NODE_ENV} = "production" && ${TRAVIS_PULL_REQUEST} != "false" ]]; then
+if [[ ${CI} != "true" || (${NODE_ENV} = "production" && ${TRAVIS_PULL_REQUEST} != "false") ]]; then
   # set path for test directory
   TESTS_PATH=./test
 
   # setup to run browser GUI
-  export DISPLAY=:99.0
-  sh -e /etc/init.d/xvfb start &
-  sleep 3 # give xvfb some time to start
+  if [[ ${CI} = "true" ]]; then
+    export DISPLAY=:99.0
+    sh -e /etc/init.d/xvfb start &
+    sleep 3 # give xvfb some time to start
+  fi
 
   # run local server in the background and wait until it starts
   # ref: https://stackoverflow.com/a/21002153
+  server = $(( ${CI} == "true" ? travis : dev ))
   echo "Running local server..."
-  exec 3< <(npm run travis)
+  exec 3< <(npm run $server)
   sed '/webpack: Compiled successfully.$/q' <&3 ; cat <&3 &
 
   # go to test directory
   cd $TESTS_PATH
 
   # install gem dependencies using monster_rsa private key to fetch the monster gem
-  GIT_SSH_COMMAND="ssh -i ~/.ssh/monster_rsa" bundle install
+  if [[ ${CI} = "true"; then
+    GIT_SSH_COMMAND="ssh -i ~/.ssh/monster_rsa" bundle install
+  else
+    bundle install
+  fi
 
   # run cucumber tests against deployed domain
   SDK_URL="https://localhost:8080/?async=false"
