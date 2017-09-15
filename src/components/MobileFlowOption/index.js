5068d1439ca67fecb1916af7719332a47294bd4b
Stefania Cardenas <stefania.cardenas@onfido.com>
2017-09-15 16:02:25 +0100

    Added new upload icon

--- src/components/MobileFlowOption/index.js
+++ src/components/MobileFlowOption/index.js
@@ -1,14 +1,16 @@
-import style from './style.css'
 import { h } from 'preact'
+import style from './style.css'
 
 const MobileFlowOption = () => {
   return (
     <div className={style.container}>
-      <div className={style.icon} />
-      <div className={style.copy}>
-        <div className={style.header}>Need to use your mobile to take photos?</div>
-        <p className={style.submessage}>Safely continue verification on your mobile </p>
-      </div>
+      <a href='#'>
+        <div className={style.icon} />
+        <div className={style.copy}>
+          <div className={style.header}>Need to use your mobile to take photos?</div>
+          <p className={style.submessage}>Safely continue verification on your mobile </p>
+        </div>
+      </a>
       <div className={style.chevron} />
     </div>
   )
