f6f303bea9cbdadb15e1a3af999dfec1e27456d4
Henry Heath <henry.heath@onfido.com>
2017-09-27 14:16:43 +0100

    Add cancel button to MobileConnected
    Other UI fixes

--- src/components/MobileConnected/index.js
+++ src/components/MobileConnected/index.js
@@ -1,30 +1,32 @@
 import { h, Component } from 'preact'
+
 import theme from '../Theme/style.css'
 import style from './style.css'
+import {preventDefaultOnClick} from '../utils'
 
 class MobileConnected extends Component {
   constructor (props) {
     super(props)
   }
 
-  render ({message, submessage}) {
+  render ({message, submessage, confirmAction}) {
     //TODO apply Tips box styles to all components with tips box
     return (
-      <div>
       <div className={theme.step}>
         <h1 className={`${theme.title} ${theme.center}`}>{message}</h1>
-        <p className={`${style.submessage}`}>{submessage}</p>
-        <span className={`${theme.icon}  ${style.icon}`}></span>
-        <div className={style.help}>
-          <div className={style.header}>How do I do this?</div>
+        <p className={style.submessage}>{submessage}</p>
+        <span className={`${theme.icon} ${style.icon}`}></span>
+        <div className={style.header}>Tips</div>
+        <div className={`${style.shelp} ${style.ghelp}`}>
           <ul className={style.helpContainer}>
             <li>Keep this window open while using your mobile</li>
             <li>Your mobile link will expire in one hour</li>
             <li>Don't refresh this page</li>
           </ul>
         </div>
+        <div href='#' className={style.cancel}
+           onClick={preventDefaultOnClick(confirmAction)}>Cancel</div>
       </div>
-    </div>
     )
   }
 }
