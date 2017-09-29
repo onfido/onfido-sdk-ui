afed32e4de88d1c512a6ab0034d70badebfeb2b7
Stefania Cardenas <stefania.cardenas@onfido.com>
2017-09-29 10:44:43 +0100

    Made the whole area clickable on SwitchDevice component

--- src/components/crossDevice/SwitchDevice/index.js
+++ src/components/crossDevice/SwitchDevice/index.js
@@ -4,16 +4,16 @@ import style from './style.css'
 
 const SwitchDevice = ({changeFlowTo}) => {
   return (
-    <div className={style.container}>
-      <a href='#' onClick={preventDefaultOnClick(() => changeFlowTo('crossDeviceSteps'))}>
-        <div className={style.icon} />
-        <div className={style.copy}>
-          <div className={style.header}>Need to use your mobile to take photos?</div>
-          <p className={style.submessage}>Safely continue verification on your mobile </p>
-        </div>
-        <div className={style.chevron} />
-      </a>
-    </div>
+    <a href='#' onClick={preventDefaultOnClick(() => changeFlowTo('crossDeviceSteps'))}>
+      <div className={style.container}>
+          <div className={style.icon} />
+          <div className={style.copy}>
+            <div className={style.header}>Need to use your mobile to take photos?</div>
+            <p className={style.submessage}>Safely continue verification on your mobile </p>
+          </div>
+          <div className={style.chevron} />
+      </div>
+    </a>
   )
 }
 
