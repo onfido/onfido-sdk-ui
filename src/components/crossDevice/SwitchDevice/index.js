0c64d70fc349b3fc589522188313d3c4f587ec96
Stefania Cardenas <stefania.cardenas@onfido.com>
2017-09-29 14:49:20 +0100

    CSS changes

--- src/components/crossDevice/SwitchDevice/index.js
+++ src/components/crossDevice/SwitchDevice/index.js
@@ -9,7 +9,7 @@ const SwitchDevice = ({changeFlowTo}) => {
           <div className={style.icon} />
           <div className={style.copy}>
             <div className={style.header}>Need to use your mobile to take photos?</div>
-            <p className={style.submessage}>Safely continue verification on your mobile </p>
+            <p className={style.submessage}>Safely continue verification on your mobile</p>
           </div>
           <div className={style.chevron} />
       </div>
