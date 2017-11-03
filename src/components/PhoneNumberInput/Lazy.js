2c1699cd61f08bf0df63bb4aa442a7927fa03a94
Stefania Cardenas <stefania.cardenas@onfido.com>
2017-11-03 11:10:29 +0000

    Added comment for code splitting

--- src/components/PhoneNumberInput/Lazy.js
+++ src/components/PhoneNumberInput/Lazy.js
@@ -7,6 +7,8 @@ class PhoneNumberInputLazy extends Component {
   constructor(props){
     super(props)
     this.state = {component: Loading }
+    // We are loading the mobile validation component dynamically
+    // The code is splitted in two separate chunks.
 
     import(/* webpackChunkName: "mobile-number-validations" */ './index.js').then(component => {
       this.setState({component})
