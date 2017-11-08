3286485bf42d25ead989b46dc07114c19263d2ad
Henry Heath <henry.heath@onfido.com>
2017-11-08 13:48:17 +0000

    Remove custom step style

--- src/components/crossDevice/GenericError/index.js
+++ src/components/crossDevice/GenericError/index.js
@@ -10,7 +10,7 @@ class GenericError extends Component {
   }
   render () {
     return (
-      <div className={`${theme.step} ${style.step}`}>
+      <div className={theme.step}>
         <h1 className={theme.title}>Something’s gone wrong</h1>
         <p className={`${theme.center} ${style.submessage}`}>You’ll need to restart your verification on your computer</p>
         <span className={`${theme.icon}  ${style.icon}`} />
