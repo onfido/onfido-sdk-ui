e685946a8c6410d17ae3fa9a6f4186f77b22a2fd
Dawid Dylowicz <dawid.dylowicz@onfido.com>
2017-11-17 15:09:27 +0000

    Fix condition in setup_test_environment.sh

--- test/scripts/setup_test_environment.sh
+++ test/scripts/setup_test_environment.sh
@@ -3,7 +3,7 @@
 set -ev
 
 # run only on CI, pull requests and when NODE_ENV=test
-if [[ ${CI} == "true" || (${NODE_ENV} = "test" && ${TRAVIS_PULL_REQUEST} != "false") ]]; then
+if [[ ${CI} == "true" && (${NODE_ENV} = "test" && ${TRAVIS_PULL_REQUEST} != "false") ]]; then
   chmod ugo+x ./test/scripts/install_chromedriver_travis.sh
   sudo ./test/scripts/install_chromedriver_travis.sh
 
