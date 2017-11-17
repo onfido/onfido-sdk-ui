8468742847c3bdebbfbb13af527fcdf23bc12705
Stefania Cardenas <stefania.cardenas@onfido.com>
2017-11-17 11:49:18 +0000

    Fix hover state of button back

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -1,28 +1,15 @@
-import { h, Component } from 'preact'
+import { h } from 'preact'
 import style from './style.css'
 import classNames from 'classnames'
 import {preventDefaultOnClick} from '../utils'
 
-class NavigationBar extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {hover: false}
-  }
-
-  setHoverState = (state) => {
-    this.setState({hover: state})
-  }
-
-  render = ({back}) =>
-    <div className={style.navigation}>
-      <button href='#' className={classNames(style.back,{[style.backHover]: this.state.hover} )}
-        onClick={preventDefaultOnClick(back)}
-        onMouseEnter={() => this.setHoverState(true)}
-        onMouseLeave={() => this.setHoverState(false)}>
-          <span className={style.iconBack} />
-          back
-      </button>
-   </div>
-}
+const NavigationBar = ({back}) =>
+  <div className={style.navigation}>
+    <button href='#' className={classNames(style.back)}
+      onClick={preventDefaultOnClick(back)}>
+        <span className={style.iconBack} />
+        back
+    </button>
+ </div>
 
 export default NavigationBar
