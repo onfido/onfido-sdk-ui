b84c3cd2940ddd8d422eeb864381891a87fbabfd
Henry Heath <henry.heath@onfido.com>
2017-11-24 14:05:16 +0000

    PR feedback - fix bug on deploying tags and branches to surge

--- deploy.sh
+++ deploy.sh
@@ -21,25 +21,32 @@ then
   then
     DEPLOY_SUBDOMAIN_UNFORMATTED_LIST+=(${TRAVIS_PULL_REQUEST}-pr)
   fi
-elif [ -n "${TRAVIS_TAG// }" ]  && [ "$NODE_ENV" == "production" ] #TAG is not empty
+elif [ -n "${TRAVIS_TAG// }" ] #TAG is not empty
 then
-  #sorts the tags and picks the latest
-  #sort -V does not work on the travis machine
-  #sort -V              ref: http://stackoverflow.com/a/14273595/689223
-  #sort -t ...          ref: http://stackoverflow.com/a/4495368/689223
-  #reverse with sed     ref: http://stackoverflow.com/a/744093/689223
-  #git tags | ignore release candidates | sort versions | reverse | pick first line
-  LATEST_TAG=`git tag | grep -v rc | sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n | sed '1!G;h;$!d' | sed -n 1p`
-  echo $LATEST_TAG
-
-  if [ "$TRAVIS_TAG" == "$LATEST_TAG" ]
+  if [ "$NODE_ENV" == "production" ]
   then
-    DEPLOY_SUBDOMAIN_UNFORMATTED_LIST+=(latest)
-  fi
 
-  DEPLOY_SUBDOMAIN_UNFORMATTED_LIST+=(${TRAVIS_TAG}-tag)
+    #sorts the tags and picks the latest
+    #sort -V does not work on the travis machine
+    #sort -V              ref: http://stackoverflow.com/a/14273595/689223
+    #sort -t ...          ref: http://stackoverflow.com/a/4495368/689223
+    #reverse with sed     ref: http://stackoverflow.com/a/744093/689223
+    #git tags | ignore release candidates | sort versions | reverse | pick first line
+    LATEST_TAG=`git tag | grep -v rc | sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n | sed '1!G;h;$!d' | sed -n 1p`
+    echo $LATEST_TAG
+
+    if [ "$TRAVIS_TAG" == "$LATEST_TAG" ]
+    then
+      DEPLOY_SUBDOMAIN_UNFORMATTED_LIST+=(latest)
+    fi
+
+    DEPLOY_SUBDOMAIN_UNFORMATTED_LIST+=(${TRAVIS_TAG}-tag)
+  fi
 else
-  DEPLOY_SUBDOMAIN_UNFORMATTED_LIST+=(${TRAVIS_BRANCH}-branch)
+  if [ "$NODE_ENV" == "test" ]
+  then
+    DEPLOY_SUBDOMAIN_UNFORMATTED_LIST+=(${TRAVIS_BRANCH}-branch)
+  fi
 fi
 
 
