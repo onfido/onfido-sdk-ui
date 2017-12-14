8ea806f1aaf8374899de70e971cb3e00c643143c
Stefania Cardenas <stefania.cardenas@onfido.com>
2017-12-14 09:19:41 +0000

    Added comment re language tags

--- locales/index.js
+++ locales/index.js
@@ -1,6 +1,10 @@
 import {es} from './es'
 import {en} from './en'
 
+// Language tags should follow the IETF's BCP 47 guidelines, link below:
+//https://www.w3.org/International/questions/qa-lang-2or3
+// Generally it should be a two or three charaters tag (language) followed by a two/three characters subtag (region), if needed.
+// Example: 'en-UK'
 const locales = {
   en,
   es
