2d5eacd1044e8aba424e5e443a422726238f41e4
Stefania Cardenas <stefania.cardenas@onfido.com>
2017-12-20 16:56:42 +0000

    Spanish localisation WIP

--- locales/mobileLocales/es.js
+++ locales/mobileLocales/es.js
@@ -2,27 +2,27 @@ export const es = {
   capture: {
     driving_licence: {
       front: {
-        instructions: 'Take a photo of the front of your license',
+        instructions: 'Tome una foto del frente de su licencia',
       },
       back: {
-        instructions: 'Take a photo of the back of your license',
+        instructions: 'Tome una foto del reverso de su licencia',
       },
     },
     national_identity_card: {
       front: {
-        instructions: 'Take a photo of the front of your card',
+        instructions: 'Tome una foto del frente de su tarjeta',
       },
       back: {
-        instructions: 'Take a photo of the back of your card',
+        instructions: 'Tome una foto del reverso de su tarjeta',
       },
     },
     passport: {
       front: {
-        instructions: 'Take a photo of your passport photo page',
+        instructions: 'Tome una foto de la página del pasaporte que incluye su fotografía',
       },
     },
     face: {
-      instructions: 'Take a selfie showing your face',
+      instructions: 'Tome una selfie que muestre su cara',
     }
   }
 }
