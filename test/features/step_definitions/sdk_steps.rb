db9d6212bd0795699c411b1f2080c0212d56454e
Stefania Cardenas <stefania.cardenas@onfido.com>
2018-01-11 14:56:36 +0000

    Making first test step backward compatible

--- test/features/step_definitions/sdk_steps.rb
+++ test/features/step_definitions/sdk_steps.rb
@@ -2,7 +2,7 @@ require_relative '../helpers/i18n_helper.rb'
 
 i18n = I18nHelper.new
 
-Given(/^I verify with (passport|identity_card|drivers_license) with (es)?$/) do |document_type, locale|
+Given(/^I verify with (passport|identity_card|drivers_license)(?:| with (.+)?)$/) do |document_type, locale|
   i18n.load_locale(locale)
 
   if document_type == 'passport'
