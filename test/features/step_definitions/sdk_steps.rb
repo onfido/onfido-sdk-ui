3eaa4e4b6e9341468412fbe5fdb4b5c14c61d962
Stefania Cardenas <stefania.cardenas@onfido.com>
2018-01-11 15:53:30 +0000

    Use "should include translation for" everywhere

--- test/features/step_definitions/sdk_steps.rb
+++ test/features/step_definitions/sdk_steps.rb
@@ -6,11 +6,11 @@ Given(/^I verify with (passport|identity_card|drivers_license)(?:| with (.+)?)$/
   i18n.load_locale(locale)
 
   if document_type == 'passport'
-    title = i18n.translate('capture.passport.front.title')
+    key = 'capture.passport.front.title'
   elsif document_type == 'identity_card'
-    title = i18n.translate('capture.national_identity_card.front.title')
+    key = 'capture.national_identity_card.front.title'
   elsif document_type == 'drivers_license'
-    title = i18n.translate('capture.driving_licence.front.title')
+    key = 'capture.driving_licence.front.title'
   end
 
   steps %Q{
@@ -18,52 +18,50 @@ Given(/^I verify with (passport|identity_card|drivers_license)(?:| with (.+)?)$/
     When I click on primary_button (SDK)
     Then I should see 3 document_select_buttons ()
     When I click on #{document_type} ()
-    Then page_title () should contain "#{title}"
-    And cross_device_header () should contain "#{i18n.translate('cross_device.switch_device.header')}"
+    Then page_title should include translation for "#{key}"
+    And cross_device_header should include translation for "cross_device.switch_device.header"
   }
 end
 
 When(/^I try to upload (\w+)(?:\s*)(pdf)?( and then retry)?$/) do |document, file_type, should_retry|
   action_button = should_retry ? "take_again" : "confirm"
   if document.include?('passport') || document.include?('llama')
-    doc = i18n.translate('confirm.passport.message')
+    doc = 'confirm.passport.message'
   elsif document.include? 'identity_card'
-    doc = i18n.translate('confirm.national_identity_card.message')
+    doc = 'confirm.national_identity_card.message'
   elsif document.include?('license') || document.include?('licence')
-    doc = i18n.translate('confirm.driving_licence.message')
+    doc = 'confirm.driving_licence.message'
   end
-  face = i18n.translate('confirm.face.message')
+  face = 'confirm.face.message'
 
-  confirm_text = doc ? doc : face
+  confirm_key = doc ? doc : face
 
   steps %Q{
     When I upload #{document} #{file_type} on file_upload ()
     Then I should see uploaded_#{file_type}image ()
-    And sub_title () should contain "#{confirm_text}"
+    And sub_title should include translation for "#{confirm_key}"
     When I click on #{action_button} ()
   }
 end
 
 Then(/^I should reach the complete step$/) do
   steps %Q{
-    Then page_title () should contain "#{i18n.translate('complete.message')}"
+    Then page_title should include translation for "complete.message"
     Then page should not have back buttons
   }
 end
 
 Then(/^I can navigate back to the previous page with title "([^"]*)"$/) do | key |
-  title = i18n.translate(key)
   steps %Q{
     When I click on back ()
-    Then page_title () should contain "#{title}"
+    Then page_title should include translation for "#{key}"
   }
 end
 
 When(/^I upload my document and selfie$/) do
-  selfie = i18n.translate('capture.face.upload_title')
   steps %Q{
     When I try to upload passport
-    Then page_title () should contain "#{selfie}"
+    Then page_title should include translation for "capture.face.upload_title"
     When I try to upload one_face
   }
 end
