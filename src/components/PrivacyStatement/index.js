01c5ee36145174d45d3810a1b83a5d5d142bad33
Stefania Cardenas <stefania.cardenas@onfido.com>
2018-03-01 00:29:25 +0000

    UI WIP

--- src/components/PrivacyStatement/index.js
+++ src/components/PrivacyStatement/index.js
@@ -11,29 +11,31 @@ const PrivacyStatement = ({acceptTerms, back, i18n}) => {
     <div className={style.privacy}>
       <Title {...{title}} />
       <div className={`${theme.thickWrapper} ${style.content}`}>
-        <ul>
+        <ul className={style.list}>
           <li>{i18n.t('privacy.item_1')}</li>
           <li>{i18n.t('privacy.item_2')}</li>
           <li>{i18n.t('privacy.item_3')}</li>
         </ul>
 
-        <div className={style.smallPrint}>
-          <p>
-            {i18n.t('privacy.small_print_p1')}
-            <a href=''>{i18n.t('privacy.terms_link')}</a>
-            {i18n.t('privacy.small_print_p2')}
-            <a href=''>{i18n.t('privacy.privacy_link')}</a>
-          </p>
-        </div>
-        <div className={style.actions}>
-          <button onClick={preventDefaultOnClick(back)}
-            className={`${theme.btn} ${style["btn-outline"]}`}>
-            {i18n.t('privacy.decline')}
-          </button>
-          <button href='#' className={`${theme.btn} ${theme["btn-primary"]}`}
-            onClick={preventDefaultOnClick(acceptTerms)}>
-            {i18n.t('privacy.continue')}
-          </button>
+        <div className={style.bottomSection}>
+          <div className={style.smallPrint}>
+            <p>
+              {i18n.t('privacy.small_print_p1')}
+              <a href=''>{i18n.t('privacy.terms_link')}</a>
+              {i18n.t('privacy.small_print_p2')}
+              <a href=''>{i18n.t('privacy.privacy_link')}</a>
+            </p>
+          </div>
+          <div className={style.actions}>
+            <button onClick={preventDefaultOnClick(back)}
+              className={`${theme.btn} ${style["btn-outline"]}`}>
+              {i18n.t('privacy.decline')}
+            </button>
+            <button href='#' className={`${theme.btn} ${theme["btn-primary"]}`}
+              onClick={preventDefaultOnClick(acceptTerms)}>
+              {i18n.t('privacy.continue')}
+            </button>
+          </div>
         </div>
       </div>
     </div>
