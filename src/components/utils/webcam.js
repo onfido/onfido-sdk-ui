7d5678f0ed36fb504987d52c50ce7a983e322538
Jesus Vilar <jesus.vilar@gmail.com>
2018-06-22 14:47:52 -0300

    Add max supported camera resolution height detection

---
+++ src/components/utils/webcam.js
@@ -0,0 +1,42 @@
+import enumerateDevices from 'enumerate-devices'
+
+const enumerateDevicesInternal = (onSuccess, onError) => {
+  try {
+    enumerateDevices().then(onSuccess).catch(onError);
+  }
+  catch (exception){
+    onError(exception)
+  }
+}
+
+const checkDevicesInfo = checkFn =>
+  onResult =>
+    enumerateDevicesInternal(
+      devices => onResult(checkFn(devices)),
+      () => onResult(false)
+    )
+
+const isVideoDevice = ({ kind = '' }) => kind.includes('video')
+
+const hasDevicePermission = ({ label }) => !!label
+
+export const checkIfHasWebcam = checkDevicesInfo(
+  devices => devices.some(isVideoDevice)
+)
+
+export const checkIfWebcamPermissionGranted = checkDevicesInfo(
+  devices => devices.filter(isVideoDevice).some(hasDevicePermission)
+)
+
+const { mediaDevices = {} } = navigator;
+const { getUserMedia = () => Promise.reject() } = mediaDevices;
+const noop = () => {}
+
+export const detectWebcamMaxSupportedHeight = heights =>
+  heights.reduce((promise, height) =>
+    promise.then(maxSupportedHeight =>
+      getUserMedia.call(mediaDevices, { video: { height }, audio: false }, noop, noop)
+        .then(() => height)
+        .catch(() => Promise.resolve(maxSupportedHeight))
+    )
+  , Promise.resolve(undefined))
