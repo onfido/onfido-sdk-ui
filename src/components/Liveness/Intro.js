b80bc5ea9b532a0bcaf06e9b5f7dfde11fb45b40
Jesus Vilar <jesus.vilar@gmail.com>
2018-08-14 17:57:01 +0200

    Change flow to x-device if there is no camera

--- src/components/Liveness/Intro.js
+++ src/components/Liveness/Intro.js
@@ -1,44 +1,57 @@
 // @flow
 import * as React from 'react'
-import { h } from 'preact'
+import { h, Component } from 'preact'
 import classNames from 'classnames'
 import style from './style.css'
 import theme from '../Theme/style.css'
 import Title from '../Title'
-import {preventDefaultOnClick} from '../utils'
+import { preventDefaultOnClick, checkIfHasWebcam } from '../utils'
 import {parseI18nWithXmlTags} from '../../locales'
 import { trackComponent } from '../../Tracker'
 
 type Props = {
+  changeFlowTo: (string, ?number, ?boolean) => void,
   i18n: Object,
   nextStep: void => void,
 };
 
-const Intro = ({ i18n, nextStep }: Props) => (
-  <div className={theme.fullHeightContainer}>
-    <Title title={i18n.t('capture.liveness.intro.title')} />
-    <div className={classNames(theme.thickWrapper, style.introCopy)}>
-      <ul className={style.introBullets}>
-      {
-        ['two_actions', 'speak_out_loud'].map(key =>
-          <li key={key} className={style.introBullet}>
-            <span className={classNames(style.introIcon, style[`${key}Icon`])} />
-            { parseI18nWithXmlTags(i18n, `capture.liveness.intro.${key}`, ({ text }) => (
-               <span className={style.bolder}>{text}</span>
-            ))}
-          </li>
-        )
-      }
-      </ul>
-    </div>
-    <div className={theme.thickWrapper}>
-      <button
-        className={classNames(theme.btn, theme['btn-primary'], theme['btn-centered'])}
-        onClick={preventDefaultOnClick(nextStep)}>
-        {i18n.t('capture.liveness.intro.continue')}
-      </button>
-    </div>
-  </div>
-)
+const noop = () => {}
+
+class Intro extends Component<Props> {
+  componentDidMount() {
+    const { changeFlowTo } = this.props
+    checkIfHasWebcam(hasWebcam => !hasWebcam && changeFlowTo('crossDeviceSteps'))
+  }
+
+  render() {
+    const { i18n, nextStep } = this.props
+    return (
+      <div className={theme.fullHeightContainer}>
+        <Title title={i18n.t('capture.liveness.intro.title')} />
+        <div className={classNames(theme.thickWrapper, style.introCopy)}>
+          <ul className={style.introBullets}>
+          {
+            ['two_actions', 'speak_out_loud'].map(key =>
+              <li key={key} className={style.introBullet}>
+                <span className={classNames(style.introIcon, style[`${key}Icon`])} />
+                { parseI18nWithXmlTags(i18n, `capture.liveness.intro.${key}`, ({ text }) => (
+                   <span className={style.bolder}>{text}</span>
+                ))}
+              </li>
+            )
+          }
+          </ul>
+        </div>
+        <div className={theme.thickWrapper}>
+          <button
+            className={classNames(theme.btn, theme['btn-primary'], theme['btn-centered'])}
+            onClick={preventDefaultOnClick(nextStep)}>
+            {i18n.t('capture.liveness.intro.continue')}
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default trackComponent(Intro)
