74dee0872b41b0772881c79a25df678afb706009
Jesus Vilar <jesus.vilar@gmail.com>
2018-09-20 10:38:59 +0200

    Set default requestedVariant

--- src/components/Capture/index.js
+++ src/components/Capture/index.js
@@ -11,7 +11,7 @@ const withOptions = (WrappedComponent, additionalProps = {}) =>
 export default {
   FrontDocumentCapture: appendToTracking(withOptions(Document), 'front_capture'),
   BackDocumentCapture: appendToTracking(withOptions(Document, { side: 'back' }), 'back_capture'),
-  SelfieCapture: appendToTracking(withOptions(Face), 'selfie_capture'),
-  VideoCapture: appendToTracking(withOptions(Face, { requestedVariant: 'video'}), 'video_capture'),
+  SelfieCapture: appendToTracking(withOptions(Face, { requestedVariant: 'standard' }), 'selfie_capture'),
+  VideoCapture: appendToTracking(withOptions(Face, { requestedVariant: 'video' }), 'video_capture'),
   PoADocumentCapture: appendToTracking(withPoAGuidanceScreen(Document), 'poa'),
 }
