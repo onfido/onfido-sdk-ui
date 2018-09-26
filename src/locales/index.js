aff32f4e25f757d30f98951674cf934136440d3c
Jesus Vilar <jesus.vilar@gmail.com>
2018-09-26 16:45:37 +0200

    Improve readability of flow typing

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,5 +1,6 @@
 // @flow
 import * as React from 'react'
+import type { Node, ComponentType } from 'react'
 import { h } from 'preact'
 import { createContext } from 'preact-context'
 import { parseTags } from '../components/utils'
@@ -7,12 +8,12 @@ import initializePolyglot from './polyglot'
 
 const LocaleContext = createContext({})
 
-type Props = {
+type ProviderProps = {
   language: string,
-  children: React.Node,
+  children: Node,
 }
 
-export const LocaleProvider = ({ language, children }: Props) => {
+export const LocaleProvider = ({ language, children }: ProviderProps) => {
   const polyglot = initializePolyglot(language)
   const translate = polyglot.t.bind(polyglot)
   const parseTranslatedTags = (key, handler) => parseTags(translate(key), handler)
@@ -26,16 +27,13 @@ export const LocaleProvider = ({ language, children }: Props) => {
 
 export type LocalisedType = {
   translate: (string, ?{}) => string,
-  parseTranslatedTags: (string, { text: string } => React.Node) => React.Node,
+  parseTranslatedTags: (string, { text: string } => Node) => Node,
   language: string,
 }
 
-export const localised = <WrappedProps: *>(
-  WrappedComponent: React.ComponentType<WrappedProps>
-): React.ComponentType<{ ...LocalisedType, ...WrappedProps}> =>
-  (wrappedProps: WrappedProps) =>
+export const localised = <Props: *>(Wrapped: ComponentType<Props>): ComponentType<{...LocalisedType, ...Props}> =>
+  (props: Props) =>
     <LocaleContext.Consumer>{
-      (injectedProps: LocalisedType) => <WrappedComponent {...wrappedProps} {...injectedProps} />
+      (injectedProps: LocalisedType) => <Wrapped {...props} {...injectedProps} />
     }
     </LocaleContext.Consumer>
-
