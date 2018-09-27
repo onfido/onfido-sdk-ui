0dc7cf2a79c8ba743dbdc73897541239afc4fb41
Jesus Vilar <jesus.vilar@gmail.com>
2018-09-27 12:33:23 +0200

    Move isFullScreen to redux store

--- src/components/Theme/index.js
+++ src/components/Theme/index.js
@@ -4,11 +4,11 @@ import NavigationBar from '../NavigationBar'
 import theme from './style.css'
 
 export const themeWrap = (WrappedComponent) => (props) => {
-  const {back, isFullScreen, disableNavigation} = props
+  const {back, disableNavigation} = props
 
   return (
     <div className={theme.step}>
-      <NavigationBar {...{back, isFullScreen}} disabled={disableNavigation} className={theme.navigationBar} />
+      <NavigationBar back={back} disabled={disableNavigation} className={theme.navigationBar} />
       <div className={theme.content}><WrappedComponent {...props} /></div>
       <div className={theme.footer} />
     </div>
