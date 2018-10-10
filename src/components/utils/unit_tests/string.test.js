7b55dc0eadd03e8a44095ef6a5d0ab40d7ba51af
Bart Ziemba <Bart.Ziemba@onfido.com>
2018-10-10 11:26:59 +0100

    Update string tests

--- src/components/utils/unit_tests/string.test.js
+++ src/components/utils/unit_tests/string.test.js
@@ -1,6 +1,10 @@
 /* global expect */
-import {lowerCase} from "../string.js"
+import {lowerCase, upperCase} from "../string.js"
 
 test('Uppercase string transforms to lowercase', () => {
-    expect(lowerCase("UppERCASE")).toBe("uppercase")
+    expect(lowerCase("TOBELOWERCASE")).toBe("tobelowercase")
+});
+
+test('Lowercase string transforms to uppercase', () => {
+    expect(upperCase("tobeuppercase")).toBe("TOBEUPPERCASE")
 });
