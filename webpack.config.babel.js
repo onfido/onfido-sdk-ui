4bb9b3a482ca9a52e9807cfb98f87a2bd3db5ecc
Ricardo Freitas <ricardo.freitas@onfido.com>
2018-10-16 10:30:47 +0100

    Added conditional CSP for production only builds;
    added conditional for staging CSP web sockets cross sync link

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -245,7 +245,7 @@ const configDist = {
       disable: !PRODUCTION_BUILD
     }),
     new HtmlWebpackPlugin({
-        template: './demo/index.html',
+        template: './demo/index.ejs',
         minify: { collapseWhitespace: true },
         inject: 'body',
         JWT_FACTORY: CONFIG.JWT_FACTORY,
