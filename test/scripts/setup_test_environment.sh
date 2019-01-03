4293240469cb2605d31cd30355df048a0250cc44
Stefania Cardenas <stefania.cardenas@onfido.com>
2019-01-03 16:44:32 +0000

    testing ruby 2.3.4

--- test/scripts/setup_test_environment.sh
+++ test/scripts/setup_test_environment.sh
@@ -8,5 +8,6 @@ if [[ ${CI} == "true" && (${NODE_ENV} = "test" && ${TRAVIS_PULL_REQUEST} != "fal
   sudo ./test/scripts/install_chromedriver_travis.sh
 
   chmod 600 ~/.ssh/monster_rsa
+  rvm install 2.3.4
   gem install bundler
 fi
