f3ab9629fcc816faf97eeca01856a490e7693a7f
Stefania Cardenas <stefania.cardenas@onfido.com>
2019-01-03 18:24:43 +0000

    Test installing ruby using rvm and latest bundler version

--- test/scripts/setup_test_environment.sh
+++ test/scripts/setup_test_environment.sh
@@ -8,6 +8,16 @@ if [[ ${CI} == "true" && (${NODE_ENV} = "test" && ${TRAVIS_PULL_REQUEST} != "fal
   sudo ./test/scripts/install_chromedriver_travis.sh
 
   chmod 600 ~/.ssh/monster_rsa
-  sudo apt-get install ruby-full
-  gem install bundler -v '< 2'
+
+  # Install rvm
+  sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+  curl -sSL https://get.rvm.io | sudo bash -s stable
+  source ~/.rvm/scripts/rvm
+
+  # Install Ruby
+  rvm install ruby
+  rvm --default use ruby
+
+  # Install bundler
+  gem install bundler
 fi
