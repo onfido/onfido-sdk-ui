a0c550dd88c2fd495a9f2bc79064269f3f651571
Stefania Cardenas <stefania.cardenas@onfido.com>
2019-01-04 10:12:09 +0000

    Reverting all changes to working commit

--- test/scripts/setup_test_environment.sh
+++ test/scripts/setup_test_environment.sh
@@ -8,22 +8,6 @@ if [[ ${CI} == "true" && (${NODE_ENV} = "test" && ${TRAVIS_PULL_REQUEST} != "fal
   sudo ./test/scripts/install_chromedriver_travis.sh
 
   chmod 600 ~/.ssh/monster_rsa
-
-
-  # prepare environment for rvm installation
-  sudo apt-get update
-  sudo apt-get install -y curl gnupg build-essential
-  sudo usermod -a -G rvm `whoami`
-
-  # Install rvm
-  sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-  curl -sSL https://get.rvm.io | sudo bash -s stable
-  source ~/.rvm/scripts/rvm
-
-  # Install Ruby
-  rvm install ruby
-  rvm --default use ruby
-
-  # Install bundler
-  gem install bundler
+  sudo apt-get install ruby-full
+  gem install bundler -v '< 2'
 fi
