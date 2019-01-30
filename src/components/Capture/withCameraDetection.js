bbe4a49f7ba26092b3a909d64934f4f37f145a17
Stephen Cook <stephen.cook@onfido.com>
2019-01-30 11:54:29 +0000

    Remove camera-detection caching, as it has issues with parts of the code that forward the user's route depending on the value

--- src/components/Capture/withCameraDetection.js
+++ src/components/Capture/withCameraDetection.js
@@ -2,17 +2,10 @@ import { h } from 'preact'
 import { PureComponent } from 'preact-compat'
 import { checkIfHasWebcam } from '../utils'
 
-// It's unlikely that whether or not our device has a camera will change, so we
-// speed ourselves up slightly by assuming that the result was whatever our
-// previous value was (but we still actually check, and update the value correctly)
-// If we are incorrect, then the user will simply see a flicker of a fallback,
-// or an empty camera stream, before the correct screen once more
-let previousHasCameraCheck = null
-
 export default WrappedComponent =>
   class WithCameraDetection extends PureComponent {
     state = {
-      hasCamera: previousHasCameraCheck,
+      hasCamera: null,
     }
 
     componentDidMount(){
@@ -24,12 +17,15 @@ export default WrappedComponent =>
       clearInterval(this.cameraChecker)
     }
 
-    checkCameraSupport = () => checkIfHasWebcam(hasCamera => {
-      previousHasCameraCheck = hasCamera
-      this.setState({ hasCamera })
-    })
+    checkCameraSupport = () => checkIfHasWebcam(hasCamera => this.setState({ hasCamera }))
 
     render() {
+      const { hasCamera } = this.state
+
+      // while checking if we have a camera or not, don't render anything
+      // otherwise we'll see a flicker, after we do work out what's what
+      if (hasCamera === null) return null
+
       return <WrappedComponent {...this.props} hasCamera={this.state.hasCamera} />
     }
   }
