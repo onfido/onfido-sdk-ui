80091b4b8387720f555cc7ef542a71e4fede536a
Ricardo Freitas <ricardo.freitas@onfido.com>
2019-03-05 18:58:08 +0000

    Fixed issue where second and following scenarios would fail
    Made sure the driver and browser stack local was created every time before each scenario, rather than reused.
    This is because monster kills it (and creates it later) after every scenario.
    
    Made sure local identifier was being set on browserstack local, so that it’s aligned with the driver which has it.
    
    Removed a repeated require.
    
    Removed the ability to have multiple nodes.

--- test/features/support/env.rb
+++ test/features/support/env.rb
@@ -6,51 +6,69 @@ require 'cucumber'
 require 'parallel'
 require 'json'
 require 'browserstack/local'
-require 'browserstack-automate'
 BrowserStack.for "cucumber"
 
 SDK_URL = ENV['SDK_URL'] or raise "Missing SDK_URL environment variable"
 PRIVACY_FEATURE_ENABLED = false
 
+def start_browserstack_local
+  bs_local = BrowserStack::Local.new
+  bs_local_args = {
+    "key" => ENV['BROWSERSTACK_ACCESS_KEY'],
+    "localIdentifier" => ENV["BROWSERSTACK_LOCAL_IDENTIFIER"],
+    "force" => ""
+  }
 
-bs_local = BrowserStack::Local.new
-bs_local_args = {
-  "key" => ENV['BROWSERSTACK_ACCESS_KEY'],
-  "force" => ""
-}
-
-puts "Starting BrowserStack Local"
-puts bs_local.start(bs_local_args)
+  puts "Starting BrowserStack Local"
+  puts bs_local.start(bs_local_args)
+end
 
 @browsers = JSON.load(open('browsers.json'))
-@parallel_limit = ENV["nodes"] || 1
-@parallel_limit = @parallel_limit.to_i
 
-capabilities = Selenium::WebDriver::Remote::Capabilities.new
 
-capabilities['os'] = ENV['BS_AUTOMATE_OS']
-capabilities['os_version'] = ENV['BS_AUTOMATE_OS_VERSION']
-capabilities['browser'] = ENV['SELENIUM_BROWSER']
-capabilities['browser_version'] = ENV['SELENIUM_VERSION']
-capabilities['browserstack.debug'] = "true"
-capabilities['browserstack.local'] = "true"
-capabilities['browserstack.localIdentifier'] = ENV['BROWSERSTACK_LOCAL_IDENTIFIER']
+def create_driver
+  capabilities = Selenium::WebDriver::Remote::Capabilities.new
 
-capabilities['project'] = "JS SDK"
-capabilities['build'] = "PR"
-capabilities['name'] = "Compatibility tests"
+  capabilities['os'] = ENV['BS_AUTOMATE_OS']
+  capabilities['os_version'] = ENV['BS_AUTOMATE_OS_VERSION']
+  capabilities['browser'] = ENV['SELENIUM_BROWSER']
+  capabilities['browser_version'] = ENV['SELENIUM_VERSION']
+  capabilities['browserstack.debug'] = "true"
+  capabilities['browserstack.local'] = "true"
+  capabilities['browserstack.localIdentifier'] = ENV['BROWSERSTACK_LOCAL_IDENTIFIER']
 
-url = "http://#{ENV['BS_USERNAME']}:#{ENV['BROWSERSTACK_ACCESS_KEY']}@hub-cloud.browserstack.com/wd/hub"
+  capabilities['project'] = "JS SDK"
+  capabilities['build'] = "PR"
+  capabilities['name'] = "Compatibility tests"
 
-driver = Selenium::WebDriver.for(:remote, :url => url, :desired_capabilities => capabilities)
+  url = "http://#{ENV['BS_USERNAME']}:#{ENV['BROWSERSTACK_ACCESS_KEY']}@hub-cloud.browserstack.com/wd/hub"
 
-puts driver
+  driver = Selenium::WebDriver.for(:remote, :url => url, :desired_capabilities => capabilities)
 
-Before do |scenario|
+  # Supports uploading file to a remove runner
+  # ref: https://saucelabs.com/blog/selenium-tips-uploading-files-in-remote-webdriver
+  driver.file_detector = lambda do |args|
+     # args => ["/path/to/file"]
+     str = args.first.to_s
+     str if File.exist?(str)
+  end
+
+  return driver
+end
+
+Before('@browser') do |scenario|
+  # The driver has to be created and browser stack local has to be created everytime
+  # This only because Monster is killing both at the end of each scenario
+  puts "Before scenario"
+  start_browserstack_local()
+
+  puts @driver
+  driver = create_driver()
+  puts driver
   @driver = driver
 end
 
 at_exit do
-  driver.quit
-  bs_local.stop()
+  # driver.quit
+  # bs_local.stop()
 end
