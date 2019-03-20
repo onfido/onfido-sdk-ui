aeebb0351b63f6ac58faae8e34b81729e8aefecf
Stefania Cardenas <stefania.cardenas@onfido.com>
2019-03-20 12:21:17 +0000

    Only use websockets if websockets is supported otherwise use polling

--- src/components/utils/crossDeviceSync.js
+++ src/components/utils/crossDeviceSync.js
@@ -1,9 +1,13 @@
 import io from 'socket.io-client'
 
+const supportsWebSockets = 'WebSocket' in window || 'MozWebSocket' in window
+
+const transports = supportsWebSockets() ? ['websocket'] : ['polling']
+
 export const socketIoConfig = () =>
   io(process.env.DESKTOP_SYNC_URL, {
       path: "/v2/socket.io",
       autoConnect: false,
       upgrade: false, // default: true
-      transports: ['websocket'], // default: ['polling', 'websocket']
+      transports, // default: ['polling', 'websocket']
   })
