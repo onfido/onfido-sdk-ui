633427adff14579c20d4169eb055f0557db8ec6e
Ricardo Freitas <ricardo.freitas@onfido.com>
2019-04-02 13:09:07 +0100

    moved the woopra window key to a literal constant

--- src/Tracker/safeWoopra.js
+++ src/Tracker/safeWoopra.js
@@ -16,11 +16,8 @@ function SafeWindow () {
 }
 SafeWindow.prototype = window;
 
-
-const windowKey = "onfidoSafeWindow8xmy484y87m239843m20";
-const safeWindow = window[windowKey] = new SafeWindow()
-//Webpack needs the import to a literal string, it cannot be resolved at runtime, webpack needs a preprocessor
-require("imports-loader?this=>onfidoSafeWindow8xmy484y87m239843m20,window=>onfidoSafeWindow8xmy484y87m239843m20!wpt/wpt.min.js")
-delete window[windowKey]
+const safeWindow = window[process.env.WOOPRA_WINDOW_KEY] = new SafeWindow()
+require(`imports-loader?this=>${process.env.WOOPRA_WINDOW_KEY},window=>${process.env.WOOPRA_WINDOW_KEY}!wpt/wpt.min.js`)
+delete window[process.env.WOOPRA_WINDOW_KEY]
 
 export default safeWindow.WoopraTracker
