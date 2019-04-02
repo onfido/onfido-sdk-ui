c5d8e8b267d646d6cd0ccfaa18b07e89a7d32727
Ricardo Freitas <ricardo.freitas@onfido.com>
2019-04-02 16:00:55 +0100

    added comments explaining on safe woopra is meant to work

--- src/Tracker/safeWoopra.js
+++ src/Tracker/safeWoopra.js
@@ -1,3 +1,10 @@
+/*
+Safe window is a proxy object of window
+It has the same functionality as window,
+however changing its properties won't affect
+the global window object.
+*/
+
 function SafeWindow () {
   Object.keys(window).forEach(key => {
     Object.defineProperty(this, key,
@@ -16,8 +23,14 @@ function SafeWindow () {
 }
 SafeWindow.prototype = Window.prototype;
 
+// We create a global instance of safe window, so that imports-loader
+// Can refer to it
 const safeWindow = window[process.env.WOOPRA_WINDOW_KEY] = new SafeWindow()
+
+//The goal is to import Woopra in such a way that it doesn't pollute the global window, hence why we pass an instance of SafeWindow to imports-loader
 require(`imports-loader?this=>${process.env.WOOPRA_WINDOW_KEY},window=>${process.env.WOOPRA_WINDOW_KEY}!wpt/wpt.min.js`)
+
+// We delete the global reference since imports-loader no longer needs it
 delete window[process.env.WOOPRA_WINDOW_KEY]
 
 export default safeWindow.WoopraTracker
