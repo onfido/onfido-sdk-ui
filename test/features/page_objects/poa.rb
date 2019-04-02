66c247c3d677ba41c723a34d3bf68817a54bb0ae
Bart Ziemba <Bart.Ziemba@onfido.com>
2019-04-02 16:01:51 +0100

    Test navigation to the document selection screen

--- test/features/page_objects/poa.rb
+++ test/features/page_objects/poa.rb
@@ -10,4 +10,28 @@ class POA
     @driver.find_element(:xpath, "//span[contains(.,'Let’s verify your UK address')]")
   end
 
+  def need_a_doc_text
+    @driver.find_element(:xpath, "//p[contains(.,'You’ll need a document that:')]")
+  end
+
+  def bullet_1
+    @driver.find_element(:xpath, "//span[contains(.,'Shows your current address')]")
+  end
+
+  def bullet_2
+    @driver.find_element(:xpath, "//span[contains(.,'Matches the address you used on signup')]")
+  end
+
+  def bullet_3
+    @driver.find_element(:xpath, "//span[contains(.,'Is your most recent document')]")
+  end
+
+  def start_verification_button
+    @driver.find_element(:xpath, "//button[contains(.,'Start verification')]")
+  end
+
+  def document_selection_screen_title
+    @driver.find_element(:xpath, "//span[@class='onfido-sdk-ui-Title-titleSpan'][contains(.,'Select a UK document')]")
+  end
+
 end
--- test/features/proof_of_address.feature
+++ test/features/proof_of_address.feature
@@ -1,7 +1,17 @@
 @browser
 Feature: SDK proof of address
 
-  Scenario: Test cross device flow
+  Scenario: Test presence of the UI elements of the second screen of the PoA flow
     Given I navigate to the SDK with PoA feature enabled
     When I click on primary_button (SDK)
     Then I should see verify_uk_address_title (POA)
+    Then I should see need_a_doc_text (POA)
+    Then I should see bullet_1 (POA)
+    Then I should see bullet_2 (POA)
+    Then I should see bullet_3 (POA)
+
+  Scenario: Test navigation to the document selection screen
+    Given I navigate to the SDK with PoA feature enabled
+    When I click on primary_button (SDK)
+    When I click on start_verification_button (POA)
+    Then I should see document_selection_screen_title (POA)
