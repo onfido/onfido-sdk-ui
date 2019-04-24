c71bbe38b3771ce3fdf45f1fafb6bd54d0cc59a7
Bart Ziemba <Bart.Ziemba@onfido.com>
2019-04-24 17:11:44 +0100

    Remove empty line

--- test/js/pageobjects/Welcome.js
+++ test/js/pageobjects/Welcome.js
@@ -6,7 +6,6 @@ class WelcomeScreen extends Base{
     get welcomeSubtitle() { return this.$('.onfido-sdk-ui-Welcome-text')}
     get footer() { return this.$('.onfido-sdk-ui-Theme-footer')}
     get primaryBtn() { return this.$('.onfido-sdk-ui-Button-button')}
-
 }
 
 export default WelcomeScreen;
