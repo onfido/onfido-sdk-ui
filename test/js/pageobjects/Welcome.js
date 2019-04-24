4b0ab7c0075ec3bbe6ad53e1af50fa24d7b28cd3
Bart Ziemba <Bart.Ziemba@onfido.com>
2019-04-24 12:31:55 +0100

    JS UI tests for Welcome screen

--- test/js/pageobjects/Welcome.js
+++ test/js/pageobjects/Welcome.js
@@ -2,6 +2,10 @@ import Base from './BasePage.js'
 
 class WelcomeScreen extends Base{
     getPrimaryBtn() { return this.$('.onfido-sdk-ui-Button-button'); }
+    getWelcomeTitle() { return this.$('.onfido-sdk-ui-Title-titleSpan'); }
+    getWelcomeSubtitle() { return this.$('.onfido-sdk-ui-Welcome-text'); }
+    getPrimaryBtn() { return this.$('.onfido-sdk-ui-Button-button'); }
+    getFooter() { return this.$('.onfido-sdk-ui-Theme-footer'); }
 }
 
 export default WelcomeScreen;
--- test/js/specs/happy.js
+++ test/js/specs/happy.js
@@ -5,12 +5,47 @@ const options = {
   pageObjects: ['DocumentSelection', 'Welcome', 'DocumentUpload']
 }
 
+const localhostUrl = 'https://localhost:8080/'
+
 describe('Happy Paths',options, ({driver,pageObjects}) => {
   const {documentSelection, welcome, documentUpload} = pageObjects
 
+  //Welcome Screen Tests
+  it('test website title', async () => {
+    driver.get(localhostUrl)
+    const title = driver.getTitle();
+    expect(title).to.equal('Onfido SDK Demo');
+  })
+
+  it('test welcome screen title', async () => {
+    driver.get(localhostUrl)
+    const welcomeTitleText = welcome.getWelcomeTitle.getText()
+    expect(welcomeTitleText).to.equal('Open your new bank account');
+    const welcomeTitle = welcome.getWelcomeTitle.isDisplayed()
+  })
+
+  it('test welcome screen subtitle', async () => {
+    driver.get(localhostUrl)
+    const welcomeSubtitleText = welcome.getWelcomeSubtitle.getText()
+    expect(welcomeSubtitleText).to.equal('To open a bank account, we will need to verify your identity.' + '\n' + 'It will only take a couple of minutes.');
+    const welcomeSubtitle = welcome.getWelcomeSubtitle.isDisplayed()
+  })
+
+  it('test verify identity button', async () => {
+    driver.get(localhostUrl)
+    const verifyIdentityBtnText = welcome.getPrimaryBtn.getText()
+    expect(verifyIdentityBtnText).to.equal('Verify Identity');
+    const verifyIdentity = welcome.getPrimaryBtn.isDisplayed()
+  })
+
+  it('test footer is displayed', async () => {
+    driver.get(localhostUrl)
+    const footer = welcome.getFooter.isDisplayed()
+  })
+
+  //Document upload screen
   it('should upload a file', async () => {
-    console.log("testing")
-    driver.get('https://localhost:8080/?async=false')
+    driver.get(localhostUrl)
     welcome.getPrimaryBtn().click()
     documentSelection.getPassport().click()
     const input = documentUpload.getUploadInput()
