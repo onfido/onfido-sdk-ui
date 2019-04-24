a57f04ea7a3649fda5ee7aa78cbd05ac189e887e
Bart Ziemba <Bart.Ziemba@onfido.com>
2019-04-24 16:22:37 +0100

    Fix class for primaryBtn

--- test/js/pageobjects/Welcome.js
+++ test/js/pageobjects/Welcome.js
@@ -5,7 +5,7 @@ class WelcomeScreen extends Base{
     get welcomeTitle() { return this.$('.onfido-sdk-ui-Title-titleSpan')}
     get welcomeSubtitle() { return this.$('.onfido-sdk-ui-Welcome-text')}
     get footer() { return this.$('.onfido-sdk-ui-Theme-footer')}
-    get primaryBtn() { return this.$('.onfido-sdk-ui-Theme-footer')}
+    get primaryBtn() { return this.$('.onfido-sdk-ui-Button-button')}
 
 }
 
