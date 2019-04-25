b3b3ae773a522ac50c9b8a3524cb15ca943c3cd1
Julius Osokinas <julius.osokinas@onfido.com>
2019-04-25 16:49:57 +0100

    Convert "dismiss" to button for it to be clicklable

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -20,7 +20,7 @@ const Error = ({className, error, translate, withArrow, renderMessage = identity
           {renderInstruction(translate(instruction))}
         </span>
       </p>
-      { isDismissible && <span className={style.dismiss} onClick={onDismiss} /> }
+      { isDismissible && <button className={style.dismiss} onClick={onDismiss} /> }
     </div>
   )
 }
