5d52727e0ee631767912a7832835ba00f206d4b3
Bart Ziemba <Bart.Ziemba@onfido.com>
2019-04-25 14:57:07 +0100

    Remove assignments.

--- test/js/specs/happy.js
+++ test/js/specs/happy.js
@@ -21,23 +21,23 @@ describe('Happy Paths',options, ({driver,$,pageObjects}) => {
   it('test welcome screen title', async () => {
     const welcomeTitleText = await welcome.welcomeTitle.getText()
     expect(welcomeTitleText).to.equal('Open your new bank account');
-    const welcomeTitle = await welcome.welcomeTitle.isDisplayed()
+    await welcome.welcomeTitle.isDisplayed()
   })
 
   it('test welcome screen subtitle', async () => {
     const welcomeSubtitleText = await welcome.welcomeSubtitle.getText()
     expect(welcomeSubtitleText).to.equal('To open a bank account, we will need to verify your identity.' + '\n' + 'It will only take a couple of minutes.');
-    const welcomeSubtitle = await welcome.welcomeSubtitle.isDisplayed()
+    await welcome.welcomeSubtitle.isDisplayed()
   })
 
   it('test verify identity button', async () => {
     const verifyIdentityBtnText = await welcome.primaryBtn.getText()
     expect(verifyIdentityBtnText).to.equal('Verify Identity');
-    const verifyIdentity = await welcome.primaryBtn.isDisplayed()
+    await welcome.primaryBtn.isDisplayed()
   })
 
   it('test footer is displayed', async () => {
-    const footer = await welcome.footer.isDisplayed()
+    await welcome.footer.isDisplayed()
   })
 
   //Document upload screen
