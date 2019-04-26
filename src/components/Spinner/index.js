2a7f9bcf0e0c64e287688114bc29566c5d4cf444
Stefania Cardenas <stefania.cardenas@onfido.com>
2019-04-26 17:34:31 +0100

    Added localised strings

--- src/components/Spinner/index.js
+++ src/components/Spinner/index.js
@@ -1,10 +1,12 @@
 import { h } from 'preact'
 import style from './style.css'
+import { compose } from '../utils/func'
+import {localised} from '../../locales'
 
-const Spinner = () => {
+const Spinner = ({translate}) => {
   return (
     <div className={style.loader}>
-      <span className={style.accessibleSpinnerCopy} role="heading" aria-level="1">Loading...</span>
+      <span className={style.accessibleSpinnerCopy} role="heading" aria-level="1">{translate('loading')}</span>
       <div className={style.inner}>
         <div></div>
         <div></div>
@@ -14,4 +16,4 @@ const Spinner = () => {
   )
 }
 
-export default Spinner
+export default compose(localised)(Spinner)
