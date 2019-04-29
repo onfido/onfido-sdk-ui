dff34ee2bd74bfcacd55ed7a3b65889bb2da3bab
Stefania Cardenas <stefania.cardenas@onfido.com>
2019-04-29 16:03:53 +0100

    Made changes to spinner

--- src/components/Spinner/index.js
+++ src/components/Spinner/index.js
@@ -5,8 +5,8 @@ import {localised} from '../../locales'
 
 const Spinner = ({translate}) => {
   return (
-    <div className={style.loader}>
-      <span className={style.accessibleSpinnerCopy} role="heading" aria-level="1">{translate('loading')}</span>
+    <div className={style.loader} aria-live="polite" tabindex="-1"
+      autoFocus aria-label={translate('loading')}>
       <div className={style.inner}>
         <div></div>
         <div></div>
