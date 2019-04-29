c7f7c13a30e558d719773943bec68adaea1f5cf5
Joao Trindade <trindade.joao@gmail.com>
2019-04-29 12:56:21 +0100

    Uses different `descripe` scopes per check

--- test/js/specs/happy.js
+++ test/js/specs/happy.js
@@ -11,43 +11,46 @@ const localhostUrl = 'https://localhost:8080/'
 describe('Happy Paths', options, ({driver,$,pageObjects}) => {
   const {documentSelection, welcome, documentUpload} = pageObjects
 
-  //Welcome Screen Tests
-  const copy = welcome.copy()
 
-  it('test website title', async () => {
-    await driver.get(localhostUrl)
-    const title = driver.getTitle()
-    expect(title).to.equal('Onfido SDK Demo')
+  describe('welcome screen', function () {
+    const copy = welcome.copy()
+
+    it('test website title', async () => {
+      await driver.get(localhostUrl)
+      const title = driver.getTitle()
+      expect(title).to.equal('Onfido SDK Demo')
+    })
+
+    it('test welcome screen title', async () => {
+      const welcomeTitleText = welcome.welcomeTitle.getText()
+      expect(welcomeTitleText).to.equal(copy["title"])
+      welcome.welcomeTitle.isDisplayed()
+    })
+
+    it('test welcome screen subtitle', async () => {
+      const welcomeSubtitleText = welcome.welcomeSubtitle.getText()
+      expect(welcomeSubtitleText).to.equal(copy["description_p_1"] + "\n" + copy["description_p_2"])
+      welcome.welcomeSubtitle.isDisplayed()
+    })
+
+    it('test verify identity button', async () => {
+      const verifyIdentityBtnText = welcome.primaryBtn.getText()
+      expect(verifyIdentityBtnText).to.equal(copy["next_button"])
+      welcome.primaryBtn.isDisplayed()
+    })
+
+    it('test footer is displayed', async () => {
+      welcome.footer.isDisplayed()
+    })
   })
 
-  it('test welcome screen title', async () => {
-    const welcomeTitleText = welcome.welcomeTitle.getText()
-    expect(welcomeTitleText).to.equal(copy["title"])
-    welcome.welcomeTitle.isDisplayed()
-  })
-
-  it('test welcome screen subtitle', async () => {
-    const welcomeSubtitleText = welcome.welcomeSubtitle.getText()
-    expect(welcomeSubtitleText).to.equal(copy["description_p_1"] + "\n" + copy["description_p_2"])
-    welcome.welcomeSubtitle.isDisplayed()
-  })
-
-  it('test verify identity button', async () => {
-    const verifyIdentityBtnText = welcome.primaryBtn.getText()
-    expect(verifyIdentityBtnText).to.equal(copy["next_button"])
-    welcome.primaryBtn.isDisplayed()
-  })
-
-  it('test footer is displayed', async () => {
-    welcome.footer.isDisplayed()
-  })
-
-  //Document upload screen
-  it('should upload a file', async () => {
-    driver.get(localhostUrl)
-    welcome.getPrimaryBtn().click()
-    documentSelection.getPassport().click()
-    const input = documentUpload.getUploadInput()
-    input.sendKeys(path.join(__dirname,'../../features/helpers/resources/passport.jpg'))
+  describe('upload screen', function () {
+    it('should upload a file', async () => {
+      driver.get(localhostUrl)
+      welcome.getPrimaryBtn().click()
+      documentSelection.getPassport().click()
+      const input = documentUpload.getUploadInput()
+      input.sendKeys(path.join(__dirname, '../../features/helpers/resources/passport.jpg'))
+    })
   })
 })
