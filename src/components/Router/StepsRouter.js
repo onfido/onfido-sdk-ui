b02ee45fa65435f5405791f54612d05755d1bc2e
Julius Osokinas <julius.osokinas@onfido.com>
2019-04-30 11:01:44 +0100

    Reset main container's focus when step changes

--- src/components/Router/StepsRouter.js
+++ src/components/Router/StepsRouter.js
@@ -7,6 +7,14 @@ import theme from '../Theme/style.css'
 import { withFullScreenState } from '../FullScreen'
 
 class StepsRouter extends Component {
+  componentDidUpdate(prevProps) {
+    // When step changes, we need to refocus to content in order for correct
+    // keyboard tabbing, e.g. so that 1st tab would hit the button within the
+    // content but navigation button(s) would still keep the right order.
+    if (this.content && prevProps.step !== this.props.step) {
+      this.content.focus()
+    }
+  }
 
   trackScreen = (screenNameHierarchy, properties = {}) => {
     const { step } = this.currentComponent()
@@ -25,12 +33,16 @@ class StepsRouter extends Component {
 
     return (
       //TODO: Wrap CurrentComponent in themeWrap HOC
-      <div className={classNames(theme.step,{[theme.fullScreenStep]: isFullScreen})}>
-        <NavigationBar back={back} disabled={disableNavigation} className={theme.navigationBar}/>
-        <div className={classNames(theme.content,{
-          [theme.fullScreenContentWrapper]: isFullScreen,
-          [theme.scrollableContent]: !isFullScreen
-        })}>
+      <div className={classNames(theme.step, {[theme.fullScreenStep]: isFullScreen})}>
+        <NavigationBar back={back} disabled={disableNavigation} className={theme.navigationBar} />
+        <div
+          ref={node => this.content = node}
+          tabIndex={-1}
+          className={classNames(theme.content, {
+            [theme.fullScreenContentWrapper]: isFullScreen,
+            [theme.scrollableContent]: !isFullScreen
+          })}
+        >
           <CurrentComponent {...{...options, ...globalUserOptions, ...otherProps, back}}
             trackScreen={this.trackScreen} />
         </div>
