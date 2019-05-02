27e0ad2f12d53436c6f702e87631f7102f1c03b0
Bart Ziemba <Bart.Ziemba@onfido.com>
2019-05-02 19:58:41 +0100

    Change selector for subtitle

--- test/js/pageobjects/DocumentSelection.js
+++ test/js/pageobjects/DocumentSelection.js
@@ -4,7 +4,7 @@ class DocumentSelection extends Base{
     getTitle() { return this.$('.onfido-sdk-ui-PageTitle-titleSpan'); }
     getPassport() { return this.$('.onfido-sdk-ui-DocumentSelector-icon-passport')}
     get title() { return this.$('.onfido-sdk-ui-PageTitle-titleSpan')}
-    get subtitle() { return this.$('.onfido-sdk-ui-Title-titleWrapper > div:nth-child(2)')}
+    get subtitle() { return this.$('div#onfido-mount div.gs_copied')}
     get passportIcon() { return this.$('.onfido-sdk-ui-DocumentSelector-icon-passport')}
     get documentSelectionLabel() { return this.$('.onfido-sdk-ui-DocumentSelector-label')}
     get documentSelectionHint() { return this.$('.onfido-sdk-ui-DocumentSelector-hint')}
