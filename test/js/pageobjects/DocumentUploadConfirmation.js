e01e5f7923b6921ee53ef2bbae7d8c6e11654e32
Bart Ziemba <Bart.Ziemba@onfido.com>
2019-05-02 21:42:05 +0100

    Subtitle selector

--- test/js/pageobjects/DocumentUploadConfirmation.js
+++ test/js/pageobjects/DocumentUploadConfirmation.js
@@ -2,7 +2,7 @@ import Base from './BasePage.js'
 
 class DocumentUploadConfirmation extends Base{
 
-  get makeSureClearDetailsMessage() { return this.$('div#onfido-mount div.gs_copied')}
+  get makeSureClearDetailsMessage() { return this.$('.onfido-sdk-ui-Title-titleWrapper div:nth-of-type(2)')}
   get redoBtn() { return this.$('.onfido-sdk-ui-Confirm-retake')}
   get confirmBtn() { return this.$('.onfido-sdk-ui-Confirm-btn-primary')}
   get errorTitleText() { return this.$('div#onfido-mount span.onfido-sdk-ui-Error-title-text')}
