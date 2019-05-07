ac38fba8a1ceab64facd00c1bb9dd205b6ccf31a
Bart Ziemba <Bart.Ziemba@onfido.com>
2019-05-07 13:05:18 +0100

    Change CSS selector for verification complete thank you message

--- test/js/pageobjects/VerificationComplete.js
+++ test/js/pageobjects/VerificationComplete.js
@@ -4,7 +4,7 @@ class VerificationComplete extends Base{
 
   get verificationCompleteIcon() { return this.$('.onfido-sdk-ui-Theme-icon')}
   get verificationCompleteMessage() { return this.$('.onfido-sdk-ui-PageTitle-titleSpan')}
-  get verificationCompleteThankYou() { return this.$('.onfido-sdk-ui-Title-titleWrapper > div:nth-child(2)')}
+  get verificationCompleteThankYou() { return this.$('.onfido-sdk-ui-Title-titleWrapper div:nth-of-type(2)')}
 }
 
 export default VerificationComplete;
