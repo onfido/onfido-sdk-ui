b3ac4a5afdb215d0245c36133cd04f712f393cf3
Bart Ziemba <Bart.Ziemba@onfido.com>
2019-05-09 11:13:45 +0100

    Update CSS selector for verificationCompleteThankYou

--- test/js/pageobjects/VerificationComplete.js
+++ test/js/pageobjects/VerificationComplete.js
@@ -4,7 +4,7 @@ class VerificationComplete extends Base{
 
   get verificationCompleteIcon() { return this.$('.onfido-sdk-ui-Theme-icon')}
   get verificationCompleteMessage() { return this.$('.onfido-sdk-ui-Title-titleSpan')}
-  get verificationCompleteThankYou() { return this.$('.onfido-sdk-ui-PageTitle-titleWrapper > div:nth-child(2)')}
+  get verificationCompleteThankYou() { return this.$('.onfido-sdk-ui-Title-titleWrapper > div:nth-child(2)')}
 }
 
 export default VerificationComplete;
