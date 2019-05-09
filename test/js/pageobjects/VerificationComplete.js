15aac35658cf8f9e6e9e719f1b1ae43e0d2123ed
Bart Ziemba <Bart.Ziemba@onfido.com>
2019-05-09 14:38:00 +0100

    Previous selectors

--- test/js/pageobjects/VerificationComplete.js
+++ test/js/pageobjects/VerificationComplete.js
@@ -3,8 +3,8 @@ import Base from './BasePage.js'
 class VerificationComplete extends Base{
 
   get verificationCompleteIcon() { return this.$('.onfido-sdk-ui-Theme-icon')}
-  get verificationCompleteMessage() { return this.$('.onfido-sdk-ui-Title-titleSpan')}
-  get verificationCompleteThankYou() { return this.$('.onfido-sdk-ui-Title-titleWrapper > div:nth-child(2)')}
+  get verificationCompleteMessage() { return this.$('.onfido-sdk-ui-PageTitle-titleSpan')}
+  get verificationCompleteThankYou() { return this.$('.onfido-sdk-ui-PageTitle-titleWrapper > div:nth-child(2)')}
 }
 
 export default VerificationComplete;
