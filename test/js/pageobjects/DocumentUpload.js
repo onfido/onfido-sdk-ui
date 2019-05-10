157e4b86bdfabb74072bd12bab4d8b728f246a92
Bart Ziemba <Bart.Ziemba@onfido.com>
2019-05-10 11:25:11 +0100

    Change CSS selector for uploaderIcon

--- test/js/pageobjects/DocumentUpload.js
+++ test/js/pageobjects/DocumentUpload.js
@@ -7,7 +7,7 @@ class DocumentUpload extends Base{
     get crossDeviceHeader() { return this.$('.onfido-sdk-ui-crossDevice-SwitchDevice-header')}
     get crossDeviceSubMessage() { return this.$('.onfido-sdk-ui-crossDevice-SwitchDevice-submessage')}
     get crossDeviceArrow() { return this.$('.onfido-sdk-ui-crossDevice-SwitchDevice-chevron')}
-    get uploaderIcon() { return this.$('div#onfido-mount div.onfido-sdk-ui-Uploader-instructions > span')}
+    get uploaderIcon() { return this.$('.onfido-sdk-ui-Theme-icon')}
     get uploaderInstructionsMessage() { return this.$('.onfido-sdk-ui-Uploader-instructionsCopy')}
     get uploaderBtn() { return this.$('.onfido-sdk-ui-Uploader-buttons')}
     get uploaderError() { return this.$('.onfido-sdk-ui-Uploader-error')}
