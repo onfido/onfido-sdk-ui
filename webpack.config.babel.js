98ef7f0c1c8ffcf4d1a1a7567540b0dbd8e58b8a
Joao Trindade <trindade.joao@gmail.com>
2019-05-21 13:08:14 +0100

    Update urls to backend services
    With our k8s cluster update we now have new urls in dev.
    This commit updates the urls to the new format.

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -103,29 +103,29 @@ const PROD_CONFIG = {
   'ONFIDO_TERMS_URL': 'https://onfido.com/termsofuse',
   'ONFIDO_PRIVACY_URL': 'https://onfido.com/privacy',
   'JWT_FACTORY': 'https://token-factory.onfido.com/sdk_token',
-  'DESKTOP_SYNC_URL' : 'https://sync.onfido.com',
-  'MOBILE_URL' : 'https://id.onfido.com',
+  'DESKTOP_SYNC_URL': 'https://sync.onfido.com',
+  'MOBILE_URL': 'https://id.onfido.com',
   'SMS_DELIVERY_URL': 'https://telephony.onfido.com',
-  'PUBLIC_PATH' : `https://assets.onfido.com/web-sdk-releases/${packageJson.version}/`,
+  'PUBLIC_PATH': `https://assets.onfido.com/web-sdk-releases/${packageJson.version}/`,
   'RESTRICTED_XDEVICE_FEATURE_ENABLED': true,
   WOOPRA_DOMAIN
 }
 
 const TEST_CONFIG = { ...PROD_CONFIG,
-  PUBLIC_PATH: '/', 'MOBILE_URL' : '/',
+  PUBLIC_PATH: '/', 'MOBILE_URL': '/',
   'RESTRICTED_XDEVICE_FEATURE_ENABLED': false,
   'WOOPRA_DOMAIN': WOOPRA_DEV_DOMAIN
 }
 
 const STAGING_CONFIG = {
-  'ONFIDO_API_URL': 'https://apidev.onfido.com',
-  'ONFIDO_SDK_URL': 'https://sdk-staging.onfido.com',
+  'ONFIDO_API_URL': 'https://api.eu-west-1.dev.onfido.xyz',
+  'ONFIDO_SDK_URL': 'https://mobile-sdk.eu-west-1.dev.onfido.xyz',
   'ONFIDO_TERMS_URL': 'https://dev.onfido.com/termsofuse',
   'ONFIDO_PRIVACY_URL': 'https://dev.onfido.com/privacy',
-  'JWT_FACTORY': 'https://token-factory-dev.onfido.com/sdk_token',
-  'DESKTOP_SYNC_URL': 'https://sync-dev.onfido.com',
+  'JWT_FACTORY': 'https://sdk-token-factory.eu-west-1.dev.onfido.xyz/sdk_token',
+  'DESKTOP_SYNC_URL': 'https://cross-device-sync.eu-west-1.dev.onfido.xyz',
   'MOBILE_URL': '/',
-  'SMS_DELIVERY_URL': 'https://telephony-dev.onfido.com',
+  'SMS_DELIVERY_URL': 'https://telephony.eu-west-1.dev.onfido.xyz',
   'PUBLIC_PATH': '/',
   'RESTRICTED_XDEVICE_FEATURE_ENABLED': true,
   'WOOPRA_DOMAIN': WOOPRA_DEV_DOMAIN
