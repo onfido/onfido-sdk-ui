2686a3b48141ae4719a630782c8d4d9b791f9162
Bart Ziemba <Bart.Ziemba@onfido.com>
2019-07-01 09:41:22 +0100

    Add page objects for PoA document selection screen

---
+++ test/js/pageobjects/PoADocumentSelection.js
@@ -0,0 +1,20 @@
+import Base from './BasePage.js'
+import {locale, verifyElementCopy} from '../utils/mochaw'
+
+class PoADocumentSelection extends Base{
+  get poaDocumentSelectionTitle() { return this.$('.onfido-sdk-ui-PageTitle-titleSpan')}
+  get poaDocumentSelectionSubtitle() { return this.$('.onfido-sdk-ui-PageTitle-titleWrapper > div:nth-child(2)')}
+
+  copy(lang) { return locale(lang) }
+
+  async verifyPoaDocumentSelectionTitle(copy) {
+    verifyElementCopy(this.poaDocumentSelectionTitle, copy)
+  }
+
+  async verifyPoaDocumentSelectionSubtitle(copy) {
+    const poaDocumentSelectionStrings = copy.proof_of_address
+    verifyElementCopy(this.poaDocumentSelectionSubtitle, poaDocumentSelectionStrings.hint)
+  }
+}
+
+export default PoADocumentSelection;
