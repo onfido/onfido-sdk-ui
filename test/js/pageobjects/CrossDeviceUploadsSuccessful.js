6f9bed36add1558f1fd83e921bdbf56e389057f8
Bart Ziemba <Bart.Ziemba@onfido.com>
2019-07-02 16:06:00 +0100

    Clean up for CrossDeviceUploadsSuccessful class

--- test/js/pageobjects/CrossDeviceUploadsSuccessful.js
+++ test/js/pageobjects/CrossDeviceUploadsSuccessful.js
@@ -3,19 +3,19 @@ import {locale, verifyElementCopy} from '../utils/mochaw'
 
 class CrossDeviceUploadsSuccessful extends Base {
 
-  get uploadsSuccessfulTitle() { return this.$('.onfido-sdk-ui-PageTitle-titleSpan')}
-  get uploadsSuccessfulSubtitle() { return this.$('.onfido-sdk-ui-PageTitle-subTitle')}
-  get uploadsSuccessfulIcon() { return this.$('.onfido-sdk-ui-Theme-icon')}
-  get uploadsSuccessfulFewSecondsToUpdateMessage() { return this.$('.onfido-sdk-ui-crossDevice-ClientSuccess-text')}
+  get title() { return this.$('.onfido-sdk-ui-PageTitle-titleSpan')}
+  get subtitle() { return this.$('.onfido-sdk-ui-PageTitle-subTitle')}
+  get icon() { return this.$('.onfido-sdk-ui-Theme-icon')}
+  get fewSecondsToUpdateMessage() { return this.$('.onfido-sdk-ui-crossDevice-ClientSuccess-text')}
 
   copy(lang) { return locale(lang) }
 
-  async verifyCrossDeviceUploadsSuccessfulUIElements(copy) {
+  async verifyUIElements(copy) {
     const uploadsSuccessfulScreenStrings = copy.cross_device.client_success
-    verifyElementCopy(this.uploadsSuccessfulTitle, uploadsSuccessfulScreenStrings.title)
-    verifyElementCopy(this.uploadsSuccessfulSubtitle, uploadsSuccessfulScreenStrings.sub_title)
-    this.uploadsSuccessfulIcon.isDisplayed()
-    verifyElementCopy(this.uploadsSuccessfulFewSecondsToUpdateMessage, uploadsSuccessfulScreenStrings.body)
+    verifyElementCopy(this.title, uploadsSuccessfulScreenStrings.title)
+    verifyElementCopy(this.subtitle, uploadsSuccessfulScreenStrings.sub_title)
+    this.icon.isDisplayed()
+    verifyElementCopy(this.fewSecondsToUpdateMessage, uploadsSuccessfulScreenStrings.body)
   }
 }
 
