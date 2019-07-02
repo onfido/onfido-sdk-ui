283a06d630a283510c1933aa02ddea5648c946c1
Bart Ziemba <Bart.Ziemba@onfido.com>
2019-07-02 14:46:02 +0100

    Clean up for CrossDeviceEverythingWeNeed class

--- test/js/pageobjects/CrossDeviceEverythingWeNeed.js
+++ test/js/pageobjects/CrossDeviceEverythingWeNeed.js
@@ -3,25 +3,25 @@ import {locale, verifyElementCopy} from '../utils/mochaw'
 
 class CrossDeviceEverythingWeNeed extends Base {
 
-  get crossDeviceEverythingWeNeedTitle() { return this.$('.onfido-sdk-ui-PageTitle-titleSpan')}
-  get crossDeviceEverythingWeNeedSubTitle() { return this.$('.onfido-sdk-ui-PageTitle-subTitle')}
-  get crossDeviceEverythingWeNeedDocumentUploadedMessage() { return this.$('li:nth-child(1) > .onfido-sdk-ui-crossDevice-CrossDeviceSubmit-listText')}
-  get crossDeviceEverythingWeNeedVideoUploadedMessage() { return this.$('li:nth-child(2) > .onfido-sdk-ui-crossDevice-CrossDeviceSubmit-listText')}
-  get crossDeviceEverythingWeNeedSubmitVerificationButton() { return this.$('.onfido-sdk-ui-Button-button-text')}
+  get title() { return this.$('.onfido-sdk-ui-PageTitle-titleSpan')}
+  get subtitle() { return this.$('.onfido-sdk-ui-PageTitle-subTitle')}
+  get documentUploadedMessage() { return this.$('li:nth-child(1) > .onfido-sdk-ui-crossDevice-CrossDeviceSubmit-listText')}
+  get videoUploadedMessage() { return this.$('li:nth-child(2) > .onfido-sdk-ui-crossDevice-CrossDeviceSubmit-listText')}
+  get submitVerificationButton() { return this.$('.onfido-sdk-ui-Button-button-text')}
 
   copy(lang) { return locale(lang) }
 
-  async verifyCrossDeviceEverythingWeNeedUIElements(copy) {
+  async verifyUIElements(copy) {
     const crossDeviceEverythingWeNeedtrings = copy.cross_device.submit
-    verifyElementCopy(this.crossDeviceEverythingWeNeedTitle, crossDeviceEverythingWeNeedtrings.title)
-    verifyElementCopy(this.crossDeviceEverythingWeNeedSubTitle, crossDeviceEverythingWeNeedtrings.sub_title)
-    verifyElementCopy(this.crossDeviceEverythingWeNeedDocumentUploadedMessage, crossDeviceEverythingWeNeedtrings.one_doc_uploaded)
-    verifyElementCopy(this.crossDeviceEverythingWeNeedVideoUploadedMessage, crossDeviceEverythingWeNeedtrings.video_uploaded)
-    verifyElementCopy(this.crossDeviceEverythingWeNeedSubmitVerificationButton, crossDeviceEverythingWeNeedtrings.action)
+    verifyElementCopy(this.title, crossDeviceEverythingWeNeedtrings.title)
+    verifyElementCopy(this.subtitle, crossDeviceEverythingWeNeedtrings.sub_title)
+    verifyElementCopy(this.documentUploadedMessage, crossDeviceEverythingWeNeedtrings.one_doc_uploaded)
+    verifyElementCopy(this.videoUploadedMessage, crossDeviceEverythingWeNeedtrings.video_uploaded)
+    verifyElementCopy(this.submitVerificationButton, crossDeviceEverythingWeNeedtrings.action)
   }
 
   async clickOnSubmitVerificationButton() {
-    this.crossDeviceEverythingWeNeedSubmitVerificationButton.click()
+    this.submitVerificationButton.click()
   }
 }
 
