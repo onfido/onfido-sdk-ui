c6b48e1ff17c14fb6b584a5d99f97168046d8ac4
Bart Ziemba <Bart.Ziemba@onfido.com>
2019-07-05 15:45:41 +0100

    Verify UI elements of the guidance screens for PoA docs

--- test/js/pageobjects/PoAGuidance.js
+++ test/js/pageobjects/PoAGuidance.js
@@ -6,18 +6,76 @@ class PoaGuidance extends Base {
   get subtitle() { return this.$('.onfido-sdk-ui-ProofOfAddress-Guidance-subTitle')}
   get makeSure() { return this.$('.onfido-sdk-ui-ProofOfAddress-Guidance-makeSure')}
   get logoText() { return this.$('.onfido-sdk-ui-ProofOfAddress-Guidance-label:nth-child(7)')}
-  get fullNameext() { return this.$('.onfido-sdk-ui-ProofOfAddress-Guidance-label:nth-child(1)')}
+  get fullNameText() { return this.$('.onfido-sdk-ui-ProofOfAddress-Guidance-label:nth-child(1)')}
+  get currentText() { return this.$('.onfido-sdk-ui-ProofOfAddress-Guidance-label:nth-child(2)')}
+  get addressText() { return this.$('.onfido-sdk-ui-ProofOfAddress-Guidance-label:nth-child(3)')}
+  get issueDateText() { return this.$('.onfido-sdk-ui-ProofOfAddress-Guidance-label:nth-child(4)')}
+  get summaryPeriodText() { return this.$('.onfido-sdk-ui-ProofOfAddress-Guidance-label:nth-child(6')}
 
-  
+  get continueButton() { return this.$('.onfido-sdk-ui-Button-button-text')}
 
-  
-  
+  copy(lang) { return locale(lang) }
 
-  async verifyTitle(copy) {
-    const poaGudanceStrings = copy.capture.bank_building_society_statement.front
-    verifyElementCopy(this.title, poaGudanceStrings.title)
+  async verifyUIElementsForBankStatementGuidanceScreen(copy) {
+    const poaGudanceTitleStrings = copy.capture.bank_building_society_statement.front
+    const poaGudanceStrings = copy.proof_of_address.guidance
+    verifyElementCopy(this.title, poaGudanceTitleStrings.title)
+    verifyElementCopy(this.makeSure, poaGudanceStrings.make_sure_it_shows)
+    verifyElementCopy(this.logoText, poaGudanceStrings.logo)
+    verifyElementCopy(this.continueButton, poaGudanceStrings.continue)
   }
 
-}
+  async verifyUIElementsForUtilityBillGuidanceScreen(copy) {
+    const poaGudanceTitleStrings = copy.capture.utility_bill.front
+    const poaGudanceStrings = copy.proof_of_address.guidance
+    verifyElementCopy(this.title, poaGudanceTitleStrings.title)
+    verifyElementCopy(this.makeSure, poaGudanceStrings.make_sure_it_shows)
+    verifyElementCopy(this.logoText, poaGudanceStrings.logo)
+    verifyElementCopy(this.continueButton, poaGudanceStrings.continue)
+  }
 
+  async verifyUIElementsForCouncilTaxLetterGuidanceScreen(copy) {
+    const poaGudanceTitleStrings = copy.capture.council_tax.front
+    const poaGudanceStrings = copy.proof_of_address.guidance
+    verifyElementCopy(this.title, poaGudanceTitleStrings.title)
+    verifyElementCopy(this.makeSure, poaGudanceStrings.make_sure_it_shows)
+    verifyElementCopy(this.logoText, poaGudanceStrings.logo)
+    verifyElementCopy(this.continueButton, poaGudanceStrings.continue)
+  }
+
+  async verifyUIElementsForBenefitsLetterGuidanceScreen(copy) {
+    const poaGudanceTitleStrings = copy.capture.benefit_letters.front
+    const poaGudanceStrings = copy.proof_of_address.guidance
+    verifyElementCopy(this.title, poaGudanceTitleStrings.title)
+    verifyElementCopy(this.makeSure, poaGudanceStrings.make_sure_it_shows)
+    verifyElementCopy(this.logoText, poaGudanceStrings.logo)
+    verifyElementCopy(this.continueButton, poaGudanceStrings.continue)
+  }
+
+  async verifySubtitle(copy) {
+    verifyElementCopy(this.subtitle, copy)
+  }
+
+  async verifyFullNameText(copy) {
+    verifyElementCopy(this.fullNameText, copy)
+  }
+
+  async verifyCurrentText(copy) {
+    verifyElementCopy(this.currentText, copy)
+  }
+
+  async verifyAddressText(copy) {
+    verifyElementCopy(this.addressText, copy)
+  }
+
+  async verifyIssueDateText(copy) {
+    verifyElementCopy(this.issueDateText, copy)
+  }
+
+  async verifySummaryPeriodText(copy) {
+    verifyElementCopy(this.summaryPeriodText, copy)
+  }
+
+
+}
 export default PoaGuidance;
