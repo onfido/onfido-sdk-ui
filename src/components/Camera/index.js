74b67dd887b2efbf853bedce40dc2a041b7ad70a
Kimmi Gan <kimmi.gan@onfido.com>
2019-07-08 14:17:48 +0100

    Dimmed element should not be tab-able on error

--- src/components/Camera/index.js
+++ src/components/Camera/index.js
@@ -34,10 +34,10 @@ const CameraPure = ({
   webcamRef, onUserMedia, onFailure, video,
   translate
 }: Props) => (
-  <div className={classNames(style.camera, className)} tabIndex="0" aria-label={translate('accessibility.selfie_camera_view')}>
+  <div className={classNames(style.camera, className)}>
     {renderTitle}
     <div className={classNames(style.container, containerClassName)}>
-      <div className={style.webcamContainer}>
+      <div className={style.webcamContainer} tabIndex="0" aria-label={translate('accessibility.selfie_camera_view')}>
         <Webcam
           className={style.video}
           audio={!!video}
