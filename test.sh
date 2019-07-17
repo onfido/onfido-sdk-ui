3950aef64987410c4ed0c97ef8527da940fb5976
Ricardo Freitas <ricardo.freitas@onfido.com>
2019-07-17 13:09:37 +0100

    moved rvm use to travis yml due to stale build

--- test.sh
+++ test.sh
@@ -5,9 +5,6 @@ set -e
 # run only on pull requests and when NODE_ENV=test
 if [[ ${CI} != "true" || (${NODE_ENV} = "test" && ${TRAVIS_PULL_REQUEST} != "false") ]]; then
   # set path for test directory
-
-  bash --login #see https://stackoverflow.com/a/27415518
-  rvm use 2.4.1
   bundler --version
 
   npm run test:ui-js
