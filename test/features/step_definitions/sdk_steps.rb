70b45b4ae4d533961216be2f8cba01709f00a983
Bart Ziemba <Bart.Ziemba@onfido.com>
2019-07-19 18:26:51 +0100

    Bring back streps and page obiejcts

--- test/features/step_definitions/sdk_steps.rb
+++ test/features/step_definitions/sdk_steps.rb
@@ -37,31 +37,24 @@ Given(/^I am not using a browser with MediaRecorder API$/) do
   @driver.execute_script('window.MediaRecorder = undefined')
 end
 
-Given(/^I verify with (passport|identity_card|drivers_license)(?: with (.+)?)?$/) do |document_type, locale|
-  if document_type == 'passport'
-    key = 'capture.passport.front.title'
-  elsif document_type == 'identity_card'
-    key = 'capture.national_identity_card.front.title'
-  elsif document_type == 'drivers_license'
-    key = 'capture.driving_licence.front.title'
-  end
-
-  steps %Q{
-    Given I initiate the verification process with #{locale}
-    Then I should see 3 document_select_buttons ()
-    When I click on #{document_type} ()
-    Then I can confirm privacy terms
-    Then page_title should include translation for "#{key}"
-    And cross_device_header should include translation for "cross_device.switch_device.header"
-  }
-end
-
 Given(/^I navigate to the SDK as a modal/) do
   open_sdk(@driver, { 'useModal' => true, 'useWebcam' => false })
 end
 
-Given(/^I navigate to the SDK using liveness(?:| with "([^"]*)"?)$/) do |locale_tag|	
-  open_sdk(@driver, { 'liveness' => true, 'language' => locale_tag })	
+Given(/^I navigate to the SDK(?:| with "([^"]*)"?)$/) do |locale_tag|
+  open_sdk(@driver, { 'language' => locale_tag, 'useWebcam' => false })
+end
+
+Given(/^I navigate to the SDK using liveness(?:| with "([^"]*)"?)$/) do |locale_tag|
+  open_sdk(@driver, { 'liveness' => true, 'language' => locale_tag })
+end
+
+Given(/^I navigate to the SDK with forceCrossDevice feature enabled/) do
+  open_sdk(@driver, { 'forceCrossDevice' => true, 'useWebcam' => false })
+end
+
+Given(/^I navigate to the SDK with one document type/) do
+  open_sdk(@driver, { 'oneDoc' => true, 'useWebcam' => false })
 end
 
 When(/^I try to upload (\w+)(?:\s*)(pdf)?( and then retry)?$/) do |document, file_type, should_retry|
@@ -86,6 +79,13 @@ When(/^I try to upload (\w+)(?:\s*)(pdf)?( and then retry)?$/) do |document, fil
   }
 end
 
+Then(/^I should reach the complete step$/) do
+  steps %Q{
+    Then page_title should include translation for "complete.message"
+    Then I should not see back ()
+  }
+end
+
 Then(/^I can navigate back to the previous page with title "([^"]*)"$/) do | key |
   steps %Q{
     When I click on back ()
@@ -100,10 +100,31 @@ When(/^I see the camera permissions priming screen$/) do
   }
 end
 
+When(/^I try to upload my selfie$/) do
+  steps %Q{
+    Then page_title should include translation for "capture.face.upload_title"
+    When I try to upload one_face
+  }
+end
+
+When(/^I upload my document and selfie$/) do
+  steps %Q{
+    When I try to upload passport
+    When I try to upload my selfie
+  }
+end
+
 Then(/^(.*) should include translation for "([^"]*)"$/) do | page_element, key|
   text = i18n.translate(key)
   steps %Q{
     Then #{page_element} () should contain "#{text}"
+  }
+end
+
+Then(/^I wait until (.*) has "([^"]*)"$/) do | page_element, key |
+  text = i18n.translate(key)
+  steps %Q{
+    Then I wait until #{page_element} () contains "#{text}"
   }
 end
 
