a865f88983f7e0fc42449f15cb72a8eb97ce30cc
Bart Ziemba <Bart.Ziemba@onfido.com>
2019-07-23 12:43:43 +0100

    Remove helpers

--- test/features/utils/index.rb
+++
@@ -1,6 +0,0 @@
-def add_query_to_url(url, key, value)
-  uri = URI.parse(url)
-  updated_query_string = URI.decode_www_form(uri.query || '') << [key, value]
-  uri.query = URI.encode_www_form(updated_query_string)
-  return uri.to_s
-end
