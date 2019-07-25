a42d0a6f0812c00f4f15fb6921ee2245dc8aedd1
Bart Ziemba <Bart.Ziemba@onfido.com>
2019-07-25 11:58:40 +0100

    Remove check for bundler version.

--- test.sh
+++ test.sh
@@ -5,6 +5,5 @@ set -e
 # run only on pull requests and when NODE_ENV=test
 if [[ ${CI} != "true" || (${NODE_ENV} = "test" && ${TRAVIS_PULL_REQUEST} != "false") ]]; then
 
-  bundler --version
   npm run test:ui
 fi
