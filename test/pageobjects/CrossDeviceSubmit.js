08c7e64f2f02803afe5a25109f78952f57dd5155
Kimmi Gan <kimmi.gan@onfido.com>
2019-07-29 16:07:23 +0100

    Fix UI test that currently verifies video variant
    Fix UI test that currently verifies video variant against selfie upload and falsely passes the test

--- test/pageobjects/CrossDeviceSubmit.js
+++ test/pageobjects/CrossDeviceSubmit.js
@@ -5,8 +5,8 @@ class CrossDeviceSubmit extends Base {
 
   get title() { return this.$('.onfido-sdk-ui-PageTitle-titleSpan')}
   get subtitle() { return this.$('.onfido-sdk-ui-PageTitle-subTitle')}
-  get documentUploadedMessage() { return this.$('li:nth-child(1) > .onfido-sdk-ui-crossDevice-CrossDeviceSubmit-listText')}
-  get videoUploadedMessage() { return this.$('li:nth-child(2) > .onfido-sdk-ui-crossDevice-CrossDeviceSubmit-listText')}
+  get documentUploadedMessage() { return this.$('.onfido-sdk-ui-crossDevice-CrossDeviceSubmit-documentUploadedLabel')}
+  get selfieUploadedMessage() { return this.$('.onfido-sdk-ui-crossDevice-CrossDeviceSubmit-selfieUploadedLabel')}
   get submitVerificationButton() { return this.$('.onfido-sdk-ui-Button-button-text')}
 
   copy(lang) { return locale(lang) }
@@ -16,7 +16,7 @@ class CrossDeviceSubmit extends Base {
     verifyElementCopy(this.title, crossDeviceSubmitStrings.title)
     verifyElementCopy(this.subtitle, crossDeviceSubmitStrings.sub_title)
     verifyElementCopy(this.documentUploadedMessage, crossDeviceSubmitStrings.one_doc_uploaded)
-    verifyElementCopy(this.videoUploadedMessage, crossDeviceSubmitStrings.video_uploaded)
+    verifyElementCopy(this.selfieUploadedMessage, crossDeviceSubmitStrings.selfie_uploaded)
     verifyElementCopy(this.submitVerificationButton, crossDeviceSubmitStrings.action)
   }
 
