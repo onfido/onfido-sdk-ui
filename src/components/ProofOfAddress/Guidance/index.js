4b88a0373f9407b977c5b79193eaf09c7c5bc463
Kimmi Gan <kimmi.gan@onfido.com>
2019-08-20 12:29:43 +0100

    Add event listener to go back in PoA Guide

--- src/components/ProofOfAddress/Guidance/index.js
+++ src/components/ProofOfAddress/Guidance/index.js
@@ -1,4 +1,4 @@
-import { h } from 'preact'
+import { h, Component } from 'preact'
 import theme from '../../Theme/style.css'
 import style from './style.css'
 import PageTitle from '../../PageTitle'
@@ -7,35 +7,49 @@ import {trackComponent} from '../../../Tracker'
 import {localised} from '../../../locales'
 import Graphic from './graphic';
 
-const Guidance = ({translate, parseTranslatedTags, documentType, nextStep}) => {
-  return (
-    <div className={theme.fullHeightContainer}>
-      <PageTitle
-        title={translate(`capture.${documentType}.front.title`)}
-        subTitle={
-          <span className={style.subTitle}>
-            {parseTranslatedTags(`capture.${documentType}.front.sub_title`, ({ text }) => (
-              <span className={style.bolder}>{text}</span>
-            ))}
-          </span>
-        }
-      />
-      <div className={style.content}>
-        <div className={style.makeSure}>{translate('proof_of_address.guidance.make_sure_it_shows')}</div>
-        <div className={style.docImageContainer}>
-          <Graphic />
+class Guidance extends Component {
+
+  componentDidMount() {
+    const { previousStep } = this.props
+    this.props.events.on('backToPreviousView', previousStep)
+  }
+
+  componentWillUnmount() {
+    this.props.events.removeAllListeners('backToPreviousView')
+  }
+
+  render() {
+    const { translate, parseTranslatedTags, documentType, nextStep } = this.props
+    return (
+      <div className={theme.fullHeightContainer}>
+        <PageTitle
+          title={translate(`capture.${documentType}.front.title`)}
+          subTitle={
+            <span className={style.subTitle}>
+              {parseTranslatedTags(`capture.${documentType}.front.sub_title`, ({ text }) => (
+                <span className={style.bolder}>{text}</span>
+              ))}
+            </span>
+          }
+        />
+        <div className={style.content}>
+          <div className={style.makeSure}>{translate('proof_of_address.guidance.make_sure_it_shows')}</div>
+          <div className={style.docImageContainer}>
+            <Graphic />
+          </div>
+        </div>
+        <div className={theme.thickWrapper}>
+          <Button
+            variants={["primary", "centered"]}
+            onClick={nextStep}
+          >
+            {translate('proof_of_address.guidance.continue')}
+          </Button>
         </div>
       </div>
-      <div className={theme.thickWrapper}>
-        <Button
-          variants={["primary", "centered"]}
-          onClick={nextStep}
-        >
-          {translate('proof_of_address.guidance.continue')}
-        </Button>
-      </div>
-    </div>
-  )
+    )
+  }
+
 }
 
-export default trackComponent(localised(Guidance))
+export default trackComponent(localised(Guidance), 'poa_guide')
