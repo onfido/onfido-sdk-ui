4042af328ceedf22c06bb5a599ac41a6ce88244a
Kimmi Gan <kimmi.gan@onfido.com>
2019-08-20 12:29:55 +0100

    Add event listener to go back in PoA Intro

--- src/components/ProofOfAddress/PoAIntro/index.js
+++ src/components/ProofOfAddress/PoAIntro/index.js
@@ -1,4 +1,4 @@
-import { h } from 'preact'
+import { h, Component } from 'preact'
 import theme from '../../Theme/style.css'
 import style from './style.css'
 import PageTitle from '../../PageTitle'
@@ -6,34 +6,49 @@ import Button from '../../Button'
 import {trackComponent} from '../../../Tracker'
 import {localised} from '../../../locales'
 
-const PoAIntro = ({country, translate, parseTranslatedTags, nextStep}) => (
-  <div className={theme.fullHeightContainer}>
-    <PageTitle title={translate('proof_of_address.intro.title', {
-      country: !country || country === 'GBR' ? 'UK' : '',
-    })} />
-    <div className={style.content}>
-      <p className={style.requirements}>{translate('proof_of_address.intro.requirements')}</p>
-      {
-        ['shows_address', 'matches_signup', 'is_recent'].map(key =>
-          <div key={key} className={style.requirement}>
-            <span>
-            {parseTranslatedTags(`proof_of_address.intro.${key}`, ({ text }) => (
-              <span className={style.bolder}>{text}</span>
-            ))}
-            </span>
-          </div>
-        )
-      }
-    </div>
-    <div className={theme.thickWrapper}>
-      <Button
-        variants={["primary", "centered"]}
-        onClick={nextStep}
-      >
-        {translate('proof_of_address.intro.start')}
-      </Button>
-    </div>
-  </div>
-)
+class PoAIntro extends Component {
+
+  componentDidMount() {
+    const { previousStep } = this.props
+    this.props.events.on('backToPreviousView', previousStep)
+  }
+
+  componentWillUnmount() {
+    this.props.events.removeAllListeners('backToPreviousView')
+  }
+
+  render() {
+    const { country, translate, parseTranslatedTags, nextStep } = this.props
+    return (
+      <div className={theme.fullHeightContainer}>
+        <PageTitle title={translate('proof_of_address.intro.title', {
+          country: !country || country === 'GBR' ? 'UK' : '',
+        })} />
+        <div className={style.content}>
+          <p className={style.requirements}>{translate('proof_of_address.intro.requirements')}</p>
+          {
+            ['shows_address', 'matches_signup', 'is_recent'].map(key =>
+              <div key={key} className={style.requirement}>
+                <span>
+                {parseTranslatedTags(`proof_of_address.intro.${key}`, ({ text }) => (
+                  <span className={style.bolder}>{text}</span>
+                ))}
+                </span>
+              </div>
+            )
+          }
+        </div>
+        <div className={theme.thickWrapper}>
+          <Button
+            variants={["primary", "centered"]}
+            onClick={nextStep}
+          >
+            {translate('proof_of_address.intro.start')}
+          </Button>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default trackComponent(localised(PoAIntro), "poa_intro")
