8690d7371ec40b3eb40faf40d75a39ff657f45fb
Kimmi Gan <kimmi.gan@onfido.com>
2019-08-28 14:30:20 +0100

    ID1, ID3 card size overlays

--- src/components/Overlay/index.js
+++ src/components/Overlay/index.js
@@ -3,14 +3,14 @@ import classNames from 'classnames'
 import style from './style.css'
 import { withFullScreenState } from '../FullScreen'
 
-export const FaceOverlay = withFullScreenState(({ isFullScreen, isWithoutHole }) =>
+export const FaceOverlay = withFullScreenState(({ isFullScreen, isWithoutHole }) => (
   <div className={classNames({
     [style.fullScreenOverlay]: isFullScreen,
-    [style.isWithoutHole]: isWithoutHole,
+    [style.isWithoutHole]: isWithoutHole
   })}>
     <span className={style.face} />
   </div>
-)
+))
 
-export  const DocumentOverlay = () =>
-  <span className={style.rectangle} />
+export const DocumentOverlay = ({ documentSize }) =>
+  <span className={style[documentSize] || style.rectangle} />
