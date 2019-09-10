06f58f2193edfb185d8fc5101b412454684e402c
Kimmi Gan <kimmi.gan@onfido.com>
2019-09-10 15:35:25 +0100

    Feedback - add error, warning hover/pressed states

--- src/components/Button/FallbackButton.js
+++ src/components/Button/FallbackButton.js
@@ -1,8 +1,9 @@
 import { h } from 'preact'
+import theme from '../Theme/style.css'
 import style from './style.css'
 
 const FallbackButton = ({text, onClick}) => (
-    <button type="button" className={style.fallbackButton} onClick={onClick}>
+    <button type="button" className={`${style.fallbackButton} ${theme.warning}`} onClick={onClick}>
       {text}
     </button>
 )
