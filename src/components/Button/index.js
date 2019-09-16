5178d3b2a86f7b39a47f8645a113cffd847972b4
Kimmi Gan <kimmi.gan@onfido.com>
2019-09-16 12:35:32 +0100

    Revert "Revert "Only apply hover state to desktops for buttons""
    This reverts commit 83d1a62b343dc80edcb81d71298c4c649005b0ac.

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,5 +1,6 @@
 import { h } from 'preact'
 import classNames from 'classnames'
+import { isDesktop } from '~utils/index'
 import style from './style.css'
 
 const Button = ({ className, textClassName, variants = [], disabled, children, onClick, ariaLive, ariaBusy }) => (
@@ -9,7 +10,9 @@ const Button = ({ className, textClassName, variants = [], disabled, children, o
     aria-busy={ariaBusy}
     disabled={disabled}
     onClick={onClick}
-    className={classNames(className, style.button, ...variants.map(v => style['button-' + v]))}
+    className={classNames(className, style.button, ...variants.map(v => style['button-' + v]), {
+      [style.hoverDesktop]: isDesktop
+    })}
   >
     <span className={classNames(textClassName, style['button-text'])}>
       {children}
