8b75dd187f4cc2cdfd8bccd168ff096dbb98b84a
Kimmi Gan <kimmi.gan@onfido.com>
2019-09-16 16:20:47 +0100

    Revert "Get iOS VoiceOver to read the ARIA label"
    This reverts commit 3c03cec909c48f525a81cfec73b3a36edef360a7.

--- src/components/Camera/index.js
+++ src/components/Camera/index.js
@@ -37,12 +37,8 @@ const CameraPure = ({
 }: Props) => (
   <div className={classNames(style.camera, className)}>
     {renderTitle}
-    <div
-      className={classNames(style.container, containerClassName)}
-      aria-label={translate('accessibility.camera_view')}
-      role='group'
-      id='cameraViewAriaLabelVoiceOver'>
-      <div className={style.webcamContainer} aria-describedby='cameraViewAriaLabelVoiceOver cameraViewAriaLabel'>
+    <div className={classNames(style.container, containerClassName)} role='group'>
+      <div className={style.webcamContainer} aria-describedby='cameraViewAriaLabel'>
         <Webcam
           className={style.video}
           audio={!!video}
