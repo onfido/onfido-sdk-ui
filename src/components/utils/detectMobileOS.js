50f6a4f1a5d7436a138fab9c861ce9f62816fff9
Kimmi Gan <kimmi.gan@onfido.com>
2019-10-22 18:14:03 +0100

    Add warning about use of getMobileOSName's UA detection

--- src/components/utils/detectMobileOS.js
+++ src/components/utils/detectMobileOS.js
@@ -1,3 +1,4 @@
+// WARN: use of this util is highly discouraged unless absolutely necessary and for minor use cases
 export const getMobileOSName = () => {
   const userAgent = navigator.userAgent
   if (/android/i.test(userAgent)) {
