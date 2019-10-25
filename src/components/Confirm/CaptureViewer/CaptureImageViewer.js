29a1b207cbf667f462391996dcdc6e6ae51f0c5b
Kimmi Gan <kimmi.gan@onfido.com>
2019-10-25 15:03:46 +0100

    Update class name and crop dimension height

--- src/components/Confirm/CaptureViewer/CaptureImageViewer.js
+++ src/components/Confirm/CaptureViewer/CaptureImageViewer.js
@@ -22,7 +22,7 @@ const CaptureImageViewer = ({ src, id, isDocument, isFullScreen, altTag }) => (
       !isFullScreen &&
         <img
           key={id}//WORKAROUND necessary to prevent img recycling, see bug: https://github.com/developit/preact/issues/351
-          className={isDocument && !isDesktop ? style.croppedImage : style.image}
+          className={isDocument && !isDesktop ? style.croppedDocumentImage : style.image}
           //we use base64 if the capture is a File, since its base64 version is exif rotated
           //if it's not a File (just a Blob), it means it comes from the webcam,
           //so the base64 version is actually lossy and since no rotation is necessary
