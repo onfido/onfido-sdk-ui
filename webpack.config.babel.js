0539c65d50e5381b4678b82585d6732c62e85fe0
Stefania Cardenas <stefania.cardenas@onfido.com>
2019-11-08 18:56:36 +0000

    WIP

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -13,7 +13,6 @@ import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import Visualizer from 'webpack-visualizer-plugin';
 import path from 'path';
 import nodeExternals from 'webpack-node-externals';
-import SentryWebpackPlugin from '@sentry/webpack-plugin';
 
 
 // NODE_ENV can be one of: development | staging | test | production
@@ -293,9 +292,6 @@ const configDist = {
       JWT_FACTORY: CONFIG.JWT_FACTORY,
       DESKTOP_SYNC_URL: CONFIG.DESKTOP_SYNC_URL,
       chunks: ['previewer']
-    }),
-    new SentryWebpackPlugin({
-      include: './dist'
     }),
     ...PRODUCTION_BUILD ?
       [new webpack.LoaderOptionsPlugin({
