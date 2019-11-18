b6ae525f4b88cd6c02252a42f263cef4baae1fe5
Anurag Ajwani <anurag.ajwani@onfido.com>
2019-11-18 10:51:59 +0000

    CX-2473; Moved instructions rendering to stateless components instead of function within SelfieIntro component

--- src/components/Selfie/Intro.js
+++ src/components/Selfie/Intro.js
@@ -7,32 +7,32 @@ import classNames from 'classnames'
 import Button from '../Button'
 import { localised } from '../../locales'
 
-class Intro extends Component<Props, State> {
-
-  renderInstruction = (instruction) => {
-    return <li key={instruction.key} className={style.introBullet}>
+const InstructionPure = (instruction) => (
+    <li key={instruction.key} className={style.introBullet}>
       <span className={classNames(style.introIcon, style[`${instruction.key}Icon`])} />
       <span className={classNames(style.bolder, style.introText)}>{instruction.text}</span>
     </li>
-  }
+)
 
-  renderInstructions = (instructions) => {
-    return <div className={classNames(style.thinWrapper, style.introCopy)}>
+const RenderInstructions = (instructions) => (
+    <div className={classNames(style.thinWrapper, style.introCopy)}>
       <ul className={style.introBullets} aria-label="TODO">
       {
         instructions.map(instruction =>
-          this.renderInstruction(instruction)
+          <InstructionPure instruction={instruction} />
         )
       }
       </ul>
     </div>
-  }
+)
+
+class Intro extends Component<Props, State> {
 
   render() {
     const { translate, nextStep } = this.props
     return <div className="theme.fullHeightContainer">
       <PageTitle title={translate("capture.face.intro.title")} subTitle={translate("capture.face.intro.subtitle")} />
-      {this.renderInstructions([{key:"selfie", text:translate("capture.face.intro.selfie_instruction")},{key:"glasses", text:translate("capture.face.intro.glasses_instruction")}])}
+      <RenderInstructions instructions={[{key:"selfie", text: translate("capture.face.intro.selfie_instruction")}, {key:"glasses", text: translate("capture.face.intro.glasses_instruction")}]} />
       <div className={theme.thickWrapper}>
         <Button
           variants={['primary', 'centered']}
