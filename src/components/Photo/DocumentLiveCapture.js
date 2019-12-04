ba2d23f6181ab8d5d4f39d86bccd56b4fd7c136a
Kimmi Gan <kimmi.gan@onfido.com>
2019-12-04 12:01:37 +0000

    PR feedback - use consistent naming

--- src/components/Photo/DocumentLiveCapture.js
+++ src/components/Photo/DocumentLiveCapture.js
@@ -16,7 +16,7 @@ import style from './style.css'
 type State = {
   hasBecomeInactive: boolean,
   hasCameraError: boolean,
-  isLoading: boolean
+  isCapturing: boolean
 }
 
 type Props = {
@@ -38,7 +38,7 @@ export default class DocumentLiveCapture extends Component<Props, State> {
   state: State = {
     hasBecomeInactive: false,
     hasCameraError: false,
-    isLoading: false
+    isCapturing: false
   }
 
   handleTimeout = () => this.setState({ hasBecomeInactive: true })
@@ -58,17 +58,17 @@ export default class DocumentLiveCapture extends Component<Props, State> {
       isPreviewCropped: true
     }
     this.props.onCapture(documentCapture)
-    this.setState({ isLoading: false })
+    this.setState({ isCapturing: false })
   }
 
   captureDocumentPhoto = () => {
-    this.setState({ isLoading: true })
+    this.setState({ isCapturing: true })
     sendEvent('Taking live photo of document')
     screenshot(this.webcam, this.captureDocument, 'image/jpeg')
   }
 
   componentWillUnmount() {
-    this.setState({ isLoading: false })
+    this.setState({ isCapturing: false })
   }
 
   render() {
@@ -83,13 +83,13 @@ export default class DocumentLiveCapture extends Component<Props, State> {
       renderError,
       documentType
     } = this.props
-    const { hasBecomeInactive, hasCameraError, isLoading } = this.state
+    const { hasBecomeInactive, hasCameraError, isCapturing } = this.state
     const id1SizeDocuments = new Set([ 'driving_licence', 'national_identity_card' ])
     const documentSize = id1SizeDocuments.has(documentType) ? 'id1Card' : 'id3Card'
     const idealCameraHeightInPixels = 1280
     return (
       <div className={style.container}>
-        {this.state.isLoading ?
+        {this.state.isCapturing ?
         <Spinner /> :
         <Camera
           facing='environment'
@@ -117,7 +117,7 @@ export default class DocumentLiveCapture extends Component<Props, State> {
           <div className={style.actions}>
             <CameraButton
               ariaLabel={translate('accessibility.shutter')}
-              disabled={hasCameraError || isLoading}
+              disabled={hasCameraError || isCapturing}
               onClick={this.captureDocumentPhoto}
               className={style.btn}
             />
