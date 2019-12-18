275a78dba857f4ef5873fd8a0e28160e5ecf44cc
Kimmi Gan <kimmi.gan@onfido.com>
2019-12-18 10:54:09 +0000

    Make key names more generic, consistent with spreadsheet

--- src/components/QRCode/HowTo.js
+++ src/components/QRCode/HowTo.js
@@ -30,11 +30,11 @@ class QRCodeHowTo extends Component {
           aria-expanded={isExpanded}
           className={classNames(theme.link, style.qrCodeHelpButton)}
           onClick={this.toggleHelpListVisibility}>
-          {translate('cross_device.link.qr_code.how_to_label')}
+          {translate('cross_device.link.qr_code.help_label')}
         </button>
         <ul hidden={!isExpanded} className={style.qrCodeHelpList}>
-          <li data-onfido-qa="qrCodeHowToStep1">{translate('cross_device.link.qr_code.how_to_step_1')}</li>
-          <li data-onfido-qa="qrCodeHowToStep2">{translate('cross_device.link.qr_code.how_to_step_2')}</li>
+          <li data-onfido-qa="qrCodeHowToStep1">{translate('cross_device.link.qr_code.help_step_1')}</li>
+          <li data-onfido-qa="qrCodeHowToStep2">{translate('cross_device.link.qr_code.help_step_2')}</li>
         </ul>
       </div>
     )
