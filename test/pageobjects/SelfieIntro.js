847e473ba970ed4b99634c1ba17c4a4af7c3350d
Stefania Cardenas <stefania.cardenas@onfido.com>
2020-01-03 16:09:18 +0000

    Fix broken tests

--- test/pageobjects/SelfieIntro.js
+++ test/pageobjects/SelfieIntro.js
@@ -2,8 +2,8 @@ import BasePage from './BasePage.js'
 import { verifyElementCopy } from '../utils/mochaw'
 
 class SelfieIntro extends BasePage {
-  get selfieIcon() { return this.$('.onfido-sdk-ui-Selfie-selfieIcon')}
-  get glassesIcon() { return this.$('.onfido-sdk-ui-Selfie-glassesIcon')}
+  get selfieIcon() { return this.$('.onfido-sdk-ui-Photo-selfieIcon')}
+  get glassesIcon() { return this.$('.onfido-sdk-ui-Photo-glassesIcon')}
   get continueButton() { return this.$('.onfido-sdk-ui-Button-button-text')}
 
   async verifyUIElementsOnTheSelfieIntroScreen(copy) {
