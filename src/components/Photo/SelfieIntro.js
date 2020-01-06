afa259124d258e30e16b93d3e04b107e4c2926ed
Stefania Cardenas <stefania.cardenas@onfido.com>
2020-01-06 17:07:08 +0000

    PR feedback

--- src/components/Photo/SelfieIntro.js
+++ src/components/Photo/SelfieIntro.js
@@ -11,19 +11,16 @@ import withCrossDeviceWhenNoCamera from '../Capture/withCrossDeviceWhenNoCamera'
 import withCameraDetection from '../Capture/withCameraDetection'
 import { compose } from '~utils/func'
 
-const InstructionPure = ({instruction}) => (
-    <li key={instruction.key} className={style.introBullet}>
-      <span className={classNames(style.introIcon, style[`${instruction.key}Icon`])} />
-      <span className={classNames(style.bolder, style.introText)}>{instruction.text}</span>
-    </li>
-)
 
 const InstructionsPure = ({listScreenReaderText, instructions}) => (
     <div className={classNames(style.thinWrapper, style.introCopy)}>
       <ul className={style.introBullets} aria-label={listScreenReaderText}>
       {
         instructions.map(instruction =>
-          <InstructionPure instruction={instruction} />
+          <li className={style.introBullet}>
+            <span className={classNames(style.introIcon, style[`${instruction.key}Icon`])} />
+            <span className={classNames(style.bolder, style.introText)}>{instruction.text}</span>
+          </li>
         )
       }
       </ul>
@@ -35,8 +32,9 @@ class Intro extends Component<Props, State> {
   render() {
     const { translate, nextStep } = this.props
     const instructions = [
-      {key:"selfie", text: translate("capture.face.intro.selfie_instruction")},
-      {key:"glasses", text: translate("capture.face.intro.glasses_instruction")}]
+      { key: "selfie", text: translate("capture.face.intro.selfie_instruction") },
+      { key: "glasses", text: translate("capture.face.intro.glasses_instruction") }
+    ]
     return <div className="theme.fullHeightContainer">
       <PageTitle title={translate("capture.face.intro.title")} subTitle={translate("capture.face.intro.subtitle")} />
       <InstructionsPure listScreenReaderText={translate("capture.face.intro.accessibility.selfie_capture_tips")} instructions={instructions} />
