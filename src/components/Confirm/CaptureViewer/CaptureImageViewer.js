655674c8faa6c1b0fdffc39c4c9278aabbb9d651
Luis Santana <luis.santana@onfido.com>
2020-01-23 09:57:33 -0700

    ET-11: Add face detection to document image confirm screen

--- src/components/Confirm/CaptureViewer/CaptureImageViewer.js
+++ src/components/Confirm/CaptureViewer/CaptureImageViewer.js
@@ -1,38 +1,104 @@
-import { h } from 'preact'
+import { h, Component } from 'preact'
+import * as cocoSsd from '@tensorflow-models/coco-ssd';
 import classNames from 'classnames'
 import style from './style.css'
 import { withBlobPreviewUrl, withBlobBase64 } from './hocs'
 import EnlargedPreview from '../../EnlargedPreview'
 
-const CaptureImageViewer = ({ src, id, isDocument, isFullScreen, isPreviewCropped, altTag }) => (
-  <span className={classNames(isPreviewCropped ? style.croppedImageWrapper : style.imageWrapper, {
-    [style.fullscreenImageWrapper]: isFullScreen,
-  })}>
-    {
-      isDocument &&
-        <EnlargedPreview
-          {...{
-            src,
-            altTag
-          }}
-        />
+class CaptureImageViewer extends Component {
+  constructor(props){
+    super(props)
+  }
+
+  model = null
+
+  state = {
+    faceDetectionWarning: null,
+    modelLoaded: false,
+    detectingFace: false,
+  }
+
+  componentDidMount() {
+    cocoSsd.load()
+    .then(model => {
+      this.model = model
+      this.setState({modelLoaded: true})
+    })
+  }
+
+  async componentDidUpdate() {
+    if (this.state.modelLoaded && !this.state.detectingFace) {
+      await this.startFaceDetection()
     }
-    {
-      !isFullScreen &&
-        <img
-          key={id}//WORKAROUND necessary to prevent img recycling, see bug: https://github.com/developit/preact/issues/351
-          className={isPreviewCropped ? style.croppedDocumentImage : style.image}
-          //we use base64 if the capture is a File, since its base64 version is exif rotated
-          //if it's not a File (just a Blob), it means it comes from the webcam,
-          //so the base64 version is actually lossy and since no rotation is necessary
-          //the blob is the best candidate in this case
-          src={src}
-          alt={altTag}
-          aria-hidden={isDocument} // This prevents the image alt tag from being read twice for document as the document alt tag is already announced inside EnlargedPreview component
-        />
+  }
+
+  setDetectionWarning = (faceDetectionWarning: ?string) => {
+    this.setState({faceDetectionWarning})
+  }
+
+  handleDetections = (detections: Array<{class: string, score: number}>) => {
+    if (!detections || detections.length < 1) {
+      return this.setDetectionWarning('No face found')
     }
-  </span>
-)
+
+    const faces = detections.filter(detection => detection.class === 'person' && detection.score >= 0.55).length;
+    
+    if (faces > 1) {
+      return this.setDetectionWarning('Multiple faces detected')
+    } else if (faces === 0) {
+      return this.setDetectionWarning('No face found')
+    }
+
+    this.setDetectionWarning(null)
+  }
+
+  startFaceDetection = async () => {
+    const { src } = this.props
+    const image = new Image();
+    image.src = src;
+    this.setState({detectingFace: true})
+
+    await this.model.detect(image)
+      .then((results) => this.handleDetections(results))
+  }
+
+  render() {
+    const { src, id, isDocument, isFullScreen, isPreviewCropped, altTag } = this.props;
+    const { faceDetectionWarning } = this.state;
+
+    return (
+      <div>
+    <span className={classNames(isPreviewCropped ? style.croppedImageWrapper : style.imageWrapper, {
+      [style.fullscreenImageWrapper]: isFullScreen,
+    })}>
+      {
+        isDocument &&
+          <EnlargedPreview
+            {...{
+              src,
+              altTag
+            }}
+          />
+      }
+      {
+        !isFullScreen &&
+          <img
+            key={id}//WORKAROUND necessary to prevent img recycling, see bug: https://github.com/developit/preact/issues/351
+            className={isPreviewCropped ? style.croppedDocumentImage : style.image}
+            //we use base64 if the capture is a File, since its base64 version is exif rotated
+            //if it's not a File (just a Blob), it means it comes from the webcam,
+            //so the base64 version is actually lossy and since no rotation is necessary
+            //the blob is the best candidate in this case
+            src={src}
+            alt={altTag}
+            aria-hidden={isDocument} // This prevents the image alt tag from being read twice for document as the document alt tag is already announced inside EnlargedPreview component
+          />
+      }
+    </span>
+    { faceDetectionWarning && <div className={style.faceDetection}>{ faceDetectionWarning }</div> }
+    </div>
+  )}
+}
 
 const CaptureImageViewerWithPreviewUrl = withBlobPreviewUrl(
   ({ previewUrl, ...props }) => <CaptureImageViewer src={previewUrl} {...props} />
