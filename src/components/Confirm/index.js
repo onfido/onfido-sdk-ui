086f79dd47ce9c6266b9609f96f87b913c0d24e5
Stefania Cardenas <stefania.cardenas@onfido.com>
2020-02-05 14:47:16 +0000

    Use JSON

--- src/components/Confirm/index.js
+++ src/components/Confirm/index.js
@@ -163,7 +163,7 @@ class Confirm extends Component {
       uploadSnapshot(snapshotData, url, token, resolve, reject)
     })
     .then((res) => {
-      const snapshot_uuids = [res.uuid]
+      const snapshot_uuids = JSON.stringify([res.uuid])
       uploadLivePhoto({ file: { blob, filename }, sdkMetadata, snapshot_uuids}, url, token, onSuccess, onError)
     })
     .catch(() => {
