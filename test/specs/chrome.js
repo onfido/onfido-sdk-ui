e337f5873f636b9f1e7dc5a2c0b799b4906a3fa5
Kimmi Gan <kimmi.gan@onfido.com>
2020-02-18 16:45:48 +0000

    Disable some long running tests to be able to see final errors

--- test/specs/chrome.js
+++ test/specs/chrome.js
@@ -1,28 +1,28 @@
 import { describe } from '../utils/mochaw'
 import { supportedLanguages } from '../config.json'
-// import { welcomeScenarios } from './scenarios/welcome'
-// import { documentSelectorScenarios } from './scenarios/documentSelector'
+import { welcomeScenarios } from './scenarios/welcome'
+import { documentSelectorScenarios } from './scenarios/documentSelector'
 // import { documentScenarios } from './scenarios/document'
 // import { faceScenarios } from './scenarios/face'
-import { crossDeviceScenarios } from './scenarios/crossDevice'
+// import { crossDeviceScenarios } from './scenarios/crossDevice'
 import { proofOfAddressScenarios } from './scenarios/proofOfAddress'
 // import { modalScenarios } from './scenarios/modal'
 import { navigationScenarios } from './scenarios/navigation'
-import { accessibilityScenarios } from './scenarios/accessibility'
+// import { accessibilityScenarios } from './scenarios/accessibility'
 
 
 describe('Happy Paths on Chrome',() => {
   // Multiple language scenarios
   supportedLanguages.forEach((lang) => {
-    // welcomeScenarios(lang)
-    // documentSelectorScenarios(lang)
+    welcomeScenarios(lang)
+    documentSelectorScenarios(lang)
     // documentScenarios(lang)
     // faceScenarios(lang)
-    crossDeviceScenarios(lang)
+    // crossDeviceScenarios(lang)
     // modalScenarios(lang)
     navigationScenarios(lang)
   })
   // PoA is only available in en
   proofOfAddressScenarios()
-  accessibilityScenarios()
+  // accessibilityScenarios()
 })
