0b679e04d38aebcb761b09cf9288cdee0ef8c456
Kimmi Gan <kimmi.gan@onfido.com>
2020-02-21 11:43:41 +0000

    Bump version to 5.7.1-rc.1

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -180,7 +180,7 @@ const basePlugins = (bundle_name) => ([
     // ref: https://en.wikipedia.org/wiki/Base32
     // NOTE: please leave the BASE_32_VERSION be! It is updated automatically by
     // the release script 🤖
-    'BASE_32_VERSION': 'BA',
+    'BASE_32_VERSION': 'BB',
     'PRIVACY_FEATURE_ENABLED': false,
     JWT_FACTORY: CONFIG.JWT_FACTORY,
     US_JWT_FACTORY: CONFIG.US_JWT_FACTORY,
