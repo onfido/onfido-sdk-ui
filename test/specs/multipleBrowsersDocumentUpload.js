510a0f2d1c360e90ae162f1946bda7d1e9c6a349
Bart Ziemba <Bart.Ziemba@onfido.com>
2020-03-17 08:46:43 +0000

    Uppercase JPG and PDF

--- test/specs/multipleBrowsersDocumentUpload.js
+++ test/specs/multipleBrowsersDocumentUpload.js
@@ -8,12 +8,12 @@ const options = {
 describe('DOCUMENT AND PDF UPLOAD ON MULTIPLE BROWSERS', options, ({driver, pageObjects}) => {
   const {welcome, documentSelector, confirm, documentUpload} = pageObjects
 
-  it('should upload document with jgp', async () => {
+  it('should upload document with JPG', async () => {
     goToPassportUploadScreen(driver, welcome, documentSelector)
     uploadFileAndClickConfirmButton(documentUpload, confirm, 'passport.jpg')
   })
 
-  it('should upload document with pdf', async () => {
+  it('should upload document with PDF', async () => {
     goToPassportUploadScreen(driver, welcome, documentSelector)
     uploadFileAndClickConfirmButton(documentUpload, confirm, 'national_identity_card.pdf')
   })
