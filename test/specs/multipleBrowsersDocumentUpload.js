c63c2ae923d43049acb619aa10874c8987192c43
Bart Ziemba <Bart.Ziemba@onfido.com>
2020-03-20 15:26:20 +0000

    bring back old it naming

--- test/specs/multipleBrowsersDocumentUpload.js
+++ test/specs/multipleBrowsersDocumentUpload.js
@@ -8,12 +8,12 @@ const options = {
 describe('DOCUMENT UPLOAD ON MULTIPLE BROWSERS', options, ({driver, pageObjects}) => {
   const {welcome, documentSelector, confirm, documentUpload} = pageObjects
 
-  it('should upload jgp', async () => {
+  it('should upload document on IE11 browser', async () => {
     goToPassportUploadScreen(driver, welcome, documentSelector)
     uploadFileAndClickConfirmButton(documentUpload, confirm, 'passport.jpg')
   })
 
-  it('should upload pdf', async () => {
+  it('should upload PDF on IE11 browser', async () => {
     goToPassportUploadScreen(driver, welcome, documentSelector)
     uploadFileAndClickConfirmButton(documentUpload, confirm, 'national_identity_card.pdf')
   })
