e7abf00d669a7b05bd38388a18bc57a8532a44f6
Bart Ziemba <Bart.Ziemba@onfido.com>
2020-03-20 15:08:19 +0000

    Add on browser

--- test/specs/multipleBrowsersDocumentUpload.js
+++ test/specs/multipleBrowsersDocumentUpload.js
@@ -8,12 +8,12 @@ const options = {
 describe('DOCUMENT UPLOAD ON MULTIPLE BROWSERS', options, ({driver, pageObjects}) => {
   const {welcome, documentSelector, confirm, documentUpload} = pageObjects
 
-  it('should upload document with jgp', async () => {
+  it('should upload document with jgp on browser', async () => {
     goToPassportUploadScreen(driver, welcome, documentSelector)
     uploadFileAndClickConfirmButton(documentUpload, confirm, 'passport.jpg')
   })
 
-  it('should upload document with pdf', async () => {
+  it('should upload document with pdf on browser', async () => {
     goToPassportUploadScreen(driver, welcome, documentSelector)
     uploadFileAndClickConfirmButton(documentUpload, confirm, 'national_identity_card.pdf')
   })
