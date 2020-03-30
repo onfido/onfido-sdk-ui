8f426f908dffce18265a7e77bdd63fe2a4316b07
Kimmi Gan <kimmi.gan@onfido.com>
2020-03-30 18:50:49 +0100

    Camera handles Video "Start Recording" button also

--- src/components/Video/NotRecording.js
+++ src/components/Video/NotRecording.js
@@ -2,33 +2,27 @@
 import * as React from 'react'
 import { h } from 'preact'
 import classNames from 'classnames'
-import Timeout from '../Timeout'
 import style from './style.css'
 import { localised } from '../../locales'
 import type { LocalisedType } from '../../locales'
 
 type Props = {
-  hasError: boolean,
   disableInteraction: boolean,
-  onTimeout: void => void,
   onStart: void => void,
 } & LocalisedType
 
-const NotRecording = ({ translate, onStart, hasError, disableInteraction, onTimeout }: Props) => (
-  <div>
-    { !hasError ? <Timeout key="notRecording" seconds={ 12 } onTimeout={ onTimeout } /> : null }
-    <div className={style.actions}>
-      <div className={classNames(style.captureActionsHint, style.recordAction)}>
-        { translate('capture.liveness.press_record') }
-      </div>
-      <button
-        type="button"
-        aria-label={translate('accessibility.start_recording')}
-        disabled={disableInteraction}
-        onClick={onStart}
-        className={classNames(style.btn, style.startRecording)}
-      />
+const NotRecording = ({ translate, onStart, disableInteraction }: Props) => (
+  <div className={style.actions}>
+    <div className={classNames(style.captureActionsHint, style.recordAction)}>
+      { translate('capture.liveness.press_record') }
     </div>
+    <button
+      type="button"
+      aria-label={translate('accessibility.start_recording')}
+      disabled={disableInteraction}
+      onClick={onStart}
+      className={classNames(style.btn, style.startRecording)}
+    />
   </div>
 )
 
