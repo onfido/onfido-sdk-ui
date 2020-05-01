9be690c47887fa5dd36efd885907dae1f184ea8b
Bart Ziemba <Bart.Ziemba@onfido.com>
2020-05-01 10:52:44 +0100

    Remove commented out code.

--- test/main.js
+++ test/main.js
@@ -30,9 +30,6 @@ const bsCapabilitiesDefault = {
 // replace <browserstack-accesskey> with your key. You can also set an environment variable - "BROWSERSTACK_ACCESS_KEY".
 const browserstackLocalDefault = {
   'key': bsCapabilitiesDefault['browserstack.key'],
-  // 'binarypath': './test//browserstack/BrowserStackLocal',
-  // 'verbose': 'true',
-  // 'logFile': './test/browserstack/logs.txt'
 };
 
 const currentDate = Date.now().toString();
