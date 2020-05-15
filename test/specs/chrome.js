4102f9332897df227698f3bc0a2ffec775280610
Bart Ziemba <Bart.Ziemba@onfido.com>
2020-05-15 09:20:09 +0100

    Remove PoA

--- test/specs/chrome.js
+++ test/specs/chrome.js
@@ -22,9 +22,4 @@ describe('Happy Paths on Chrome',() => {
     modalScenarios(lang)
     navigationScenarios(lang)
   });
-  // Note: The SDK works also with language tags that do not include region (e.g. 'en', 'es')
-  // We are passing the region here so we can fetch the right json file path (e.g. `en_US/en_US.json`).
-  // PoA is only available in en
-  proofOfAddressScenarios()
-  accessibilityScenarios()
 })
