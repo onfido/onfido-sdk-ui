84ac768ea8fd45eb35d6e669397f299bf93f63c0
Bart Ziemba <Bart.Ziemba@onfido.com>
2020-06-09 09:35:09 +0100

    Remove comment

--- test/specs/regions.js
+++ test/specs/regions.js
@@ -3,7 +3,6 @@ import { regions } from '../config.json'
 import { regionsScenarios } from './scenarios/regions'
 
 describe('Happy Paths on Chrome',() => {
-  // Multiple language scenarios
   regions.forEach((regions) => {
     regionsScenarios(regions)
   });
