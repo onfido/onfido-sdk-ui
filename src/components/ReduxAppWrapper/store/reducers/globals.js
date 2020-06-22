929023d872b7b8e5dc06aa4415b90c1fa1a1e1d5
Kimmi Gan <kimmi.gan@onfido.com>
2020-06-22 15:33:58 +0100

    Add showCobrandLogo to Redux store with actions

--- src/components/ReduxAppWrapper/store/reducers/globals.js
+++ src/components/ReduxAppWrapper/store/reducers/globals.js
@@ -13,6 +13,7 @@ const initialState = {
   deviceHasCameraSupport: false,
   // This prevents logo from being shown before state can be updated to hide it.
   hideOnfidoLogo: true,
+  cobrandLogo: false,
   urls: {
     onfido_api_url: `${process.env.ONFIDO_API_URL}`,
     telephony_url: `${process.env.SMS_DELIVERY_URL}`,
@@ -61,6 +62,8 @@ export default function globals(state = initialState, action) {
       }
     case constants.HIDE_ONFIDO_LOGO:
       return {...state, hideOnfidoLogo: action.payload}
+    case constants.SHOW_COBRANDING:
+      return {...state, showCobranding: action.payload}
     default:
       return state
   }
