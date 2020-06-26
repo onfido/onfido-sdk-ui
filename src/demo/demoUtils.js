28f10f5d2c59b2f540fc1fa4f46fcfda46cc6f35
Stefania Cardenas <stefania.cardenas@onfido.com>
2020-06-26 11:36:35 +0100

    Add mobilePhrases to previewer

--- src/demo/demoUtils.js
+++ src/demo/demoUtils.js
@@ -192,7 +192,10 @@ export const commonLanguages = {
   de: 'de',
   fr: 'fr',
   'custom': {
-    phrases: { 'welcome.title': 'My custom title' }
+    phrases: { 'welcome.title': 'My custom title' },
+    mobilePhrases: {
+      "capture.driving_licence.back.instructions": "Custom instructions"
+    }
   }
 }
 
