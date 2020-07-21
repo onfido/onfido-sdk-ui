b605472e9b92c67c692130d6537b53ddcfb1856d
Kimmi Gan <kimmi.gan@onfido.com>
2020-07-21 10:33:56 +0100

    Drop in Castor button without any changes

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,7 +2,8 @@ import { h } from 'preact'
 import classNames from 'classnames'
 import { isDesktop } from '~utils/index'
 import style from './style.scss'
-
+import { Button } from '@onfido/castor'
+/* Original Button component
 const Button = ({
   className,
   textClassName,
@@ -37,3 +38,36 @@ const Button = ({
 )
 
 export default Button
+*/
+export default ({
+  className,
+  textClassName,
+  variants = [],
+  disabled,
+  children,
+  onClick,
+  ariaLive,
+  ariaRelevant,
+  ariaBusy
+}) => {
+  return (
+    <Button
+      onClick={onClick}
+      disabled={disabled}
+      aria-live={ariaLive}
+      aria-relevant={ariaRelevant}
+      aria-busy={ariaBusy}
+      className={classNames(
+        className,
+        style.button,
+        ...variants.map(v => style['button-' + v]),
+        {
+          [style.hoverDesktop]: isDesktop
+        }
+      )}>
+      <span className={classNames(textClassName, style['button-text'])}>
+        {children}
+      </span>
+    </Button>
+  )
+}
