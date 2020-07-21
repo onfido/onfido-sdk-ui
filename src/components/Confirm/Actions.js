57aeabc60cd7e2a2072b184490eca6803c7a886a
Kimmi Gan <kimmi.gan@onfido.com>
2020-07-21 18:35:40 +0100

    Remove redundant class, style

--- src/components/Confirm/Actions.js
+++ src/components/Confirm/Actions.js
@@ -7,7 +7,7 @@ import style from './style.scss'
 const RetakeAction = localised(({ retakeAction, translate, btnSize }) => (
   <Button
     variant='secondary'
-    className={classNames(style['button-secondary'], style[`button-${btnSize}`])}
+    className={style[`button-${btnSize}`]}
     onClick={retakeAction}
   >
     {translate('confirm.redo')}
