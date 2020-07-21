4aced7d223711e206c0889737f836d25492004ae
Kimmi Gan <kimmi.gan@onfido.com>
2020-07-21 16:52:07 +0100

    Remove old code, button style classes

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,44 +1,8 @@
 import { h } from 'preact'
 import classNames from 'classnames'
-import { isDesktop } from '~utils/index'
 import style from './style.scss'
 import { Button } from '@onfido/castor'
-/* Original Button component
-const Button = ({
-  className,
-  textClassName,
-  variants = [],
-  disabled,
-  children,
-  onClick,
-  ariaLive,
-  ariaRelevant,
-  ariaBusy,
-}) => (
-  <button
-    type="button"
-    aria-live={ariaLive}
-    aria-relevant={ariaRelevant}
-    aria-busy={ariaBusy}
-    disabled={disabled}
-    onClick={onClick}
-    className={classNames(
-      className,
-      style.button,
-      ...variants.map((v) => style['button-' + v]),
-      {
-        [style.hoverDesktop]: isDesktop,
-      }
-    )}
-  >
-    <span className={classNames(textClassName, style['button-text'])}>
-      {children}
-    </span>
-  </button>
-)
 
-export default Button
-*/
 export default ({
   className,
   textClassName,
@@ -59,11 +23,7 @@ export default ({
       aria-busy={ariaBusy}
       className={classNames(
         className,
-        style.button,
-        ...variants.map(v => style['button-' + v]),
-        {
-          [style.hoverDesktop]: isDesktop
-        }
+        ...variants.map(v => style['button-' + v])
       )}>
       <span className={classNames(textClassName, style['button-text'])}>
         {children}
