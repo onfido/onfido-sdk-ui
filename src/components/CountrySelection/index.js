f3eae1dadbc5fe6f98d7bda91d53546bb7147b6f
Kimmi Gan <kimmi.gan@onfido.com>
2020-07-28 18:29:03 +0100

    Add layout, styling and accessible-autocomplete library

--- src/components/CountrySelection/index.js
+++ src/components/CountrySelection/index.js
@@ -7,41 +7,102 @@ import Button from '../Button'
 import { localised } from '../../locales'
 import type { LocalisedType } from '../../locales'
 import { trackComponent } from 'Tracker'
+import { preventDefaultOnClick } from '~utils/index'
 import theme from 'components/Theme/style.scss'
-// import style from './style.scss'
+import style from './style.scss'
+
+import Autocomplete from 'accessible-autocomplete/preact'
+import 'accessible-autocomplete/dist/accessible-autocomplete.min.css'
 
 type Props = {
+  documentType: string,
+  previousStep: () => void,
+  nextStep: () => void,
   translate: (string, ?{}) => string,
-  nextStep: () => void
 } & LocalisedType
 
 type State = {
-  hasSelectedCountry: boolean
+  selectedCountry: string,
+}
+
+const countries = [
+  'France',
+  'Germany',
+  'United Kingdom',
+  'Malawi',
+  'Malaysia',
+  'Madagascar',
+]
+const suggest = (query, populateResults) => {
+  const results = countries
+  const filteredResults = results.filter((result) =>
+    result.toLowerCase().includes(query.toLowerCase())
+  )
+  populateResults(filteredResults)
 }
 
 class CountrySelection extends Component<Props, State> {
-
   state: State = {
-    hasSelectedCountry: false
+    selectedCountry: null,
   }
 
   render() {
-    const { nextStep } = this.props
+    const { previousStep, nextStep, documentType } = this.props
     return (
       <div className={theme.fullHeightContainer}>
         <PageTitle
-          title={'Get title, subtitle from Lokalise'}
-          subTitle={'Country list is based on document type (pre)selected'}
+          title={'TODO: Get copy from Lokalise'}
+          subTitle={`Selected Document Type: ${documentType}`}
         />
         <div
-          className={classNames(theme.thickWrapper, theme.scrollableContent)}
+          className={classNames(
+            theme.thickWrapper,
+            theme.scrollableContent,
+            theme.alignTextLeft
+          )}
         >
-          <p>TODO: country selector UI library, list from Docupedia export</p>
+          <div>
+            <label for="accessible-autocomplete">
+              TODO: country list from Docupedia data JSON based on document type
+              (pre)selected
+            </label>
+            <Autocomplete
+              id="accessible-autocomplete"
+              // cssNamespace={style.countrySelect}
+              required={true}
+              source={suggest}
+              minLength={2}
+              dropdownArrow={() => `<i class="${style.caretIcon}"><i/>`}
+              displayMenu="overlay"
+              templates={{
+                suggestion: (suggestion) =>
+                  suggestion &&
+                  `<span class="${style.countryLabel}">${suggestion}</span>`,
+              }}
+              onConfirm={(selectedCountry) => {
+                console.log('selected country:', selectedCountry)
+                this.setState({ selectedCountry })
+              }}
+            />
+          </div>
+          <div className={style.helpTextContainer}>
+            <i className={style.helpIcon} />
+            <span className={style.helpText}>
+              If you can’t find your country,{' '}
+            </span>
+            <a
+              href="#"
+              className={theme.link}
+              onClick={preventDefaultOnClick(previousStep)}
+            >
+              try another document
+            </a>
+          </div>
         </div>
         <div className={classNames(theme.thickWrapper)}>
           <Button
             variants={['centered', 'primary', 'lg']}
-            disabled={!this.state.hasSelectedCountry}
+            disabled={!this.state.selectedCountry}
             onClick={nextStep}
           >
             TODO: same as above
@@ -50,7 +111,6 @@ class CountrySelection extends Component<Props, State> {
       </div>
     )
   }
-
 }
 
 export default trackComponent(localised(CountrySelection))
