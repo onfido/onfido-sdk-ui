83fd01e6c5957ca0773d57ae3789a33cd1203f57
Kimmi Gan <kimmi.gan@onfido.com>
2020-07-28 12:51:31 +0100

    Set up CountrySelection component

--- src/components/CountrySelection/index.js
+++ src/components/CountrySelection/index.js
@@ -1,13 +1,56 @@
+// @flow
+import * as React from 'react'
 import { h, Component } from 'preact'
-// import { localised } from '../../locales'
+import classNames from 'classnames'
+import PageTitle from '../PageTitle'
+import Button from '../Button'
+import { localised } from '../../locales'
+import type { LocalisedType } from '../../locales'
+import { trackComponent } from 'Tracker'
+import theme from 'components/Theme/style.scss'
 // import style from './style.scss'
 
-class CountrySelection extends Component {
+type Props = {
+  translate: (string, ?{}) => string,
+  nextStep: () => void
+} & LocalisedType
+
+type State = {
+  hasSelectedCountry: boolean
+}
+
+class CountrySelection extends Component<Props, State> {
+
+  state: State = {
+    hasSelectedCountry: false
+  }
 
   render() {
-    return <div>TODO</div>
+    const { nextStep } = this.props
+    return (
+      <div className={theme.fullHeightContainer}>
+        <PageTitle
+          title={'Get title, subtitle from Lokalise'}
+          subTitle={'Country list is based on document type (pre)selected'}
+        />
+        <div
+          className={classNames(theme.thickWrapper, theme.scrollableContent)}
+        >
+          <p>TODO: country selector UI library, list from Docupedia export</p>
+        </div>
+        <div className={classNames(theme.thickWrapper)}>
+          <Button
+            variants={['centered', 'primary', 'lg']}
+            disabled={!this.state.hasSelectedCountry}
+            onClick={nextStep}
+          >
+            TODO: same as above
+          </Button>
+        </div>
+      </div>
+    )
   }
 
 }
 
-export default CountrySelection
+export default trackComponent(localised(CountrySelection))
