1f8a92ad3b8dd11db5cdca9854e26d8ec383eb4b
Kimmi Gan <kimmi.gan@onfido.com>
2020-07-30 15:47:27 +0100

    Use Docupedia document data for country list

---
+++ src/supported-documents/index.js
@@ -0,0 +1,36 @@
+// @flow
+import supportedDrivingLicences from './supported-docs-driving_licence.json'
+import supportedNationalIDCards from './supported-docs-national_identity_card.json'
+
+export type CountryType = {
+  countryCode: string,
+  name: string,
+  icon?: string,
+}
+
+export const getSupportedCountriesForDocument = (documentType) => {
+  switch (documentType) {
+    case 'driving_licence':
+      return getCountriesList(supportedDrivingLicences)
+    case 'national_identity_card':
+      return getCountriesList(supportedNationalIDCards)
+    default:
+      throw new Error('Unsupported documentType:', documentType)
+  }
+}
+
+const getCountriesList = (supportedDocsData) => {
+  const countriesList = supportedDocsData.map((docData) => {
+    const { sourceData } = docData
+    return {
+      countryCode: sourceData.country_alpha2,
+      name: sourceData.country,
+    }
+  })
+  const uniqueCountriesList = [
+    ...new Map(
+      countriesList.map((country) => [country.countryCode, country])
+    ).values(),
+  ]
+  return uniqueCountriesList.sort((a, b) => a.name.localeCompare(b.name))
+}
