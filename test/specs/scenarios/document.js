2018605378558fb3e502d0d53e3f314c751843c9
Kimmi Gan <kimmi.gan@onfido.com>
2020-09-03 11:18:19 +0100

    Clearer description for 1st of 2 retry upload tests

--- test/specs/scenarios/document.js
+++ test/specs/scenarios/document.js
@@ -135,7 +135,7 @@ export const documentScenarios = async (lang) => {
         confirm.verifyNoDocumentError(copy)
       })
 
-      it('should upload a document on retry', async () => {
+      it('should upload a document on retry after uploading a non-doc image', async () => {
         goToPassportUploadScreen(
           driver,
           welcome,
