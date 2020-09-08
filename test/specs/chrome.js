10bdcf1588334696037adb8b6ddc05caa1bf35de
Ethan Nguyen <ethan.nguyen@onfido.com>
2020-09-08 16:05:04 +0100

    Try running face scenario only

--- test/specs/chrome.js
+++ test/specs/chrome.js
@@ -1,36 +1,38 @@
 import { describe } from '../utils/mochaw'
-import { supportedLanguages, fullTestCoverageLanguages } from '../config.json'
-import { welcomeScenarios } from './scenarios/welcome'
-import { documentSelectorScenarios } from './scenarios/documentSelector'
-import { countrySelectorScenarios } from './scenarios/countrySelector'
-import { documentScenarios } from './scenarios/document'
+import {
+  /* supportedLanguages, */ fullTestCoverageLanguages,
+} from '../config.json'
+// import { welcomeScenarios } from './scenarios/welcome'
+// import { documentSelectorScenarios } from './scenarios/documentSelector'
+// import { countrySelectorScenarios } from './scenarios/countrySelector'
+// import { documentScenarios } from './scenarios/document'
 import { faceScenarios } from './scenarios/face'
-import { crossDeviceScenarios } from './scenarios/crossDevice'
-import { proofOfAddressScenarios } from './scenarios/proofOfAddress'
-import { modalScenarios } from './scenarios/modal'
-import { navigationScenarios } from './scenarios/navigation'
-import { hostAppHistoryScenarios } from './scenarios/hostAppHistory'
-import { accessibilityScenarios } from './scenarios/accessibility'
+// import { crossDeviceScenarios } from './scenarios/crossDevice'
+// import { proofOfAddressScenarios } from './scenarios/proofOfAddress'
+// import { modalScenarios } from './scenarios/modal'
+// import { navigationScenarios } from './scenarios/navigation'
+// import { hostAppHistoryScenarios } from './scenarios/hostAppHistory'
+// import { accessibilityScenarios } from './scenarios/accessibility'
 
 describe('Happy Paths on Chrome', () => {
   // Multiple language scenarios
   fullTestCoverageLanguages.forEach((lang) => {
-    welcomeScenarios(lang)
-    documentSelectorScenarios(lang)
-    countrySelectorScenarios(lang)
-    documentScenarios(lang)
+    // welcomeScenarios(lang)
+    // documentSelectorScenarios(lang)
+    // countrySelectorScenarios(lang)
+    // documentScenarios(lang)
     faceScenarios(lang)
-    crossDeviceScenarios(lang)
-    modalScenarios(lang)
-    navigationScenarios(lang)
+    // crossDeviceScenarios(lang)
+    // modalScenarios(lang)
+    // navigationScenarios(lang)
   })
   // Note: The SDK works also with language tags that do not include region (e.g. 'en', 'es')
   // We are passing the region here so we can fetch the right json file path (e.g. `en_US/en_US.json`).
-  supportedLanguages.forEach((lang) => {
+  /* supportedLanguages.forEach((lang) => {
     welcomeScenarios(lang)
-  })
+  }) */
   // PoA is only available in en
-  proofOfAddressScenarios()
-  accessibilityScenarios()
-  hostAppHistoryScenarios()
+  // proofOfAddressScenarios()
+  // accessibilityScenarios()
+  // hostAppHistoryScenarios()
 })
