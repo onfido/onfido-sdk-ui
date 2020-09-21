fed5bbe5c0c92ce6083f568a897b6cee07089397
Kimmi Gan <kimmi.gan@onfido.com>
2020-09-21 18:10:44 +0100

    add eslint ignore for skipped tests

--- test/specs/scenarios/document.js
+++ test/specs/scenarios/document.js
@@ -200,6 +200,7 @@ export const documentScenarios = async (lang) => {
 
       // @TODO re-enable image-quality related test
       // See more: https://jira.onfido.co.uk/browse/CX-5545
+      // eslint-disable-next-line jest/no-disabled-tests
       it.skip('should return image quality message on front of doc', async () => {
         driver.get(`${baseUrl}&async=false`)
         welcome.continueToNextStep()
@@ -238,6 +239,7 @@ export const documentScenarios = async (lang) => {
 
       // @TODO re-enable image-quality related test
       // See more: https://jira.onfido.co.uk/browse/CX-5545
+      // eslint-disable-next-line jest/no-disabled-tests
       it.skip('should return image quality message on back of doc', async () => {
         driver.get(`${baseUrl}&async=false`)
         welcome.continueToNextStep()
