a94260d6dfba943a9ba47dc165781bdb8a16916c
Kimmi Gan <kimmi.gan@onfido.com>
2020-09-22 15:23:48 +0100

    Enable a small UI test scenario with document upload

--- test/specs/chrome.js
+++ test/specs/chrome.js
@@ -7,7 +7,7 @@ import { countrySelectorScenarios } from './scenarios/countrySelector'
 // import { faceScenarios } from './scenarios/face'
 // import { crossDeviceScenarios } from './scenarios/crossDevice'
 import { modalScenarios } from './scenarios/modal'
-// import { navigationScenarios } from './scenarios/navigation'
+import { navigationScenarios } from './scenarios/navigation'
 // import { proofOfAddressScenarios } from './scenarios/proofOfAddress'
 import { hostAppHistoryScenarios } from './scenarios/hostAppHistory'
 // import { accessibilityScenarios } from './scenarios/accessibility'
@@ -22,7 +22,7 @@ describe('Happy Paths on Chrome', () => {
     // faceScenarios(lang)
     // crossDeviceScenarios(lang)
     modalScenarios(lang)
-    // navigationScenarios(lang)
+    navigationScenarios(lang)
   })
   // Note: The SDK works also with language tags that do not include region (e.g. 'en', 'es')
   // We are passing the region here so we can fetch the right json file path (e.g. `en_US/en_US.json`).
