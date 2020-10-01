e677800fc27da0512ea215b1795de7820e14ab57
Kimmi Gan <kimmi.gan@onfido.com>
2020-10-01 16:29:48 +0100

    Undo conditional rendering of title (doesn't work as expected)

--- src/components/Camera/index.js
+++ src/components/Camera/index.js
@@ -58,7 +58,7 @@ const CameraPure = ({
   fallbackHeight,
 }: Props) => (
   <div className={classNames(style.camera, className)}>
-    {!renderError && renderTitle}
+    {renderTitle}
     <div className={classNames(style.container, containerClassName)}>
       <div
         className={style.webcamContainer}
