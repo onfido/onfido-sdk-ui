fb565c8d0ab934860cb3e7b1033680c8835d3f37
Ethan Nguyen <ethan.nguyen@onfido.com>
2020-10-07 17:30:02 +0100

    Refine unwrap_lokalise script

--- scripts/unwrap_lokalise.js
+++ scripts/unwrap_lokalise.js
@@ -16,6 +16,13 @@ const fs = require('fs')
 const path = require('path')
 
 const BASE_DIR = '../src/locales'
+const COLORS = {
+  RESET: '\x1b[0m',
+  BLUE: '\x1b[34m',
+  GREEN: '\x1b[32m',
+  RED: '\x1b[31m',
+  YELLOW: '\x1b[33m',
+}
 const COMMAND = 'unwrap_lokalise'
 const COMMAND_VERSION = 'v1.0.0'
 const TAB_WIDTH = 4
@@ -24,42 +31,27 @@ const PARSED_ARGS = {
   wrapKey: 'onfido',
 }
 
+/* Helper functions */
+function buildColorMessage(message, ...colors) {
+  return [...colors, message, COLORS.RESET].join('')
+}
+
 function buildEscapedJson(json) {
   const stringified = JSON.stringify(json, null, TAB_WIDTH)
   const escaped = stringified.replace(/\//gi, '\\/')
   return `${escaped}\n`
 }
 
-function unwrapFile(filePath) {
-  const { wrapKey } = PARSED_ARGS
-  const fileData = fs.readFileSync(filePath)
-  const json = JSON.parse(fileData)
-  const unwrappedJson = json[wrapKey] ? json[wrapKey] : json
-  fs.writeFileSync(filePath, buildEscapedJson(unwrappedJson))
+function printError(message) {
+  console.error(buildColorMessage(`Error: ${message}`, COLORS.RED))
 }
 
-// For test purposes
-function wrapFile(filePath) {
-  const { wrapKey } = PARSED_ARGS
-  const fileData = fs.readFileSync(filePath)
-  const json = JSON.parse(fileData)
+function verboseLogging(...args) {
+  if (!PARSED_ARGS.verbose) {
+    return
+  }
 
-  const wrappedJson = json[wrapKey] ? json : { [wrapKey]: json }
-  fs.writeFileSync(filePath, buildEscapedJson(wrappedJson))
-}
-
-function readDirRecursive(dirPath, extName) {
-  const foundPaths = fs.readdirSync(dirPath).flatMap((file) => {
-    const childPath = path.resolve(dirPath, file)
-
-    if (fs.statSync(childPath).isDirectory()) {
-      return readDirRecursive(childPath, extName)
-    } else if (path.extname(childPath) === extName) {
-      return childPath
-    }
-  })
-
-  return foundPaths.filter((file) => file)
+  console.log(...args)
 }
 
 function parseOptionValue(name, trigger, params) {
@@ -89,6 +81,11 @@ function parseArgs() {
         Object.assign(PARSED_ARGS, { testWrap: true })
         break
 
+      case '--verbose':
+      case '-v':
+        Object.assign(PARSED_ARGS, { verbose: true })
+        break
+
       case '--version':
       case '-V':
         printVersion()
@@ -109,8 +106,12 @@ function printVersion() {
   process.exit(0)
 }
 
-function printHelp() {
-  console.log(`${COMMAND} - unwrap JSON files pulled from Lokalise
+function printHelp(errorMessage) {
+  if (errorMessage) {
+    printError(`${errorMessage}\n`)
+  }
+
+  console.info(`${COMMAND} - unwrap JSON files pulled from Lokalise
 
 Usage:
       ${COMMAND} [flags]
@@ -121,21 +122,64 @@ Examples:
       ${COMMAND} --test-wrap
 
 Available flags:
-  --wrap-key, -k          Specify key to unwrap, default to 'onfido'.
-  --test-wrap, -t         To wrap files with --wrap-key|-k value.
+  -k, --wrap-key          Specify key to unwrap, default to 'onfido'.
+  -t, --test-wrap         To wrap files with --wrap-key|-k value.
                           Do nothing if they are already wrapped.
-  --help, -h              Print this message.
+  -v, --verbose           Verbose logging.
+  -h, --help              Print this message.
   `)
+
+  process.exit(errorMessage ? 1 : 0)
+}
+
+/* Main functions */
+function unwrapFile(filePath) {
+  verboseLogging(`Unwrapping file ${buildColorMessage(filePath, COLORS.GREEN)}`)
+  const { wrapKey } = PARSED_ARGS
+  const fileData = fs.readFileSync(filePath)
+  const json = JSON.parse(fileData)
+  const unwrappedJson = json[wrapKey] ? json[wrapKey] : json
+  fs.writeFileSync(filePath, buildEscapedJson(unwrappedJson))
+}
+
+function wrapFile(filePath) {
+  verboseLogging(`Wrapping file ${buildColorMessage(filePath, COLORS.GREEN)}`)
+  const { wrapKey } = PARSED_ARGS
+  const fileData = fs.readFileSync(filePath)
+  const json = JSON.parse(fileData)
+
+  const wrappedJson = json[wrapKey] ? json : { [wrapKey]: json }
+  fs.writeFileSync(filePath, buildEscapedJson(wrappedJson))
+}
+
+function readDirRecursive(dirPath, extName) {
+  const foundPaths = fs.readdirSync(dirPath).flatMap((file) => {
+    const childPath = path.resolve(dirPath, file)
+
+    if (fs.statSync(childPath).isDirectory()) {
+      return readDirRecursive(childPath, extName)
+    } else if (path.extname(childPath) === extName) {
+      return childPath
+    }
+  })
+
+  return foundPaths.filter((file) => file)
 }
 
 function main() {
   parseArgs()
 
-  const { testWrap } = PARSED_ARGS
+  const { testWrap, wrapKey } = PARSED_ARGS
 
   const filePaths = readDirRecursive(path.resolve(__dirname, BASE_DIR), '.json')
   filePaths.forEach(testWrap ? wrapFile : unwrapFile)
-  console.log('Unwrapping files done')
+
+  console.info(
+    `All files are ${buildColorMessage(
+      testWrap ? 'wrapped' : 'unwrapped',
+      COLORS.YELLOW
+    )} with key ${buildColorMessage(wrapKey, COLORS.BLUE)}`
+  )
 }
 
 main()
