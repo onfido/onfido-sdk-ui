992e83d34bb2e9a6f46a63a7d845649c4e08a644
Ethan Nguyen <ethan.nguyen@onfido.com>
2020-10-16 15:56:54 +0100

    Mock demoUtils to initialise Onfido SDK properly

--- src/demo/demo.test.js
+++ src/demo/demo.test.js
@@ -2,6 +2,29 @@ import { h } from 'preact'
 import { mount } from 'enzyme'
 import { expect } from 'chai'
 
+jest.mock('./demoUtils', () => ({
+  getInitSdkOptions: jest.fn().mockReturnValue({}),
+  queryParamToValueString: { useHistory: false },
+  getTokenFactoryUrl: () => 'https://token-factory.onfido.com/sdk_token',
+  getToken: jest
+    .fn()
+    .mockImplementation((_hasPreview, _url, _eventEmitter, onSuccess) =>
+      onSuccess('TEST_TOKEN')
+    ),
+}))
+
+// when the Onfido SDK is imported Woopra needs to be mocked
+const mockWoopraFn = jest.fn()
+
+jest.mock('../Tracker/safeWoopra', () =>
+  jest.fn().mockImplementation(() => ({
+    init: () => mockWoopraFn,
+    config: () => mockWoopraFn,
+    identify: () => mockWoopraFn,
+    track: () => mockWoopraFn,
+  }))
+)
+
 describe('Mount Demo App', () => {
   let Demo = null
 
@@ -22,20 +45,6 @@ describe('Mount Demo App', () => {
   })
 
   describe('without mocking Onfido SDK', () => {
-    beforeEach(() => {
-      // when the Onfido SDK is imported Woopra needs to be mocked
-      const mockWoopraFn = jest.fn()
-
-      jest.mock('../Tracker/safeWoopra', () =>
-        jest.fn().mockImplementation(() => ({
-          init: () => mockWoopraFn,
-          config: () => mockWoopraFn,
-          identify: () => mockWoopraFn,
-          track: () => mockWoopraFn,
-        }))
-      )
-    })
-
     it('mounts the Onfido Demo without crashing', () => {
       // the component needs to be assigned to a lowercase variable to work!
       const sdk = <Demo />
