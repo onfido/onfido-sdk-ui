8f0b49fa5ee74fb62cb0e09af833ead9be7420fa
Ethanify <ethan.nguyen@onfido.com>
2020-11-26 14:34:27 +0000

    Add mock server for ui tests

---
+++ test/mock-server/server.ts
@@ -0,0 +1,16 @@
+import { Application, Router } from 'https://deno.land/x/oak/mod.ts'
+
+const router = new Router()
+router
+  .get('/', (context) => {
+    context.response.body = 'v1.0.0'
+  })
+  .get('/ping', (context) => {
+    context.response.body = { message: 'pong' }
+  })
+
+const app = new Application()
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+await app.listen({ port: 8080 })
