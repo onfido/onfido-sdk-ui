5e48cd9fcab1b619947e88e79618eaaa4bb32ed0
Ethanify <ethan.nguyen@onfido.com>
2020-11-26 12:52:40 +0000

    Focus simple tests to try mocked requests

--- test/specs/scenarios/documentSelector.js
+++ test/specs/scenarios/documentSelector.js
@@ -1,3 +1,4 @@
+import { before } from 'mocha'
 import { describe, it } from '../../utils/mochaw'
 import { localhostUrl } from '../../config.json'
 
@@ -6,12 +7,16 @@ const options = {
 }
 
 export const documentSelectorScenarios = async (lang) => {
-  describe(
+  describe.only(
     `DOCUMENT SELECTOR scenarios in ${lang}`,
     options,
     ({ driver, pageObjects }) => {
       const { welcome, documentSelector, basePage } = pageObjects
       const copy = basePage.copy(lang)
+
+      before(() => {
+        console.log('invoke beforeAll')
+      })
 
       it('should verify UI elements on the document selection screen', async () => {
         driver.get(`${localhostUrl}?language=${lang}`)
