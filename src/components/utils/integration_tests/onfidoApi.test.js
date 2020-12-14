f77cd60afbdb80f3934324f94aecbaeb335d943e
Kimmi Gan <kimmi.gan@onfido.com>
2020-12-14 16:33:48 +0000

    Add integration test for video challenge request

---
+++ src/components/utils/integration_tests/onfidoApi.test.js
@@ -0,0 +1,91 @@
+import { requestChallenges } from '../onfidoApi'
+
+const apiUrl = 'https://api.onfido.com'
+const expiredJwtToken =
+  'eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MDc3MDc1NTEsInBheWxvYWQiOiJycUFvMEtSbXdtWlViWFRLUHp2TXlTaGZtelNDNVhtRWM3aVZ4ZzJ5b2NQbEQrbk9rQmxtcHBaK0FCKzBcbkwveEtYRm4yeTBNZGxNNXRXVE5HeVNVSG5nPT1cbiIsInV1aWQiOiJpd29rRlZlZEcxOCIsImVudGVycHJpc2VfZmVhdHVyZXMiOnsiY29icmFuZCI6dHJ1ZSwiaGlkZU9uZmlkb0xvZ28iOnRydWV9LCJ1cmxzIjp7InRlbGVwaG9ueV91cmwiOiJodHRwczovL3RlbGVwaG9ueS5vbmZpZG8uY29tIiwiZGV0ZWN0X2RvY3VtZW50X3VybCI6Imh0dHBzOi8vc2RrLm9uZmlkby5jb20iLCJzeW5jX3VybCI6Imh0dHBzOi8vc3luYy5vbmZpZG8uY29tIiwiYXV0aF91cmwiOiJodHRwczovL2VkZ2UuYXBpLm9uZmlkby5jb20iLCJvbmZpZG9fYXBpX3VybCI6Imh0dHBzOi8vYXBpLm9uZmlkby5jb20iLCJob3N0ZWRfc2RrX3VybCI6Imh0dHBzOi8veGQub25maWRvLmNvbSJ9fQ.Ece4NQpZLsPzsgd6W4kDYNugW66W_Fl__jfz6d96WEI'
+let jwtToken = null
+
+const getTestJwtToken = (resolve) => {
+  const tokenFactoryUrl = 'https://token-factory.onfido.com/sdk_token'
+  const request = new XMLHttpRequest()
+  request.open('GET', tokenFactoryUrl, true)
+  request.setRequestHeader(
+    'Authorization',
+    `BASIC ${process.env.SDK_TOKEN_FACTORY_SECRET}`
+  )
+  request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+      const data = JSON.parse(request.responseText)
+      return resolve(data.message)
+    }
+  }
+  request.send()
+}
+
+describe('API requestChallenges endpoint', () => {
+  beforeEach(async () => {
+    jwtToken = await new Promise((resolve) => getTestJwtToken(resolve))
+  })
+
+  afterEach(() => {
+    jwtToken = null
+  })
+
+  test('requestChallenges returns a random 3-digit number challenge and a face turn challenge', async () => {
+    const onSuccessCallback = (resolve, response) => {
+      const { challenge } = response.data
+      expect(challenge).toHaveLength(2)
+      // Example challenge response (order of challenge types is random):
+      // [{"query": "turnLeft", "type": "movement"}, {"query": [1, 9, 0], "type": "recite"}]
+      const expectedTypeRegex = /(movement|recite)/
+      expect(challenge).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            type: expect.stringMatching(expectedTypeRegex),
+            query: expect.anything(),
+          }),
+          expect.objectContaining({
+            type: expect.stringMatching(expectedTypeRegex),
+            query: expect.anything(),
+          }),
+        ])
+      )
+      resolve()
+    }
+    await new Promise((resolve, reject) =>
+      requestChallenges(
+        apiUrl,
+        jwtToken,
+        (response) => onSuccessCallback(resolve, response),
+        (error) => reject(error)
+      )
+    )
+  })
+  // eslint-disable-next-line jest/no-disabled-tests
+  test.skip('requestChallenges returns an error if request is made an expired JWT token', async () => {
+    const onErrorCallback = (reject, error) => {
+      // FIXME
+      // console.error('error:', error)
+      expect(error).objectContaining({
+        status: 401,
+        response: {
+          error: {
+            fields: {},
+            message: 'The token has expired, please request a new one',
+            type: 'expired_token',
+          },
+        },
+      })
+      reject(error)
+    }
+
+    await new Promise((resolve, reject) =>
+      requestChallenges(
+        apiUrl,
+        expiredJwtToken,
+        (response) => resolve(response),
+        (error) => onErrorCallback(reject, error)
+      )
+    )
+  })
+})
