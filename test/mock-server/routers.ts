427b9e6b4bc5fac7d950b23b84b549f4f4e981a1
Ethanify <thanhnx.605@gmail.com>
2020-12-14 14:26:42 +0000

    Re-enable all tests

--- test/mock-server/routers.ts
+++ test/mock-server/routers.ts
@@ -6,6 +6,20 @@ import responses from './responses.ts'
 
 const applicantId = '742af521-2c8f-4b54-a8b7-01c962849d5b'
 
+/**
+ * This helper is to match against uploaded files
+ * instead of using switch-case
+ * because IE11 on Windows submit files in a weird format
+ * e.g: C:Windowsproxyd0bd7093-f21a-4579-8707-f7aabd4948c2upload4900467381210572080filepassport.jpg
+ */
+const matchUploadedFile = (
+  uploadedFileName: string,
+  matchFileNames: string[]
+): boolean => {
+  const regex = new RegExp(`(${matchFileNames.join('|')})$`)
+  return uploadedFileName.match(regex) != null
+}
+
 const buildDocumentsResponse = (
   formData: FormDataBody
 ): { body: object; status: Status } | undefined => {
@@ -13,80 +27,103 @@ const buildDocumentsResponse = (
     return
   }
 
-  const { type, sdk_metadata } = formData.fields
-  const sdkMetadata = JSON.parse(sdk_metadata)
-
   const [{ originalName }] = formData.files
   console.log('Uploaded file:', originalName)
 
-  switch (originalName) {
-    case 'driving_licence.png':
-    case 'uk_driving_licence.png':
-      return {
-        body: Object.assign(responses.api.v3.documents.driving_licence_front, {
-          applicant_id: applicantId,
-        }),
-        status: Status.OK,
-      }
-    case 'back_driving_licence.jpg':
-    case 'back_driving_licence.png':
-      return {
-        body: Object.assign(responses.api.v3.documents.driving_licence_back, {
-          applicant_id: applicantId,
-        }),
-        status: Status.OK,
-      }
-    case 'identity_card_with_cut-off.png':
-    case 'identity_card_with_cut-off_glare.png':
-      return {
-        body: Object.assign(responses.api.v3.documents.cut_off, {
-          applicant_id: applicantId,
-        }),
-        status: Status.OK,
-      }
-    case 'identity_card_with_glare.jpg':
+  if (
+    matchUploadedFile(originalName, [
+      'driving_licence.png',
+      'uk_driving_licence.png',
+    ])
+  ) {
+    return {
+      body: Object.assign(responses.api.v3.documents.driving_licence_front, {
+        applicant_id: applicantId,
+      }),
+      status: Status.OK,
+    }
+  }
+
+  if (
+    matchUploadedFile(originalName, [
+      'back_driving_licence.jpg',
+      'back_driving_licence.png',
+    ])
+  ) {
+    return {
+      body: Object.assign(responses.api.v3.documents.driving_licence_back, {
+        applicant_id: applicantId,
+      }),
+      status: Status.OK,
+    }
+  }
+
+  if (
+    matchUploadedFile(originalName, [
+      'identity_card_with_cut-off.png',
+      'identity_card_with_cut-off_glare.png',
+    ])
+  ) {
+    return {
+      body: Object.assign(responses.api.v3.documents.cut_off, {
+        applicant_id: applicantId,
+      }),
+      status: Status.OK,
+    }
+  }
+
+  if (matchUploadedFile(originalName, ['identity_card_with_glare.jpg'])) {
+    return {
+      body: Object.assign(responses.api.v3.documents.glare, {
+        applicant_id: applicantId,
+      }),
+      status: Status.OK,
+    }
+  }
+
+  if (
+    matchUploadedFile(originalName, [
+      'national_identity_card.jpg',
+      'national_identity_card.pdf',
+    ])
+  ) {
+    return {
+      body: Object.assign(responses.api.v3.documents.id_card, {
+        applicant_id: applicantId,
+      }),
+      status: Status.OK,
+    }
+  }
+
+  if (matchUploadedFile(originalName, ['passport.jpg', 'passport.pdf'])) {
+    return {
+      body: Object.assign(responses.api.v3.documents.passport, {
+        applicant_id: applicantId,
+        file_name: originalName,
+      }),
+      status: Status.OK,
+    }
+  }
+
+  if (originalName === 'blob') {
+    const { type, sdk_metadata } = formData.fields
+    const sdkMetadata = JSON.parse(sdk_metadata)
+
+    if (type === 'passport' && sdkMetadata.imageResizeInfo) {
       return {
         body: Object.assign(responses.api.v3.documents.glare, {
           applicant_id: applicantId,
         }),
         status: Status.OK,
       }
-    case 'national_identity_card.jpg':
-    case 'national_identity_card.pdf':
-      return {
-        body: Object.assign(responses.api.v3.documents.id_card, {
-          applicant_id: applicantId,
-        }),
-        status: Status.OK,
-      }
-    case 'passport.jpg':
-    case 'passport.pdf':
-      return {
-        body: Object.assign(responses.api.v3.documents.passport, {
-          applicant_id: applicantId,
-          file_name: originalName,
-        }),
-        status: Status.OK,
-      }
-    case 'blob': {
-      if (type === 'passport' && sdkMetadata.imageResizeInfo) {
-        return {
-          body: Object.assign(responses.api.v3.documents.glare, {
-            applicant_id: applicantId,
-          }),
-          status: Status.OK,
-        }
-      }
     }
-    case 'face.jpeg':
-    case 'llama.pdf':
-    default:
-      return {
-        body: Object.assign(responses.api.v3.documents.no_doc, {
-          applicant_id: applicantId,
-        }),
-        status: Status.UnprocessableEntity,
-      }
+  }
+
+  return {
+    body: Object.assign(responses.api.v3.documents.no_doc, {
+      applicant_id: applicantId,
+    }),
+    status: Status.UnprocessableEntity,
   }
 }
 
@@ -97,32 +134,31 @@ const buildLivePhotosResponse = (
     return
   }
 
-  const { type, sdk_metadata } = formData.fields
-  const sdkMetadata = JSON.parse(sdk_metadata)
-
   const [{ originalName }] = formData.files
   console.log('Uploaded file:', originalName)
 
-  switch (originalName) {
-    case 'face.jpeg':
-    case 'applicant_selfie.png':
+  if (matchUploadedFile(originalName, ['face.jpeg', 'applicant_selfie.png'])) {
+    return {
+      body: responses.api.v3.live_photos.face,
+      status: Status.OK,
+    }
+  }
+
+  if (originalName === 'blob') {
+    const { type, sdk_metadata } = formData.fields
+    const sdkMetadata = JSON.parse(sdk_metadata)
+
+    if (type === 'passport' && sdkMetadata.imageResizeInfo) {
       return {
         body: responses.api.v3.live_photos.face,
         status: Status.OK,
       }
-    case 'blob': {
-      if (sdkMetadata.imageResizeInfo) {
-        return {
-          body: responses.api.v3.live_photos.face,
-          status: Status.OK,
-        }
-      }
     }
-    default:
-      return {
-        body: responses.api.v3.live_photos.unsupported,
-        status: Status.UnprocessableEntity,
-      }
+  }
+
+  return {
+    body: responses.api.v3.live_photos.unsupported,
+    status: Status.UnprocessableEntity,
   }
 }
 
@@ -148,6 +184,9 @@ apiRouter
     try {
       const body = context.request.body({ type: 'form-data' })
       const formData = await body.value.read()
+
+      console.log('formData.fields:', formData.fields)
+      console.log('formData.files:', formData.files)
 
       const responseData = buildDocumentsResponse(formData)
 
