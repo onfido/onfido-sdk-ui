28819b35e8330b0545b8383610c7d9ba14c7013f
Kimmi Gan <kimmi.gan@onfido.com>
2020-12-16 16:14:25 +0000

    Add happy path test for uploadDocument

--- src/components/utils/integration_tests/onfidoApi.test.js
+++ src/components/utils/integration_tests/onfidoApi.test.js
@@ -6,6 +6,7 @@ import {
   requestChallenges,
   uploadLiveVideo,
 } from '../onfidoApi'
+import fs from 'fs'
 
 const API_URL = 'https://api.onfido.com'
 const EXPIRED_JWT_TOKEN =
@@ -33,6 +34,101 @@ beforeEach(async () => {
   jwtToken = await new Promise((resolve) => getTestJwtToken(resolve))
 })
 
+describe('API uploadDocument endpoint', () => {
+  // TODO happy path, failure scenarios
+  test('uploadDocument returns expected response on successful upload', () => {
+    const onSuccessCallback = (response) => {
+      console.log('* uploadDocument response:', response)
+      console.log('* uploadDocument expectedApiResponse:', expectedApiResponse)
+      const expectedApiResponse = {
+        applicant_id: 'f31503c7-7764-42b3-a11a-596121ee8492',
+        created_at: '2020-12-15T15:31:21Z', // Date ISO string
+        download_href:
+          '/v3/documents/0fa0b9ac-df61-4d30-bb0d-9637cece2a0d/download',
+        file_name: 'mock-document.jpg',
+        file_size: 50000,
+        file_type: 'jpg',
+        href: '/v3/documents/0fa0b9ac-df61-4d30-bb0d-9637cece2a0d',
+        id: '0fa0b9ac-df61-4d30-bb0d-9637cece2a0d',
+        issuing_country: null,
+        sdk_warnings: {
+          detect_blur: { valid: true },
+          detect_cutoff: { valid: true },
+          detect_glare: { valid: true },
+        },
+        image_quality: {
+          quality: 'good',
+          breakdown: {
+            blur: {
+              has_blur: false,
+              max: 1,
+              min: 0,
+              score: 0.585895717144012,
+              threshold: 0.2012,
+            },
+            cutoff: {
+              has_cutoff: false,
+              max: 1,
+              min: 0,
+              score: 0.0520833333333333,
+              threshold: 0.015,
+            },
+            document: {
+              detection_score: 0.999860764,
+              has_document: true,
+              max: 1,
+              min: 0,
+              threshold: 0.9887919909,
+            },
+          },
+          image_quality_uuid: 'b56cc15c-4cc2-4aa3-b191-3193c3c7900b',
+        },
+        side: 'front',
+        type: 'passport',
+      }
+      expect(response).toMatchObject(expectedApiResponse) // should fail!
+    }
+    fs.readFile(
+      `${__dirname}/./../../../../test/resources/passport.jpg`,
+      (err, data) => {
+        if (err) throw err
+        const testFile = new File(data, 'passport.jpg')
+        const documentData = {
+          file: testFile,
+          sdkMetadata: {
+            captureMethod: 'html5',
+            deviceType: 'desktop',
+            imageResizeInfo: null,
+            isCrossDeviceFlow: false,
+            system: {
+              os: 'Macintosh',
+              os_version: '10.15.7',
+              browser: 'Chrome',
+              browser_version: '87.0.4280.88',
+            },
+          },
+          validations: {
+            detect_document: 'error',
+            detect_cutoff: 'warn',
+            detect_glare: 'warn',
+            detect_blur: 'warn',
+          },
+          side: 'front',
+          type: 'passport',
+        }
+        uploadDocument(
+          documentData,
+          API_URL,
+          jwtToken,
+          onSuccessCallback,
+          (error) => {
+            console.error(error)
+            throw new Error(error)
+          }
+        )
+      }
+    )
+  })
 })
 
 describe('API requestChallenges endpoint', () => {
