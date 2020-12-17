5ec888ada295be60564dfd76407a06524e8a0e8c
Kimmi Gan <kimmi.gan@onfido.com>
2020-12-17 18:24:36 +0000

    More code clean up

--- src/components/utils/integration_tests/onfidoApi.test.js
+++ src/components/utils/integration_tests/onfidoApi.test.js
@@ -9,19 +9,6 @@ import {
 import fs from 'fs'
 
 const API_URL = 'https://api.onfido.com'
-const EXPIRED_JWT_TOKEN =
-  'eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MDc3MDc1NTEsInBheWxvYWQiOiJycUFvMEtSbXdtWlViWFRLUHp2TXlTaGZtelNDNVhtRWM3aVZ4ZzJ5b2NQbEQrbk9rQmxtcHBaK0FCKzBcbkwveEtYRm4yeTBNZGxNNXRXVE5HeVNVSG5nPT1cbiIsInV1aWQiOiJpd29rRlZlZEcxOCIsImVudGVycHJpc2VfZmVhdHVyZXMiOnsiY29icmFuZCI6dHJ1ZSwiaGlkZU9uZmlkb0xvZ28iOnRydWV9LCJ1cmxzIjp7InRlbGVwaG9ueV91cmwiOiJodHRwczovL3RlbGVwaG9ueS5vbmZpZG8uY29tIiwiZGV0ZWN0X2RvY3VtZW50X3VybCI6Imh0dHBzOi8vc2RrLm9uZmlkby5jb20iLCJzeW5jX3VybCI6Imh0dHBzOi8vc3luYy5vbmZpZG8uY29tIiwiYXV0aF91cmwiOiJodHRwczovL2VkZ2UuYXBpLm9uZmlkby5jb20iLCJvbmZpZG9fYXBpX3VybCI6Imh0dHBzOi8vYXBpLm9uZmlkby5jb20iLCJob3N0ZWRfc2RrX3VybCI6Imh0dHBzOi8veGQub25maWRvLmNvbSJ9fQ.Ece4NQpZLsPzsgd6W4kDYNugW66W_Fl__jfz6d96WEI'
-const EXPECTED_EXPIRED_TOKEN_ERROR = {
-  status: 401,
-  response: {
-    error: {
-      fields: {},
-      message: 'The token has expired, please request a new one',
-      type: 'expired_token',
-    },
-  },
-}
-
 let jwtToken = null
 const getTestJwtToken = (resolve) => {
   const tokenFactoryUrl = 'https://token-factory.onfido.com/sdk_token' // EU region
@@ -39,15 +26,25 @@ const getTestJwtToken = (resolve) => {
   }
   request.send()
 }
-
-const docValidations = {
+const EXPIRED_JWT_TOKEN =
+  'eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MDc3MDc1NTEsInBheWxvYWQiOiJycUFvMEtSbXdtWlViWFRLUHp2TXlTaGZtelNDNVhtRWM3aVZ4ZzJ5b2NQbEQrbk9rQmxtcHBaK0FCKzBcbkwveEtYRm4yeTBNZGxNNXRXVE5HeVNVSG5nPT1cbiIsInV1aWQiOiJpd29rRlZlZEcxOCIsImVudGVycHJpc2VfZmVhdHVyZXMiOnsiY29icmFuZCI6dHJ1ZSwiaGlkZU9uZmlkb0xvZ28iOnRydWV9LCJ1cmxzIjp7InRlbGVwaG9ueV91cmwiOiJodHRwczovL3RlbGVwaG9ueS5vbmZpZG8uY29tIiwiZGV0ZWN0X2RvY3VtZW50X3VybCI6Imh0dHBzOi8vc2RrLm9uZmlkby5jb20iLCJzeW5jX3VybCI6Imh0dHBzOi8vc3luYy5vbmZpZG8uY29tIiwiYXV0aF91cmwiOiJodHRwczovL2VkZ2UuYXBpLm9uZmlkby5jb20iLCJvbmZpZG9fYXBpX3VybCI6Imh0dHBzOi8vYXBpLm9uZmlkby5jb20iLCJob3N0ZWRfc2RrX3VybCI6Imh0dHBzOi8veGQub25maWRvLmNvbSJ9fQ.Ece4NQpZLsPzsgd6W4kDYNugW66W_Fl__jfz6d96WEI'
+const EXPECTED_EXPIRED_TOKEN_ERROR = {
+  status: 401,
+  response: {
+    error: {
+      fields: {},
+      message: 'The token has expired, please request a new one',
+      type: 'expired_token',
+    },
+  },
+}
+const DOCUMENT_VALIDATIONS = {
   detect_document: 'error',
   detect_cutoff: 'warn',
   detect_glare: 'warn',
   detect_blur: 'warn',
 }
 
-/* eslint jest/no-disabled-tests: 0 */
 /* eslint jest/no-test-callback: 0 */
 
 describe('API uploadDocument endpoint', () => {
@@ -87,7 +84,7 @@ describe('API uploadDocument endpoint', () => {
         const documentData = {
           file: testFile,
           sdkMetadata: {},
-          validations: { ...docValidations },
+          validations: { ...DOCUMENT_VALIDATIONS },
           side: 'front',
           type: 'passport',
         }
@@ -123,7 +120,7 @@ describe('API uploadDocument endpoint', () => {
         const documentData = {
           file: testFile,
           sdkMetadata: {},
-          validations: { ...docValidations },
+          validations: { ...DOCUMENT_VALIDATIONS },
           side: 'front',
           type: 'passport',
         }
@@ -155,7 +152,7 @@ describe('API uploadDocument endpoint', () => {
     const documentData = {
       file: testFile,
       sdkMetadata: {},
-      validations: { ...docValidations },
+      validations: { ...DOCUMENT_VALIDATIONS },
       side: 'front',
       type: 'passport',
     }
