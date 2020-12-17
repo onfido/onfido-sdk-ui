e30fd37bbbbf3722d99b990b0a5ee298887e5886
Kimmi Gan <kimmi.gan@onfido.com>
2020-12-17 13:37:56 +0000

    Clean up code

--- src/components/utils/integration_tests/onfidoApi.test.js
+++ src/components/utils/integration_tests/onfidoApi.test.js
@@ -11,8 +11,18 @@ import fs from 'fs'
 const API_URL = 'https://api.onfido.com'
 const EXPIRED_JWT_TOKEN =
   'eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MDc3MDc1NTEsInBheWxvYWQiOiJycUFvMEtSbXdtWlViWFRLUHp2TXlTaGZtelNDNVhtRWM3aVZ4ZzJ5b2NQbEQrbk9rQmxtcHBaK0FCKzBcbkwveEtYRm4yeTBNZGxNNXRXVE5HeVNVSG5nPT1cbiIsInV1aWQiOiJpd29rRlZlZEcxOCIsImVudGVycHJpc2VfZmVhdHVyZXMiOnsiY29icmFuZCI6dHJ1ZSwiaGlkZU9uZmlkb0xvZ28iOnRydWV9LCJ1cmxzIjp7InRlbGVwaG9ueV91cmwiOiJodHRwczovL3RlbGVwaG9ueS5vbmZpZG8uY29tIiwiZGV0ZWN0X2RvY3VtZW50X3VybCI6Imh0dHBzOi8vc2RrLm9uZmlkby5jb20iLCJzeW5jX3VybCI6Imh0dHBzOi8vc3luYy5vbmZpZG8uY29tIiwiYXV0aF91cmwiOiJodHRwczovL2VkZ2UuYXBpLm9uZmlkby5jb20iLCJvbmZpZG9fYXBpX3VybCI6Imh0dHBzOi8vYXBpLm9uZmlkby5jb20iLCJob3N0ZWRfc2RrX3VybCI6Imh0dHBzOi8veGQub25maWRvLmNvbSJ9fQ.Ece4NQpZLsPzsgd6W4kDYNugW66W_Fl__jfz6d96WEI'
-let jwtToken = null
+const EXPECTED_EXPIRED_TOKEN_ERROR = {
+  status: 401,
+  response: {
+    error: {
+      fields: {},
+      message: 'The token has expired, please request a new one',
+      type: 'expired_token',
+    },
+  },
+}
 
+let jwtToken = null
 const getTestJwtToken = (resolve) => {
   const tokenFactoryUrl = 'https://token-factory.onfido.com/sdk_token'
   const request = new XMLHttpRequest()
@@ -165,17 +175,10 @@ describe('API requestChallenges endpoint', () => {
   })
 
   test('requestChallenges returns an error if request is made with an expired JWT token', () => {
-    const onErrorCallback = (error) => {
-      expect(error.error).toMatchObject({
-        status: 401,
-        response: {
-          error: {
-            fields: {},
-            message: 'The token has expired, please request a new one',
-            type: 'expired_token',
-          },
-        },
-      })
+    const onErrorCallback = (error, reject) => {
+      console.log('requestChallenges Error:', error)
+      expect(error.error).toEqual(EXPECTED_EXPIRED_TOKEN_ERROR)
+      reject()
     }
     requestChallenges(
       API_URL,
