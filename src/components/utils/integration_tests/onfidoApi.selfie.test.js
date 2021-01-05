2f0e6d2a0bd1e0c5ab9501256a5a8e9c28b9a864
Kimmi Gan <kimmi.gan@onfido.com>
2021-01-05 17:41:59 +0000

    Fix duplicated expired JWT token, response in test files

--- src/components/utils/integration_tests/onfidoApi.selfie.test.js
+++ src/components/utils/integration_tests/onfidoApi.selfie.test.js
@@ -6,21 +6,12 @@ import {
 } from '../onfidoApi'
 import { getTestJwtToken, createEmptyFile } from './helpers'
 import { API_URL, PATH_TO_RESOURCE_FILES } from './testUrls'
+import {
+  EXPIRED_JWT_TOKEN,
+  EXPECTED_EXPIRED_TOKEN_ERROR,
+} from './mockExpiredJwt'
 
 let jwtToken = null
-
-const EXPIRED_JWT_TOKEN =
-  'eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MDc3MDc1NTEsInBheWxvYWQiOiJycUFvMEtSbXdtWlViWFRLUHp2TXlTaGZtelNDNVhtRWM3aVZ4ZzJ5b2NQbEQrbk9rQmxtcHBaK0FCKzBcbkwveEtYRm4yeTBNZGxNNXRXVE5HeVNVSG5nPT1cbiIsInV1aWQiOiJpd29rRlZlZEcxOCIsImVudGVycHJpc2VfZmVhdHVyZXMiOnsiY29icmFuZCI6dHJ1ZSwiaGlkZU9uZmlkb0xvZ28iOnRydWV9LCJ1cmxzIjp7InRlbGVwaG9ueV91cmwiOiJodHRwczovL3RlbGVwaG9ueS5vbmZpZG8uY29tIiwiZGV0ZWN0X2RvY3VtZW50X3VybCI6Imh0dHBzOi8vc2RrLm9uZmlkby5jb20iLCJzeW5jX3VybCI6Imh0dHBzOi8vc3luYy5vbmZpZG8uY29tIiwiYXV0aF91cmwiOiJodHRwczovL2VkZ2UuYXBpLm9uZmlkby5jb20iLCJvbmZpZG9fYXBpX3VybCI6Imh0dHBzOi8vYXBpLm9uZmlkby5jb20iLCJob3N0ZWRfc2RrX3VybCI6Imh0dHBzOi8veGQub25maWRvLmNvbSJ9fQ.Ece4NQpZLsPzsgd6W4kDYNugW66W_Fl__jfz6d96WEI'
-const EXPECTED_EXPIRED_TOKEN_ERROR = {
-  status: 401,
-  response: {
-    error: {
-      fields: {},
-      message: 'The token has expired, please request a new one',
-      type: 'expired_token',
-    },
-  },
-}
 
 /* eslint jest/no-test-callback: 0 */
 /*
