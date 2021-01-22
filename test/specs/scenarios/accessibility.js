e5d8bb063770aa6d5d18399130e277d443106bb2
Ethanify <thanhnx.605@gmail.com>
2021-01-22 14:55:35 +0000

    Refine

--- test/specs/scenarios/accessibility.js
+++ test/specs/scenarios/accessibility.js
@@ -261,7 +261,7 @@ export const accessibilityScenarios = async (lang = 'en_US') => {
       //   runAccessibilityTest(driver)
       // })
 
-      it('should verify accessibility for face video intro screen', async () => {
+      it('should verify accessibility for faceVideo intro screen', async () => {
         goToPassportUploadScreen(
           driver,
           welcome,
@@ -303,7 +303,7 @@ export const accessibilityScenarios = async (lang = 'en_US') => {
       })
 
       //FIXME: This is commented out due to the color-contrast accessibility rule fail - CX-4214.
-      // it('should verify accessibility for face video recording and face video confirmation screens', async () => {
+      // it('should verify accessibility for faceVideo recording and faceVideo confirmation screens', async () => {
       //   goToPassportUploadScreen(driver, welcome, documentSelector,`?language=${lang}&faceVideo=true`)
       //   driver.executeScript('window.navigator.mediaDevices.enumerateDevices = () => Promise.resolve([{ kind: "video" }])')
       //   documentUpload.clickUploadButton()
