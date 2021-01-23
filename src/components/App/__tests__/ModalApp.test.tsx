cda4784fc3aae1bf10e43d2f91c56656934a2b16
Ethanify <thanhnx.605@gmail.com>
2021-01-23 13:20:04 +0000

    Add ~jest utils

--- src/components/App/__tests__/ModalApp.test.tsx
+++ src/components/App/__tests__/ModalApp.test.tsx
@@ -1,44 +1,29 @@
 import { h } from 'preact'
-import { Provider as ReduxProvider } from 'react-redux'
 import { mount, shallow } from 'enzyme'
-import configureMockStore from 'redux-mock-store'
 
-import { initialState as captures } from 'components/ReduxAppWrapper/store/reducers/captures'
-import { initialState as globals } from 'components/ReduxAppWrapper/store/reducers/globals'
-
+import MockedReduxProvider from '~jest/MockedReduxProvider'
 import ModalApp from '../ModalApp'
-
-const mockStore = configureMockStore()
-const mockState = {
-  captures,
-  globals,
-}
 
 describe('ModalApp', () => {
   it('renders without crashing', () => {
-    const store = mockStore(mockState)
-
     const wrapper = shallow(
-      <ReduxProvider store={store}>
+      <MockedReduxProvider>
         <ModalApp options={{}} />
-      </ReduxProvider>
+      </MockedReduxProvider>
     )
 
     expect(wrapper.exists()).toBeTruthy()
+    console.log(wrapper.debug())
   })
 
-  /* it.skip('renders the front document capture', () => {
-    const store = mockStore(mockState)
-
+  it.skip('renders the LocaleProvider', () => {
     const wrapper = mount(
-      <ReduxProvider store={store}>
-        <MockedLocalised>
-          <ModalApp {...defaultProps} />
-        </MockedLocalised>
-      </ReduxProvider>
+      <MockedReduxProvider>
+        <ModalApp options={{}} />
+      </MockedReduxProvider>
     )
 
-    const documentLiveCapture = wrapper.find('DocumentLiveCapture')
-    console.log(documentLiveCapture.debug())
-  }) */
+    expect(wrapper.exists()).toBeTruthy()
+    console.log(wrapper.debug())
+  })
 })
