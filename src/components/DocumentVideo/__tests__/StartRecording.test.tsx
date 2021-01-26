d420f6e9fd4c153ee002c4a1ee54c05ead4bdbb2
Ethanify <thanhnx.605@gmail.com>
2021-01-26 11:46:19 +0000

    Add StartRecording tests

---
+++ src/components/DocumentVideo/__tests__/StartRecording.test.tsx
@@ -0,0 +1,55 @@
+import { h } from 'preact'
+import { mount, shallow } from 'enzyme'
+
+import MockedLocalised from '~jest/MockedLocalised'
+import StartRecording from '../StartRecording'
+
+const defaultProps = {
+  disableInteraction: false,
+  onClick: jest.fn(),
+}
+
+describe('DocumentVideo', () => {
+  describe('StartRecording', () => {
+    it('renders without crashing', () => {
+      const wrapper = shallow(
+        <MockedLocalised>
+          <StartRecording {...defaultProps} />
+        </MockedLocalised>
+      )
+      expect(wrapper.exists()).toBeTruthy()
+    })
+
+    it('renders instructions & button correctly', () => {
+      const wrapper = mount(
+        <MockedLocalised>
+          <StartRecording {...defaultProps} />
+        </MockedLocalised>
+      )
+
+      expect(wrapper.find('Instructions .title').text()).toEqual(
+        'doc_video_capture.instructions.video_intro_title'
+      )
+      expect(wrapper.find('Instructions .subtitle').text()).toEqual(
+        'doc_video_capture.instructions.video_intro_subtitle'
+      )
+
+      const button = wrapper.find('Button > button')
+      expect(button.prop('disabled')).toBeFalsy()
+      expect(button.text()).toEqual(
+        'doc_video_capture.button_record_accessibility'
+      )
+    })
+
+    it('disables button when disableInteraction=true', () => {
+      const wrapper = mount(
+        <MockedLocalised>
+          <StartRecording {...defaultProps} disableInteraction />
+        </MockedLocalised>
+      )
+
+      const button = wrapper.find('Button > button')
+      expect(button.prop('disabled')).toBeTruthy()
+    })
+  })
+})
