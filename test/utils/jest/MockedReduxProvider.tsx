70e7765fd0fd7355e6cfdac1e1efe1bdcfff360f
Ethanify <thanhnx.605@gmail.com>
2021-01-27 17:17:08 +0000

    Add overrideState to MockedReduxProvider

--- test/utils/jest/MockedReduxProvider.tsx
+++ test/utils/jest/MockedReduxProvider.tsx
@@ -4,16 +4,22 @@ import configureMockStore from 'redux-mock-store'
 
 import { initialState as captures } from 'components/ReduxAppWrapper/store/reducers/captures'
 import { initialState as globals } from 'components/ReduxAppWrapper/store/reducers/globals'
+import type { RootState } from 'components/ReduxAppWrapper/store/reducers'
 
 type Props = {
   children?: h.JSX.Element
+  overrideState?: RootState
 }
 
-const MockedReduxProvider: FunctionComponent<Props> = ({ children }) => {
+const MockedReduxProvider: FunctionComponent<Props> = ({
+  children,
+  overrideState,
+}) => {
   const mockStore = configureMockStore()
   const mockState = {
     captures,
     globals,
+    ...overrideState,
   }
   const store = mockStore(mockState)
 
