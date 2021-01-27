0ab2ea145088875fff8a3df8cf63530ee122c973
Ethanify <thanhnx.605@gmail.com>
2021-01-27 14:03:07 +0000

    Add tests for CameraPermissions/Primer

---
+++ src/components/CameraPermissions/__tests__/Primer.test.tsx
@@ -0,0 +1,42 @@
+import { h } from 'preact'
+import { mount, shallow } from 'enzyme'
+
+import MockedLocalised from '~jest/MockedLocalised'
+import MockedReduxProvider from '~jest/MockedReduxProvider'
+import Primer from '../Primer'
+
+const defaultProps = {
+  onNext: jest.fn(),
+  trackScreen: jest.fn(),
+}
+
+describe('CameraPermissions', () => {
+  describe('Primer', () => {
+    it('renders without crashing', () => {
+      const wrapper = shallow(
+        <MockedReduxProvider>
+          <MockedLocalised>
+            <Primer {...defaultProps} />
+          </MockedLocalised>
+        </MockedReduxProvider>
+      )
+
+      expect(wrapper.exists()).toBeTruthy()
+    })
+
+    describe('when mounted', () => {
+      it('renders the LocaleProvider', () => {
+        const wrapper = mount(
+          <MockedReduxProvider>
+            <MockedLocalised>
+              <Primer {...defaultProps} />
+            </MockedLocalised>
+          </MockedReduxProvider>
+        )
+
+        expect(wrapper.exists()).toBeTruthy()
+        expect(wrapper.find('PageTitle').exists()).toBeTruthy()
+      })
+    })
+  })
+})
