6d613de6b345f3f057639cd0473b55c7475db426
Ethanify <thanhnx.605@gmail.com>
2021-01-27 17:13:51 +0000

    Add tests for PageTitle

---
+++ src/components/PageTitle/__tests__/index.test.tsx
@@ -0,0 +1,35 @@
+import { h } from 'preact'
+import { mount, shallow } from 'enzyme'
+
+import MockedReduxProvider from '~jest/MockedReduxProvider'
+import PageTitle from '../index'
+
+const defaultProps = {
+  title: 'Fake title',
+}
+
+describe('PageTitle', () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(
+      <MockedReduxProvider>
+        <PageTitle {...defaultProps} />
+      </MockedReduxProvider>
+    )
+
+    expect(wrapper.exists()).toBeTruthy()
+    expect(wrapper.find('Connect(PageTitle)').exists()).toBeTruthy()
+  })
+
+  describe('when mounted', () => {
+    it('renders without crashing', () => {
+      const wrapper = mount(
+        <MockedReduxProvider>
+          <PageTitle {...defaultProps} />
+        </MockedReduxProvider>
+      )
+
+      expect(wrapper.exists()).toBeTruthy()
+      expect(wrapper.find('.titleSpan').text()).toEqual(defaultProps.title)
+    })
+  })
+})
