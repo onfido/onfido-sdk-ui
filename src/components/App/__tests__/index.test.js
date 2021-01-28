8d794bd56c78619d47e881bf3368ed64a984e0f6
Kimmi Gan <kimmi.gan@onfido.com>
2021-01-28 16:28:44 +0000

    Mock @onfido/castor-react module

--- src/components/App/__tests__/index.test.js
+++ src/components/App/__tests__/index.test.js
@@ -3,15 +3,6 @@ import { shallow } from 'enzyme'
 
 import App from '../index'
 
-jest.mock('@onfido/castor-react', () => {
-  return {
-    __esModule: true,
-    button: () => {
-      return <div>Mock Button</div>
-    },
-  }
-})
-
 it('renders without crashing', () => {
   shallow(<App />)
 })
