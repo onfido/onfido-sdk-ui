281d4792c5820eb850a253147e858c7935de7f02
Ethanify <thanhnx.605@gmail.com>
2021-01-28 09:25:21 +0000

    Enable prettier for src/locales

--- src/locales/index.tsx
+++ src/locales/index.tsx
@@ -3,7 +3,11 @@ import { parseTags } from '~utils'
 import initializePolyglot from './polyglot'
 
 import type { WithLocalisedProps } from '~types/hocs'
-import type { SupportedLanguages, LocaleConfig, TranslatedTagParser } from '~types/locales'
+import type {
+  SupportedLanguages,
+  LocaleConfig,
+  TranslatedTagParser,
+} from '~types/locales'
 
 type ProviderProps = {
   language: SupportedLanguages | LocaleConfig
@@ -12,19 +16,31 @@ type ProviderProps = {
 
 export const LocaleContext = createContext<WithLocalisedProps>(null)
 
-export const LocaleProvider: FunctionComponent<ProviderProps> = ({ language, children }) => {
+export const LocaleProvider: FunctionComponent<ProviderProps> = ({
+  language,
+  children,
+}) => {
   const polyglot = initializePolyglot(language)
   const translate = polyglot.t.bind(polyglot)
-  const parseTranslatedTags: TranslatedTagParser = (key, handler) => parseTags(translate(key), handler)
+  const parseTranslatedTags: TranslatedTagParser = (key, handler) =>
+    parseTags(translate(key), handler)
 
   return (
-    <LocaleContext.Provider value={{language: polyglot.currentLocale, translate, parseTranslatedTags}}>
+    <LocaleContext.Provider
+      value={{
+        language: polyglot.currentLocale,
+        translate,
+        parseTranslatedTags,
+      }}
+    >
       {children}
     </LocaleContext.Provider>
   )
 }
 
-export const localised = <P extends unknown>(WrappedComponent: ComponentType<P & WithLocalisedProps>): ComponentType<P> => {
+export const localised = <P extends unknown>(
+  WrappedComponent: ComponentType<P & WithLocalisedProps>
+): ComponentType<P> => {
   const LocalisedComponent: FunctionComponent<P> = (props) => (
     <LocaleContext.Consumer>
       {(injectedProps) => <WrappedComponent {...props} {...injectedProps} />}
