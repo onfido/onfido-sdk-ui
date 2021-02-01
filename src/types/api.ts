8c40ad9bc1170819ea3e87cb95ab11b43e832700
Ethanify <thanhnx.605@gmail.com>
2021-02-01 10:38:32 +0000

    Refine onfidoApi types

--- src/types/api.ts
+++ src/types/api.ts
@@ -17,6 +17,8 @@ export type ImageQualityValidationPayload = Partial<
 /* Errors */
 
 const API_ERROR_VALIDATION = 'validation_error'
+const API_ERROR_EXPIRED_TOKEN = 'expired_token'
+const API_ERROR_AUTHORIZATION = 'authorization_error'
 
 type ValidationReasons =
   | 'attachment_content_type'
@@ -27,17 +29,33 @@ type ValidationReasons =
   | 'detect_blur'
 
 type ValidationError = {
-  type: typeof API_ERROR_VALIDATION
-  message: string
-  fields: Partial<Record<ValidationReasons, string[]>>
+  type?: never
+  message?: never
+  error?: {
+    type: typeof API_ERROR_VALIDATION
+    message: string
+    fields: Partial<Record<ValidationReasons, string[]>>
+  }
 }
 
-type ErrorPayload = {
-  error?: ValidationError
+type ExpiredTokenError = {
+  type: typeof API_ERROR_EXPIRED_TOKEN
+  message: string
+  error?: never
+}
+
+type AuthorizationError = {
+  type?: never
+  message?: never
+  error: {
+    type: typeof API_ERROR_AUTHORIZATION
+    message: string
+    fields: Record<string, unknown>
+  }
 }
 
 export type ApiError = {
-  response: string | ErrorPayload
+  response: string | ValidationError | ExpiredTokenError | AuthorizationError
   status: number
 }
 
