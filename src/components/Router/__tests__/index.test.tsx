d8c4cf31fb6e72e5638ca811ab4267bbd7dd3be6
Ethanify <thanhnx.605@gmail.com>
2021-02-02 18:07:10 +0000

    Add mock functions for ~utils

--- src/components/Router/__tests__/index.test.tsx
+++ src/components/Router/__tests__/index.test.tsx
@@ -9,6 +9,7 @@ import Router from '../index'
 
 import type { NarrowSdkOptions } from '~types/commons'
 
+jest.mock('../../utils')
 jest.mock('../../utils/crossDeviceSync')
 
 const defaultOptions: NarrowSdkOptions = {
@@ -20,46 +21,44 @@ const defaultOptions: NarrowSdkOptions = {
   ],
 }
 
-describe('CameraPermissions', () => {
-  describe('Router', () => {
-    it('renders without crashing', () => {
-      const wrapper = shallow(
-        <Router {...mockedReduxProps} options={defaultOptions} />
+describe('Router', () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(
+      <Router {...mockedReduxProps} options={defaultOptions} />
+    )
+    expect(wrapper.exists()).toBeTruthy()
+  })
+
+  describe('when mounted', () => {
+    it('renders MainRouter by default', () => {
+      const wrapper = mount(
+        <MockedReduxProvider>
+          <MockedLocalised>
+            <Router {...mockedReduxProps} options={defaultOptions} />
+          </MockedLocalised>
+        </MockedReduxProvider>
       )
+
       expect(wrapper.exists()).toBeTruthy()
+      expect(wrapper.find('MainRouter').exists()).toBeTruthy()
+      expect(wrapper.find('CrossDeviceMobileRouter').exists()).toBeFalsy()
     })
 
-    describe('when mounted', () => {
-      it('renders MainRouter by default', () => {
-        const wrapper = mount(
-          <MockedReduxProvider>
-            <MockedLocalised>
-              <Router {...mockedReduxProps} options={defaultOptions} />
-            </MockedLocalised>
-          </MockedReduxProvider>
-        )
+    it('renders CrossDeviceMobileRouter when mobileFlow=true', () => {
+      const wrapper = mount(
+        <MockedReduxProvider>
+          <MockedLocalised>
+            <Router
+              {...mockedReduxProps}
+              options={{ ...defaultOptions, mobileFlow: true }}
+            />
+          </MockedLocalised>
+        </MockedReduxProvider>
+      )
 
-        expect(wrapper.exists()).toBeTruthy()
-        expect(wrapper.find('MainRouter').exists()).toBeTruthy()
-        expect(wrapper.find('CrossDeviceMobileRouter').exists()).toBeFalsy()
-      })
-
-      it('renders CrossDeviceMobileRouter when mobileFlow=true', () => {
-        const wrapper = mount(
-          <MockedReduxProvider>
-            <MockedLocalised>
-              <Router
-                {...mockedReduxProps}
-                options={{ ...defaultOptions, mobileFlow: true }}
-              />
-            </MockedLocalised>
-          </MockedReduxProvider>
-        )
-
-        expect(wrapper.exists()).toBeTruthy()
-        expect(wrapper.find('MainRouter').exists()).toBeFalsy()
-        expect(wrapper.find('CrossDeviceMobileRouter').exists()).toBeTruthy()
-      })
+      expect(wrapper.exists()).toBeTruthy()
+      expect(wrapper.find('MainRouter').exists()).toBeFalsy()
+      expect(wrapper.find('CrossDeviceMobileRouter').exists()).toBeTruthy()
     })
   })
 })
--- src/components/utils/__mocks__/index.tsx
+++ src/components/utils/__mocks__/index.tsx
@@ -1,1 +1,10 @@
-export const checkIfWebcamPermissionGranted = jest.fn()
+export const buildIteratorKey = jest.fn().mockImplementation((key) => key)
+export const checkIfHasWebcam = jest
+  .fn()
+  .mockImplementation((callback) => callback(true))
+export const checkIfWebcamPermissionGranted = jest
+  .fn()
+  .mockImplementation((callback) => callback(true))
+export const getEnabledDocuments = jest.fn().mockReturnValue([])
+export const hasOnePreselectedDocument = jest.fn().mockReturnValue(false)
+export const isSafari131 = jest.fn().mockReturnValue(false)
