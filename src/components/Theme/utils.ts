e9be927c514901af6868a05635bdc8b398c415df
Kimmi Gan <kimmi.gan@onfido.com>
2021-02-18 10:45:33 +0000

    Fix Typescript error

--- src/components/Theme/utils.ts
+++ src/components/Theme/utils.ts
@@ -1,16 +1,20 @@
 import { kebabCase } from '~utils/string'
+import { UICustomisationOptions } from '~types/ui-customisation-options'
 
-export const setUICustomisations = (customUI = {}): void => {
+export const setUICustomisations = (customUI: UICustomisationOptions): void => {
   const sdkCustomisations = Object.keys(customUI).map(
-    (key) => `--osdk-${kebabCase(key)}: ${customUI[key]};`
+    (property: keyof typeof customUI) =>
+      `--osdk-${kebabCase(property)}: ${customUI[property]};`
   )
-  const style = `
-    <style>
-        :root {
-          ${sdkCustomisations.join('\n')}
-        }
-    </style>`
-  document.head.insertAdjacentHTML('beforeend', style)
+  if (sdkCustomisations.length > 0) {
+    const style = `
+      <style>
+          :root {
+            ${sdkCustomisations.join('\n')}
+          }
+      </style>`
+    document.head.insertAdjacentHTML('beforeend', style)
+  }
 }
 
 export const isButtonGroupStacked = (): boolean =>
