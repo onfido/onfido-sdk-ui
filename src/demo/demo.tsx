c95614ef4e430f12c4507ad3dc76c21f9078ceaa
Skyler Robbins <skyler.robbins@onfido.com>
2021-03-04 15:52:52 -0700

    pushing on behalf of david, added text to match mocks for all SDK pages except last

--- src/demo/demo.tsx
+++ src/demo/demo.tsx
@@ -130,6 +130,21 @@ const SdkDemo: FunctionComponent<{
     onError: (error) => console.error('onError callback:', error),
     onModalRequestClose: () => setIsModalOpen(false),
     ...(sdkOptions || {}),
+    language: {
+      phrases: {
+        welcome: {
+          title: "Create your Verfiable Credential with Microsoft.",
+          description_p_1: "To create your credentials, we will need to verify your identity. It will only take a few minutes.",
+          description_p_2: "Please ready a government-issued identity document such as a Driver License, ID card, or passport.",
+        },
+        generic: {
+          back: "",
+        },
+        cross_device_checklist: {
+          button_primary: "Generate Verifiable Credential",
+        },
+      }
+    }
   }
 
   return (
