3e84c228a1a1edf4bb082552693c672665086425
Kimmi Gan <kimmi.gan@onfido.com>
2021-03-05 13:00:31 +0000

    Re-added the wrong polyfill in previous commit (2fb4103)

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import 'custom-event-polyfill'
 // TODO: These IE11 polyfills are missing in `development` after the Typescript conversion.
 //       But on PRs where the components that use these Array methods have been converted the polyfills seem to be included.
 //       Should be fine to remove when those PRs are merged in eventually.
-import 'ie-array-find-polyfill'
+import 'array-flat-polyfill'
 
 import { upperCase } from '~utils/string'
 import { noop } from '~utils/func'
