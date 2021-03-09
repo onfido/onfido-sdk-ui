d22ce0ca5bb6150ed22b48e472fce0a79af78496
Skyler Robbins <skyler.robbins@onfido.com>
2021-03-08 21:51:41 -0700

    added qrcode and pin to complete page

--- src/components/Complete/index.js
+++ src/components/Complete/index.js
@@ -4,20 +4,48 @@ import PageTitle from '../PageTitle'
 import { localised } from '../../locales'
 import theme from '../Theme/style.scss'
 import style from './style.scss'
+import axios from 'axios'
 
 class Complete extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      qrCode: this.fetchQrCode(),
+      pin: '',
+    }
+  }
+
   componentDidMount() {
     this.props.nextStep()
+  }
+
+  fetchQrCode = () => {
+    axios
+      .get('http://127.0.0.1:3000/idv-qr-code')
+      .then((response) => {
+        const {
+          data: { qrCode, pin },
+        } = response
+        console.log('success', response.data)
+        this.setState({
+          qrCode,
+          pin,
+        })
+      })
+      .catch((err) => {
+        console.log('fail', err)
+      })
   }
 
   render({ message, submessage, translate }) {
     const title = message || translate('outro.title')
     const body = submessage || translate('outro.body')
-
     return (
       <div className={style.wrapper}>
         <span className={`${theme.icon}  ${style.icon}`} />
         <PageTitle title={title} subTitle={body} />
+        <img src={this.state.qrCode} alt="Red dot" />
+        <p>{this.state.pin}</p>
       </div>
     )
   }
