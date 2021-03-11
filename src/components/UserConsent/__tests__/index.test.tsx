ee480dd16601b7df6f5b72a3ed5043b7239ea739
Ethanify <thanhnx.605@gmail.com>
2021-03-11 13:33:54 +0000

    Fix XMLHttpRequest errors for UserConsent tests

--- src/components/UserConsent/__tests__/index.test.tsx
+++ src/components/UserConsent/__tests__/index.test.tsx
@@ -9,6 +9,19 @@ import type { StepComponentUserConsentProps } from '~types/routers'
 
 jest.mock('dompurify')
 
+const xhrMock: Partial<XMLHttpRequest> = {
+  open: jest.fn(),
+  send: jest.fn(),
+  setRequestHeader: jest.fn(),
+  readyState: 4,
+  status: 200,
+  response: '<h1>My Sanitized Header</h1>',
+}
+
+jest
+  .spyOn(window, 'XMLHttpRequest')
+  .mockImplementation(() => xhrMock as XMLHttpRequest)
+
 const defaultOptions: StepComponentUserConsentProps = {
   ...mockedReduxProps,
   componentsList: [
@@ -33,7 +46,7 @@ describe('UserConsent', () => {
   describe('when mounted', () => {
     beforeEach(() => {
       const sanitizer = sanitize as jest.Mock
-      sanitizer.mockReturnValueOnce('<h1>My Sanitized Header</h1>')
+      sanitizer.mockReturnValueOnce(xhrMock.response)
     })
 
     it('renders UserConsent with actions', () => {
@@ -56,7 +69,9 @@ describe('UserConsent', () => {
       // In Enzyme v3 you need to use `render()` to see the HTML inside `dangerouslySetInnerHTML`
       // See the following issues https://github.com/enzymejs/enzyme/issues/419 and https://github.com/enzymejs/enzyme/issues/1297
 
-      expect(wrapper.render().html()).toContain('<h1>My Sanitized Header</h1>')
+      expect(wrapper.find('ScreenLayout').render().html()).toContain(
+        xhrMock.response
+      )
     })
 
     it('renders the DeclineModal component', () => {
