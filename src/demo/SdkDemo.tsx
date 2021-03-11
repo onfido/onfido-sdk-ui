d4b9e3f22e0518f770ce25e1fc5406d2cdaa7293
Skyler Robbins <skyler.robbins@onfido.com>
2021-03-11 13:42:38 -0700

    moved language changes to en_US.json

--- src/demo/SdkDemo.tsx
+++ src/demo/SdkDemo.tsx
@@ -71,7 +71,7 @@ const SdkDemo: FunctionComponent<Props> = ({
     language: {
       phrases: {
         welcome: {
-          title: 'Create your Verfiable Credential with Microsoft.',
+          title: 'Create your Verifiable Credential with Microsoft.',
           description_p_1:
             'To create your credentials, we will need to verify your identity. It will only take a few minutes.',
           description_p_2:
