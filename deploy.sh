c3ff5b4e283e7d485dd12852b560fccf2e4352fe
Ryan Shea <ryan.shea@onfido.com>
2021-03-15 09:37:29 -0600

    Removed teardown
     Changes to be committed:
    	modified:   deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -62,8 +62,6 @@ for DEPLOY_SUBDOMAIN_UNFORMATTED in "${DEPLOY_SUBDOMAIN_UNFORMATTED_LIST[@]}"
 do
   if [ "$NODE_ENV" == "production" ]; then
     DEPLOY_DOMAIN=https://microsoft-idv-sdk-ui-onfido.surge.sh
-    surge teardown $DEPLOY_DOMAIN
-    echo "Tore down domain: ${DEPLOY_DOMAIN}"
   else
     # replaces non alphanumeric symbols with "-"
     # sed -r is only supported in linux, ref http://stackoverflow.com/a/2871217/689223
