f49c86be1341de27d017f15d80270727cc013697
Skyler Robbins <skyler.robbins@onfido.com>
2021-03-15 18:32:31 -0600

    lambda t4st

--- src/components/Complete/index.js
+++ src/components/Complete/index.js
@@ -5,9 +5,43 @@ import PageTitle from '../PageTitle'
 import { localised } from '../../locales'
 import theme from '../Theme/style.scss'
 import style from './style.scss'
+import axios from 'axios'
 class Complete extends Component {
   componentDidMount() {
     this.props.nextStep()
+
+    this.fetchQrCode()
+  }
+
+  fetchQrCode = () => {
+    const data = JSON.stringify({
+      document_id: '02161f96-8563-4e9e-8b5f-9e2625af9c26',
+    })
+
+    const config = {
+      method: 'post',
+      url: 'https://edge.api.onfido.com/v3/idv-qr-code',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      data,
+    }
+    axios(config)
+      .then((response) => {
+        const {
+          data: { qrCode, pin },
+        } = response
+        console.log('success', response.data)
+        this.setState({
+          qrCode,
+          pin,
+          loading: false,
+        })
+      })
+      .catch((err) => {
+        console.log('fail', err)
+      })
   }
 
   render({ message, submessage, translate }) {
