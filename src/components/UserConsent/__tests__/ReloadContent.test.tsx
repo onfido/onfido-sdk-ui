210be1bf14976e42f2b8bf3c58f705d36b76ea95
Kimmi Gan <kimmi.gan@onfido.com>
2021-04-09 17:40:15 +0100

    Add unit test for ReloadContent when mounted

--- src/components/UserConsent/__tests__/ReloadContent.test.tsx
+++ src/components/UserConsent/__tests__/ReloadContent.test.tsx
@@ -3,18 +3,38 @@ import { mount, shallow } from 'enzyme'
 
 import ReloadContent from '../ReloadContent'
 import MockedLocalised from '~jest/MockedLocalised'
-
-const defaultProps = {
-  onPrimaryButtonClick: jest.fn(),
-}
+import MockedReduxProvider from '~jest/MockedReduxProvider'
 
 describe('ReloadContent', () => {
   it('renders without crashing', () => {
     const wrapper = shallow(
       <MockedLocalised>
-        <ReloadContent {...defaultProps} />
+        <ReloadContent onPrimaryButtonClick={mockedOnClickFunction} />
       </MockedLocalised>
     )
     expect(wrapper.exists()).toBeTruthy()
+  })
+
+  describe('when mounted', () => {
+    it('renders without crashing', () => {
+      const mockedOnClickFunction = jest.fn()
+      const wrapper = mount(
+        <MockedReduxProvider>
+          <MockedLocalised>
+            <ReloadContent onPrimaryButtonClick={mockedOnClickFunction} />
+          </MockedLocalised>
+        </MockedReduxProvider>
+      )
+      const primaryBtn = wrapper.find({
+        'data-onfido-qa': 'userConsentReloadScreenBtn',
+      })
+      expect(wrapper.exists()).toBeTruthy()
+      expect(primaryBtn.exists()).toBeTruthy()
+
+      primaryBtn.simulate('click')
+      expect(mockedOnClickFunction).toHaveBeenCalled()
+
+      wrapper.unmount()
+    })
   })
 })
