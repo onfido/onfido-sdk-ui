de2a971e7c11df34a115eaf81d84c6cdf9fb5bf4
Kimmi Gan <kimmi.gan@onfido.com>
2021-05-05 15:11:01 +0100

    Update path from v2 to v4

--- src/components/utils/crossDeviceSync.ts
+++ src/components/utils/crossDeviceSync.ts
@@ -1,7 +1,7 @@
 import { io, Socket, ManagerOptions } from 'socket.io-client'
 
 const socketData: Partial<ManagerOptions> = {
-  path: '/v2/socket.io',
+  path: '/v4/socket.io', // default: /socket.io/ (previously was set to /v2/socket.io in SDK code base)
   upgrade: false, // default: true
   autoConnect: false,
   transports: ['websocket', 'polling'], // default: ['polling', 'websocket']
