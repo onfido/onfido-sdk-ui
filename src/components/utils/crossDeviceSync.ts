806257b43de7d2756271acce7ccd02f66c95a6a6
Stefania Cardenas <stefania.cardenas@onfido.com>
2021-05-11 20:36:15 +0100

    push v1 implemetation

--- src/components/utils/crossDeviceSync.ts
+++ src/components/utils/crossDeviceSync.ts
@@ -1,10 +1,7 @@
 import io from 'socket.io-client'
 
 const socketData: SocketIOClient.ConnectOpts = {
-  path: '/v2/socket.io',
-  upgrade: false, // default: true
-  autoConnect: false,
-  transports: ['websocket', 'polling'], // default: ['polling', 'websocket']
+  path: '/socket.io',
 }
 
 export const createSocket = (url?: string): SocketIOClient.Socket => {
