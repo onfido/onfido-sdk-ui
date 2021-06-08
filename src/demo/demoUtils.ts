0728a29bf919b4df2c72bc88939562b4be134bd6
Kimmi Gan <kimmi.gan@onfido.com>
2021-06-08 17:24:04 +0100

    Add UI test for legacy/boolean style doc type options

--- src/demo/demoUtils.ts
+++ src/demo/demoUtils.ts
@@ -25,6 +25,7 @@ export type QueryParams = {
   liveness?: StringifiedBoolean
   multiDocWithInvalidPresetCountry?: StringifiedBoolean
   multiDocWithPresetCountry?: StringifiedBoolean
+  multiDocBooleanWithCountrySelection?: StringifiedBoolean
   noCompleteStep?: StringifiedBoolean
   oneDoc?: DocumentTypes
   oneDocWithCountrySelection?: StringifiedBoolean
@@ -127,6 +128,13 @@ const getPreselectedDocumentTypes = (): Partial<
       national_identity_card: {
         country: 'XYZ',
       },
+    }
+  }
+
+  if (queryParamToValueString.multiDocBooleanWithCountrySelection === 'true') {
+    return {
+      driving_licence: true,
+      national_identity_card: true,
     }
   }
 
