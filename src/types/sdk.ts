c4706c329f1055c8d92855830e1db81cdeb32a36
Ethanify <thanhnx.605@gmail.com>
2021-06-10 10:00:05 +0100

    Add variant field to DocumentResponse

--- src/types/sdk.ts
+++ src/types/sdk.ts
@@ -1,5 +1,5 @@
 import type { SupportedLanguages, LocaleConfig } from './locales'
-import type { StepConfig, StepTypes } from './steps'
+import type { RequestedVariant, StepConfig, StepTypes } from './steps'
 import type { EnterpriseFeatures } from './enterprise'
 import type { UICustomizationOptions } from './ui-customisation-options'
 
@@ -7,11 +7,12 @@ type DocumentResponse = {
   id: string
   side: string
   type: string
+  variant: RequestedVariant
 }
 
 type FaceResponse = {
   id: string
-  variant: string
+  variant: RequestedVariant
 }
 
 export type SdkResponse = {
