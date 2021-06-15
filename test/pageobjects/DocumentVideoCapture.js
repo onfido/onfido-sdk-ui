a441514914aa9885bcdcceac39490f617daf4580
Ethanify <thanhnx.605@gmail.com>
2021-06-15 13:52:23 +0100

    Enable dedicated className for instruction subtitle

--- test/pageobjects/DocumentVideoCapture.js
+++ test/pageobjects/DocumentVideoCapture.js
@@ -41,9 +41,7 @@ class DocumentVideoCapture extends BasePage {
     return this.$('.onfido-sdk-ui-DocumentVideo-reusables-title')
   }
   async documentVideoSubInstructions() {
-    return this.$(
-      '.onfido-sdk-ui-DocumentVideo-reusables-instructions > span:nth-child(2)'
-    )
+    return this.$('.onfido-sdk-ui-DocumentVideo-reusables-subtitle')
   }
   async loadingBar() {
     return this.$('.onfido-sdk-ui-DocumentVideo-reusables-active')
