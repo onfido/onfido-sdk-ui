e57c6ba1888f696d9d4d30dfe2a9da8ccd5cb03b
Stefania Cardenas <stefania.cardenas@onfido.com>
2021-06-16 09:10:42 +0100

    Remove unused types

--- src/types/redux/captures.ts
+++ src/types/redux/captures.ts
@@ -73,8 +73,6 @@ export type CaptureActions =
 export type CaptureState = {
   document_front?: DocumentCapture & MetadataState
   document_back?: DocumentCapture & MetadataState
-  // when is this used
-  // document?: DocumentCapture & MetadataState
   face?: FaceCapture & MetadataState
 }
 
