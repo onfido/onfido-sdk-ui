77a8934843ab213cd4c3005b85ed96465683352e
Kimmi Gan <kimmi.gan@onfido.com>
2021-06-21 11:51:40 +0100

    Refactor to use useLocales() hook instead

--- src/components/crossDevice/MobileNotificationSent/index.tsx
+++ src/components/crossDevice/MobileNotificationSent/index.tsx
@@ -2,11 +2,11 @@ import { h, FunctionComponent } from 'preact'
 import PageTitle from '../../PageTitle'
 import { trackComponent } from '../../../Tracker'
 import { preventDefaultOnClick } from '~utils'
-import { localised } from '../../../locales'
+import { useLocales } from '~locales'
 import theme from '../../Theme/style.scss'
 import style from './style.scss'
 
-import type { WithLocalisedProps, WithTrackingProps } from '~types/hocs'
+import type { WithTrackingProps } from '~types/hocs'
 
 type SmsInfo = {
   number: string
@@ -18,47 +18,50 @@ type MobileNotificationSentProps = {
   previousStep: () => void
 }
 
-type Props = MobileNotificationSentProps &
-  WithLocalisedProps &
-  WithTrackingProps
+type Props = MobileNotificationSentProps & WithTrackingProps
 
 const MobileNotificationSent: FunctionComponent<Props> = ({
   sms,
-  translate,
   previousStep,
-}) => (
-  <div>
-    <PageTitle title={translate('sms_sent.title')} />
+}) => {
+  const { translate } = useLocales()
+  return (
     <div>
-      <div className={style.submessage}>
-        {translate('sms_sent.subtitle', {
-          number: sms.number,
-        })}
+      <PageTitle title={translate('sms_sent.title')} />
+      <div>
+        <div className={style.submessage}>
+          {translate('sms_sent.subtitle', {
+            number: sms.number,
+          })}
+        </div>
+        <div className={style.boldMessage}>
+          {translate('sms_sent.subtitle_minutes')}
+        </div>
+        <span className={`${theme.icon} ${style.icon}`} />
+        <div className={theme.header}>{translate('sms_sent.info')}</div>
+        <div className={`${style.help} ${theme.help}`}>
+          <ul
+            className={theme.helpList}
+            aria-label={translate('sms_sent.info')}
+          >
+            <li>{translate('sms_sent.info_link_window')}</li>
+            <li>{translate('sms_sent.info_link_expire')}</li>
+          </ul>
+        </div>
+        <a
+          href="#"
+          role="button"
+          className={`${theme.link} ${style.cancel}`}
+          onClick={preventDefaultOnClick(previousStep)}
+        >
+          {translate('sms_sent.link')}
+        </a>
       </div>
-      <div className={style.boldMessage}>
-        {translate('sms_sent.subtitle_minutes')}
-      </div>
-      <span className={`${theme.icon} ${style.icon}`} />
-      <div className={theme.header}>{translate('sms_sent.info')}</div>
-      <div className={`${style.help} ${theme.help}`}>
-        <ul className={theme.helpList} aria-label={translate('sms_sent.info')}>
-          <li>{translate('sms_sent.info_link_window')}</li>
-          <li>{translate('sms_sent.info_link_expire')}</li>
-        </ul>
-      </div>
-      <a
-        href="#"
-        role="button"
-        className={`${theme.link} ${style.cancel}`}
-        onClick={preventDefaultOnClick(previousStep)}
-      >
-        {translate('sms_sent.link')}
-      </a>
     </div>
-  </div>
-)
+  )
+}
 
 export default trackComponent(
-  localised(MobileNotificationSent),
+  MobileNotificationSent,
   'mobile_notification_sent'
 )
