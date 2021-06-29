865909c000452d0a6bbeb61a60c5a6749032c5b9
Kimmi Gan <kimmi.gan@onfido.com>
2021-06-29 17:47:44 +0100

    Add video stream settings to SDK metadata

--- src/types/commons.ts
+++ src/types/commons.ts
@@ -57,6 +57,12 @@ export type SdkMetadata = {
     browser: string
     browser_version: string
   }
+  camera_settings?: {
+    aspect_ratio: number | undefined
+    frame_rate: number | undefined
+    height: number | undefined
+    width: number | undefined
+  }
 }
 
 export type CountryData = {
