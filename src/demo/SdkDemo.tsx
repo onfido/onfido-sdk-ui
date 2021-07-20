20a1634c66f019a5b5564ce68ce6f9b7c77e89d5
Skyler Robbins <skyler.robbins@onfido.com>
2021-07-20 12:01:30 -0600

    change sdk token url

--- src/demo/SdkDemo.tsx
+++ src/demo/SdkDemo.tsx
@@ -54,8 +54,8 @@ const SdkDemo: FunctionComponent<Props> = ({
     ).toUpperCase() as ServerRegions
     setRegionCode(builtRegionCode)
 
-    // const url = getTokenFactoryUrl(builtRegionCode)
-    const url = 'http://localhost:3000/sdk-token'
+    const url = getTokenFactoryUrl(builtRegionCode)
+    // const url = 'http://localhost:3000/sdk-token'
     setTokenUrl(url)
 
     getToken(
