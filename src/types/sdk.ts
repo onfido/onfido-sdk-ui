fa1564a22a593c4fccdb0c33ecd1c2f2972882c6
Kimmi Gan <kimmi.gan@onfido.com>
2021-09-02 12:06:01 +0100

    Remove nested option type

--- src/types/sdk.ts
+++ src/types/sdk.ts
@@ -37,11 +37,6 @@ export type UserExitCode = 'USER_CONSENT_DENIED'
 
 export type ServerRegions = 'US' | 'EU' | 'CA'
 
-export type CrossDeviceClientIntroOptions = {
-  productName?: string
-  ProductLogoSrc?: string
-}
-
 interface FunctionalConfigurations {
   disableAnalytics?: boolean
   mobileFlow?: boolean
@@ -73,7 +68,6 @@ export interface SdkOptions extends FunctionalConfigurations {
   steps?: Array<StepTypes | StepConfig>
   enterpriseFeatures?: EnterpriseFeatures
   customUI?: UICustomizationOptions | null
-  crossDeviceClientIntro?: CrossDeviceClientIntroOptions
   crossDeviceClientIntroProductName?: string
   crossDeviceClientIntroProductLogoSrc?: string
   _crossDeviceLinkMethods?: Array<string> | null
