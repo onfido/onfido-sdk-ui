f4eef4c78a1a03a0d5265af10cee1058aa2a89ed
Tony Findeisen <tony.findeisen@onfido.com>
2022-01-07 12:45:15 +0100

    serve local directory as part of the frontend as well

--- test/mock-server/middlewares.ts
+++ test/mock-server/middlewares.ts
@@ -1,4 +1,4 @@
-import { Middleware, send } from './deps.ts'
+import { Middleware, Router, send } from './deps.ts'
 
 export const loggerMiddleware: Middleware = async (context, next) => {
   const start = Date.now()
@@ -22,9 +22,15 @@ export const loggerMiddleware: Middleware = async (context, next) => {
 }
 
 export const frontendMiddleware: Middleware = async (context) => {
+  const pathname: string = context.request.url.pathname
+
+  const baseDir: string = pathname.match(/^\/local\//)
+    ? Deno.cwd()
+    : `${Deno.cwd()}/frontend`
+
   // Serve Preact app
-  await send(context, context.request.url.pathname, {
-    root: `${Deno.cwd()}/frontend`,
+  await send(context, pathname, {
+    root: baseDir,
     index: 'index.html',
   })
 }
