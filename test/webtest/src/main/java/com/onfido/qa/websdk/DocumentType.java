6ae080969ea07f1419079601b69dec2263030b91
Tony Findeisen <tony.findeisen@onfido.com>
2022-01-11 09:42:01 +0100

    added number of sides to DocumentType

--- test/webtest/src/main/java/com/onfido/qa/websdk/DocumentType.java
+++ test/webtest/src/main/java/com/onfido/qa/websdk/DocumentType.java
@@ -2,15 +2,25 @@ package com.onfido.qa.websdk;
 
 public enum DocumentType implements IDocumentType {
 
-    DRIVING_LICENCE("driving_licence"),
-    IDENTITY_CARD("national_identity_card"),
-    PASSPORT("passport"),
-    RESIDENT_PERMIT("residence_permit");
+    DRIVING_LICENCE("driving_licence", 2),
+    IDENTITY_CARD("national_identity_card", 2),
+    PASSPORT("passport", 1),
+    RESIDENT_PERMIT("residence_permit", 2);
 
     private final String canonicalName;
+    private final int numberOfSides;
 
-    DocumentType(String name) {
+    DocumentType(String name, int numberOfSides) {
         this.canonicalName = name;
+        this.numberOfSides = numberOfSides;
+    }
+
+    public boolean isSingleSideDocument() {
+        return numberOfSides == 1;
+    }
+
+    public boolean isDoubleSideDocument() {
+        return numberOfSides == 2;
     }
 
     @Override
