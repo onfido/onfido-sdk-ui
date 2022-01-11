b54bcb610307bea118101e46096dd55196c91ce4
Tony Findeisen <tony.findeisen@onfido.com>
2022-01-11 15:54:34 +0100

    added more page objects for tests

--- test/webtest/src/main/java/com/onfido/qa/websdk/page/DocumentUpload.java
+++ test/webtest/src/main/java/com/onfido/qa/websdk/page/DocumentUpload.java
@@ -1,17 +1,11 @@
 package com.onfido.qa.websdk.page;
 
 import com.onfido.qa.webdriver.Driver;
-import com.onfido.qa.websdk.UploadDocument;
-import com.onfido.qa.websdk.util.Javascript;
 import org.openqa.selenium.By;
 
-import java.io.File;
-import java.io.IOException;
-
-public class DocumentUpload extends BasePage {
+public class DocumentUpload extends DocumentUploadBasePage {
 
     public static final By ICON = By.cssSelector(".onfido-sdk-ui-Uploader-icon");
-    public static final By FILE_INPUT = By.cssSelector(".onfido-sdk-ui-CustomFileInput-input");
 
     public DocumentUpload(Driver driver) {
         super(driver);
@@ -23,26 +17,4 @@ public class DocumentUpload extends BasePage {
         return ICON;
     }
 
-    public ConfirmUpload upload(UploadDocument document) {
-
-        String path = path(document);
-
-        var input = driver.findElement(FILE_INPUT);
-
-        new Javascript(driver).changeDisplayStyle(input, "block");
-        input.sendKeys(path);
-
-        return new ConfirmUpload(driver);
-    }
-
-    private String path(UploadDocument document) {
-        var directory = new File("../resources/");
-
-        var file = new File(directory, document.filename);
-        try {
-            return file.getCanonicalPath();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
