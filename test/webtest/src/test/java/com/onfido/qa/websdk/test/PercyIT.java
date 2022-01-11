45a16d359eaaa8d5cf7d96f3e4288cdec5f5ce89
Tony Findeisen <tony.findeisen@onfido.com>
2022-01-11 09:43:16 +0100

    moved percy a level up

--- test/webtest/src/test/java/com/onfido/qa/websdk/test/PercyIT.java
+++ test/webtest/src/test/java/com/onfido/qa/websdk/test/PercyIT.java
@@ -4,19 +4,13 @@ import com.onfido.qa.websdk.PoADocumentType;
 import com.onfido.qa.websdk.page.Complete;
 import com.onfido.qa.websdk.page.PoAIntro;
 import com.onfido.qa.websdk.page.Welcome;
-import io.percy.selenium.Percy;
 import org.testng.SkipException;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
 
 public class PercyIT extends WebSdkIT {
-
-    public static final int MIN = 500;
-    public static final int SLEEP_BEFORE_SNAPSHOT = 250;
-    private Percy percy;
 
     public PercyIT() {
     }
@@ -31,42 +25,27 @@ public class PercyIT extends WebSdkIT {
         return Arrays.stream(PoADocumentType.values()).map(x-> {return new Object[]{x};}).toArray(Object[][]::new);
     }
 
-    @BeforeMethod
-    public void beforeTest() {
-        percy = new Percy(driver());
-    }
-
-    private void snapshot(String name) {
-        try {
-            Thread.sleep(SLEEP_BEFORE_SNAPSHOT);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        percy.snapshot(String.format("%s-%s", name, language), Arrays.asList(MIN));
-    }
-
-    @Test
+    @Test(groups = {"percy"})
     public void welcome() {
         var welcome = onfido().withSteps("welcome", "complete").init(Welcome.class);
-        snapshot("Welcome");
+        takePercySnapshot("Welcome");
 
         welcome.continueToNextStep(Complete.class);
-        snapshot("Complete");
+        takePercySnapshot("Complete");
 
     }
 
-    @Test
+    @Test(groups = {"percy"})
     public void poa() {
         var intro = onfido().withSteps("poa").init(PoAIntro.class);
-        snapshot("PoAIntro");
+        takePercySnapshot("PoAIntro");
 
         intro.startVerification();
-        snapshot("PoADocumentSelection");
+        takePercySnapshot("PoADocumentSelection");
 
     }
 
-    @Test(dataProvider = "poaDocumentTypes")
+    @Test(dataProvider = "poaDocumentTypes", groups = {"percy"})
     public void poaGuidance(PoADocumentType documentType) {
 
         // TODO: initialize with correct country
@@ -78,6 +57,7 @@ public class PercyIT extends WebSdkIT {
                 .startVerification()
                 .select(documentType);
 
-        snapshot("PoAGuidance-" + documentType.canonicalName());
+        takePercySnapshot("PoAGuidance-" + documentType.canonicalName());
     }
+
 }
