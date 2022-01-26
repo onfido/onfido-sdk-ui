d856b7ec709255476c018292a31ad55043cc4fac
Tony Findeisen <tony.findeisen@onfido.com>
2022-01-26 18:39:11 +0100

    migrated hostAppHistory

--- test/specs/chrome.js
+++ test/specs/chrome.js
@@ -1,8 +1,5 @@
 import { describe } from '../utils/mochaw'
-import { hostAppHistoryScenarios } from './scenarios/hostAppHistory'
 
 describe('Happy Paths on Chrome', () => {
   // Multiple language scenarios
-
-  hostAppHistoryScenarios()
 })
