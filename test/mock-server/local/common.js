a9fb9eecf763e234519fce28f9c3621aadddca75
Tony Findeisen <tony.findeisen@onfido.com>
2022-02-04 10:07:30 +0100

    move getToken method above, as the URLSearchParams is not defined for IE11

--- test/mock-server/local/common.js
+++ test/mock-server/local/common.js
@@ -1,17 +1,5 @@
 (function(window) {
-  var parameters = new URLSearchParams(location.search);
-  const name = 'link_id';
-
-  if (parameters.has(name)) {
-    window.onfido = Onfido.init({
-      mobileFlow: true,
-      containerId: 'root',
-      roomId: parameters.get(name).replace(/^CV/, '')
-    });
-  }
-
   window.getToken = function() {
-
     try {
       var xhr = new XMLHttpRequest();
       xhr.open('GET', '/token-factory/sdk_token', false);
@@ -22,5 +10,17 @@
       return "error";
     }
   }
+
+  var parameters = new URLSearchParams(location.search);
+  var name = 'link_id';
+
+  if (parameters.has(name)) {
+    window.onfido = Onfido.init({
+      mobileFlow: true,
+      roomId: parameters.get(name).replace(/^CV/, '')
+    });
+  }
+
+
 })(window);
 
