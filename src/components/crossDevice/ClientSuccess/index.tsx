9a77e17ce0053fecc4c4b01c2558ff4903e11c04
Jérôme Viveret <jerome.viveret@onfido.com>
2022-03-01 16:30:28 +0100

    Fix type check

--- src/components/crossDevice/ClientSuccess/index.tsx
+++ src/components/crossDevice/ClientSuccess/index.tsx
@@ -1,41 +1,35 @@
-import { h, Component } from 'preact'
+import { h, FunctionComponent } from 'preact'
+import { useEffect } from 'preact/hooks'
 import classNames from 'classnames'
 import PageTitle from '../../PageTitle'
 import { trackComponent } from '../../../Tracker'
 import theme from '../../Theme/style.scss'
 import style from './style.scss'
 import { useLocales } from '~locales'
-import type { StepComponentProps } from '~types/routers'
+import { StepComponentProps } from '~types/routers'
 
 export type Props = {
   sendClientSuccess: () => void
 } & StepComponentProps
 
-class ClientSuccess extends Component<Props> {
-  componentDidMount() {
-    this.props.sendClientSuccess()
-  }
+const ClientSuccess: FunctionComponent<Props> = ({ sendClientSuccess }) => {
+  useEffect(sendClientSuccess)
 
-  render() {
-    const { translate } = useLocales();
-    return (
-      <div data-page-id={'CrossDeviceClientSuccess'}>
-        <PageTitle
-          title={translate('cross_device_return.title')}
-          subTitle={translate('cross_device_return.subtitle')}
-        />
-        <div className={theme.iconContainer}>
-          <span className={classNames(theme.icon, style.icon)} />
-          <div className={style.text}>
-            {translate('cross_device_return.body')}
-          </div>
+  const { translate } = useLocales()
+  return (
+    <div data-page-id={'CrossDeviceClientSuccess'}>
+      <PageTitle
+        title={translate('cross_device_return.title')}
+        subTitle={translate('cross_device_return.subtitle')}
+      />
+      <div className={theme.iconContainer}>
+        <span className={classNames(theme.icon, style.icon)} />
+        <div className={style.text}>
+          {translate('cross_device_return.body')}
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
-export default trackComponent(
-  ClientSuccess,
-  'crossdevice_mobile_success'
-)
+export default trackComponent(ClientSuccess, 'crossdevice_mobile_success')
