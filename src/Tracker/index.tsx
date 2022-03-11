d0870893e0ac0fba49cdd78d5f6d19ba90ac28bd
Danny van der Jagt <dannyvanderjagt@gmail.com>
2022-03-11 14:46:22 +0100

    Add analytics cookie to track devices

--- src/Tracker/index.tsx
+++ src/Tracker/index.tsx
@@ -6,6 +6,8 @@ import { map as mapObject } from '~utils/object'
 import { isOnfidoHostname } from '~utils/string'
 import { sendAnalyticsEvent } from './onfidoTracker'
 import { integratorTrackedEvents } from './trackerData'
+import { v4 as uuidv4 } from 'uuid'
+const Cookies = require('js-cookie')
 
 import type { TrackScreenCallback, WithTrackingProps } from '~types/hocs'
 import type {
@@ -237,6 +239,56 @@ const setWoopraCookie = (cookie: string): void => {
 
 const getWoopraCookie = (): Optional<string> => (woopra ? woopra.cookie : null)
 
+const cookieAttributes = {
+  name: 'onfido-web-sdk-analytics',
+  domain: location.hostname,
+  path: '/',
+  expires: 30,
+  secure: true,
+}
+
+// Internal Analytics Cookie
+type setAnonymousUuidFunc = (payload?: string) => void
+const setupAnalyticsCookie = (
+  setAnonymousUuid: setAnonymousUuidFunc,
+  anonymousUuid?: string
+) => {
+  const cookie = getAnalyticsCookie()
+
+  if (!anonymousUuid && !cookie) {
+    const uuid = uuidv4()
+    setAnonymousUuid(uuid)
+    setAnalyticsCookie(uuid)
+    return
+  }
+
+  if (!anonymousUuid && cookie) {
+    setAnonymousUuid(cookie)
+    return
+  }
+
+  if (anonymousUuid && anonymousUuid !== cookie) {
+    setAnalyticsCookie(anonymousUuid)
+  }
+}
+
+const setAnalyticsCookie = (anonymousUuid: string) => {
+  Cookies.set(cookieAttributes.name, anonymousUuid, cookieAttributes)
+}
+
+const uninstallAnalyticsCookie = (setAnonymousUuid: setAnonymousUuidFunc) => {
+  setAnonymousUuid(undefined)
+  Cookies.remove(cookieAttributes.name, cookieAttributes)
+}
+
+const getAnalyticsCookie = () => {
+  const cookie = Cookies.get(cookieAttributes.name)
+  if (cookie === 'undefined') {
+    return undefined
+  }
+  return cookie
+}
+
 export {
   setUp,
   install,
@@ -250,4 +302,7 @@ export {
   appendToTracking,
   setWoopraCookie,
   getWoopraCookie,
+  setupAnalyticsCookie,
+  uninstallAnalyticsCookie,
+  getAnalyticsCookie,
 }
