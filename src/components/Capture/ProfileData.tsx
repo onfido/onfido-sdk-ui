321ac21de77aacbcf7c7265c4a18a7959c682294
Julius Osokinas <20243687+josokinas@users.noreply.github.com>
2022-04-22 15:32:55 +0100

    feat: add new date of birth input (#1818)

--- src/components/Capture/ProfileData.tsx
+++ src/components/Capture/ProfileData.tsx
@@ -1,5 +1,10 @@
-import { h, Component } from 'preact'
-import { useState } from 'preact/compat'
+import { h } from 'preact'
+import {
+  createContext,
+  useContext,
+  useState,
+  StateUpdater,
+} from 'preact/compat'
 import { useLocales } from '~locales'
 import { color } from '@onfido/castor'
 import { IconError } from '@onfido/castor-icons'
@@ -14,13 +19,25 @@ import {
   Input,
   Button,
   Asterisk,
-  InputProps,
 } from '@onfido/castor-react'
 import type { WithLocalisedProps } from '~types/hocs'
 import CountrySelector from './CountrySelector'
+import { DateOfBirthInput } from './DateOfBirthInput'
 
 import type { StepComponentDataProps, CompleteStepValue } from '~types/routers'
 import { StepOptionData } from '~types/steps'
+
+const validationContext = createContext({} as ValidationState)
+const ValidationProvider = validationContext.Provider
+
+export const useValidation = () => useContext(validationContext)
+
+type Validation = { [key: string]: boolean }
+
+interface ValidationState {
+  validation: Validation
+  setValidation: StateUpdater<Validation>
+}
 
 type ProfileDataProps = StepComponentDataProps & {
   title: string
@@ -38,7 +55,8 @@ const ProfileData = ({
   completeStep,
 }: ProfileDataProps) => {
   const [submitData, setSubmitData] = useState(data)
-  const [validation, setValidation] = useState(
+  const [formSubmitted, setFormSubmitted] = useState<boolean>(false)
+  const [validation, setValidation] = useState<Validation>(
     Object.fromEntries(Object.entries(data).map(([key]) => [key, false]))
   )
   const { translate } = useLocales()
@@ -67,6 +85,8 @@ const ProfileData = ({
   }
 
   const handleSubmit = () => {
+    setFormSubmitted(true)
+
     if (validate()) {
       if (dataPath) completeStep({ [dataPath]: submitData })
       else completeStep(submitData)
@@ -87,52 +107,53 @@ const ProfileData = ({
   return (
     <ScreenLayout actions={actions}>
       <PageTitle title={translate(`profile_data.${title}`) || title} />
-      {Object.entries(submitData).map(([key, value]) => {
-        const isRequired = !['state'].includes(key)
+      <ValidationProvider value={{ validation, setValidation }}>
+        {Object.entries(submitData).map(([key, value]) => {
+          const isRequired = !['state'].includes(key)
 
-        return (
-          <Field key={key}>
-            <FieldLabel>
-              <span>
-                {translate(`profile_data.${key}`) || key}
-                {isRequired && <Asterisk aria-label="required" />}
-              </span>
-              {key === 'country' ? (
-                <CountrySelector
-                  value={`${value}`}
-                  error={validation[key]}
-                  onChange={(value: string) => handleChange(key, value)}
-                />
-              ) : (
-                <Input
-                  type={getType(key) as InputProps['type']}
-                  value={`${value}`}
-                  invalid={validation[key]}
-                  required={isRequired}
-                  onChange={(e) => handleChange(key, e.target.value)}
-                />
-              )}
-              {validation[key] && (
-                <Validation state="error">
-                  <IconError fill={color('error-500')} />
-                  {translate('profile_data.required_error')}
-                </Validation>
-              )}
-            </FieldLabel>
-          </Field>
-        )
-      })}
+          return (
+            <Field key={key}>
+              <FieldLabel>
+                <span>
+                  {translate(`profile_data.${key}`) || key}
+                  {isRequired && <Asterisk aria-label="required" />}
+                </span>
+                {key === 'country' ? (
+                  <CountrySelector
+                    value={`${value}`}
+                    error={formSubmitted && validation[key]}
+                    onChange={(value: string) => handleChange(key, value)}
+                  />
+                ) : key === 'dob' ? (
+                  <DateOfBirthInput
+                    fieldKey={key}
+                    name={key}
+                    value={`${value}`}
+                    invalid={formSubmitted && validation[key]}
+                    onChange={(e) => handleChange(key, e.target.value)}
+                  />
+                ) : (
+                  <Input
+                    type="text"
+                    value={`${value}`}
+                    invalid={formSubmitted && validation[key]}
+                    required={isRequired}
+                    onChange={(e) => handleChange(key, e.target.value)}
+                  />
+                )}
+                {formSubmitted && validation[key] && (
+                  <Validation state="error">
+                    <IconError fill={color('error-500')} />
+                    {translate('profile_data.required_error')}
+                  </Validation>
+                )}
+              </FieldLabel>
+            </Field>
+          )
+        })}
+      </ValidationProvider>
     </ScreenLayout>
   )
-}
-
-const getType = (key: string) => {
-  switch (key) {
-    case 'dob':
-      return 'date'
-    default:
-      return 'text'
-  }
 }
 
 export default ProfileData
