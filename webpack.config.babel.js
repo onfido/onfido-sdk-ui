bbe357eb4a860be79abb56cdd2574afa7887f8dc
Danny van der Jagt <dannyvanderjagt@gmail.com>
2022-04-29 11:32:59 +0200

    Fix base32 hashes to prepare for future release

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -222,7 +222,7 @@ const basePlugins = (bundle_name = '') => [
       // ref: https://en.wikipedia.org/wiki/Base32
       // NOTE: please leave the BASE_32_VERSION be! It is updated automatically by
       // the release script 🤖
-      BASE_32_VERSION: 'DH',
+      BASE_32_VERSION: 'DI',
       PRIVACY_FEATURE_ENABLED: false,
       JWT_FACTORY: CONFIG.JWT_FACTORY,
       US_JWT_FACTORY: CONFIG.US_JWT_FACTORY,
