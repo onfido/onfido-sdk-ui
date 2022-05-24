6f4a073d4028c6f085ae84c23eb5a2242035d44f
Nicolas Ulrich <nicolas.ulrich@onfido.com>
2022-05-24 18:00:58 +0200

    Update i18n keys. Enable RestrictedDocument Step

--- src/components/Router/StepComponentMap.tsx
+++ src/components/Router/StepComponentMap.tsx
@@ -50,9 +50,8 @@ import type {
 import PoAClientIntro from '../ProofOfAddress/PoAIntro'
 import Guidance from '../ProofOfAddress/Guidance'
 import PoADocumentSelector from '../DocumentSelector/PoADocumentSelector'
-import { IdentityDocumentSelector } from '../DocumentSelector/IdentityDocumentSelector'
-import IdentityCountrySelector from '../CountrySelector/IdentityCountrySelector'
 import PoACountrySelector from '../CountrySelector/PoACountrySelector'
+import { RestrictedDocumentSelection } from '../RestrictedDocumentSelection'
 
 let LazyAuth: ComponentType<StepComponentProps>
 
@@ -294,18 +293,14 @@ const buildRequiredSelfieComponents = (
 }
 
 const buildNonPassportPreCaptureComponents = (
-  hasOnePreselectedDocument: boolean,
-  showCountrySelection: boolean
+  hasOnePreselectedDocument: boolean
 ): ComponentType<StepComponentProps>[] => {
   const prependDocumentSelector = hasOnePreselectedDocument
     ? []
-    : [IdentityDocumentSelector]
-  const prependCountrySelector = showCountrySelection
-    ? [IdentityCountrySelector]
-    : []
+    : [RestrictedDocumentSelection]
   // @ts-ignore
   // TODO: convert DocumentSelector to TS
-  return [...prependDocumentSelector, ...prependCountrySelector]
+  return [...prependDocumentSelector]
 }
 
 const buildDocumentComponents = (
@@ -316,17 +311,6 @@ const buildDocumentComponents = (
   mobileFlow: boolean | undefined,
   isFirstCaptureStepInFlow: boolean | undefined
 ): ComponentType<StepComponentProps>[] => {
-  const options = documentStep?.options
-
-  // DEPRECATED: documentStep.options.showCountrySelection will be deprecated in a future release
-  const showCountrySelectionForSinglePreselectedDocument =
-    options?.showCountrySelection
-
-  const configForDocumentType =
-    documentType && options?.documentTypes
-      ? options?.documentTypes[documentType]
-      : undefined
-
   const shouldUseVideo =
     documentStep?.options?.requestedVariant === 'video' &&
     window.MediaRecorder != null
@@ -344,7 +328,7 @@ const buildDocumentComponents = (
   if (isPassportDocument) {
     const preCaptureComponents = hasOnePreselectedDocument
       ? []
-      : [IdentityDocumentSelector]
+      : [RestrictedDocumentSelection]
 
     if (shouldUseVideo) {
       // @ts-ignore
@@ -369,26 +353,8 @@ const buildDocumentComponents = (
       : [...preCaptureComponents, ...standardCaptureComponents]
   }
 
-  const countryCode =
-    typeof configForDocumentType === 'boolean'
-      ? null
-      : configForDocumentType?.country
-  const supportedCountry = getCountryDataForDocumentType(
-    countryCode,
-    documentType
-  )
-  const hasMultipleDocumentsWithUnsupportedCountry =
-    !hasOnePreselectedDocument && !supportedCountry
-  const hasCountryCodeOrDocumentTypeFlag =
-    countryCode !== null || configForDocumentType === true
-  const showCountrySelection =
-    showCountrySelectionForSinglePreselectedDocument ||
-    (hasMultipleDocumentsWithUnsupportedCountry &&
-      hasCountryCodeOrDocumentTypeFlag)
-
   const preCaptureComponents = buildNonPassportPreCaptureComponents(
-    hasOnePreselectedDocument,
-    showCountrySelection
+    hasOnePreselectedDocument
   )
 
   if (shouldUseVideo) {
