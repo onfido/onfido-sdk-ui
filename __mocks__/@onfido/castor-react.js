8faddf2ae37868e9fa4065ab5a592a4d35dc016c
Danny van der Jagt <dannyvanderjagt@gmail.com>
2022-05-24 11:43:15 +0200

    remove mocks

--- __mocks__/@onfido/castor-react.js
+++
@@ -1,3 +0,0 @@
-export const button = () => {
-  return <div>Mock Button</div>
-}
--- __mocks__/blueimp-load-image.ts
+++
@@ -1,7 +0,0 @@
-const loadImage = jest
-  .fn()
-  .mockImplementation((_blob, callback) =>
-    callback(document.createElement('canvas'))
-  )
-
-export default loadImage
