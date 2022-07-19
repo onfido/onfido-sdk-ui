09ca613560b9537012d24630944d189000171c43
Andre Maia <andre.maia@onfido.com>
2022-07-19 16:26:04 +0100

    fix(orc-1166/1137): fix mobile layout for profile capture data and add SSN input validations
    add ssn max length and pattern

--- src/components/Capture/ProfileData.tsx
+++ src/components/Capture/ProfileData.tsx
@@ -236,7 +236,7 @@ const FieldComponent = ({
   }
 
   return (
-    <Field>
+    <Field className={style['field']}>
       <FieldLabel>
         <span>
           {getTranslatedFieldLabel(translate, type, selectedCountry)}
@@ -283,6 +283,17 @@ const getFieldComponent = (
       return <DateOfBirthInput {...props} />
     case 'postcode':
       return <Input {...props} type="text" style={{ width: space(22) }} />
+    case 'ssn':
+      return (
+        <Input
+          {...props}
+          placeholder={'999-99-9999'}
+          pattern={'d{3}-d{2}-d{4}'}
+          maxLength={11}
+          type="text"
+          style={{ width: space(22) }}
+        />
+      )
     default:
       return <Input {...props} type="text" />
   }
