4fbe119a3e73f125785bcb55beecff2e84f9993b
Warren Seine <warren.seine@onfido.com>
2022-08-08 23:54:22 +0200

    🏗️ Fix Motion camera permission integration
    In the current integration, when no camera is available, a red alert appears with a "Try using your phone instead" link. This works, but differs from other product flows. The camera detection should immediately fall back to cross-device.
    
    Similarly, when a camera is available but permissions were denied, the appropriate error screen would not appear. The error was silently ignored, leaving the SDK in a dangling state.
    
    The solution is to make use of the existing HOC helper `withFailureHandling` instead of re-implementing the error handling in the component, and to bubble the `react-webcam`-emitted permission errors instead of hiding them in a `LivenessError` enumeration.

--- src/components/ActiveVideo/Lazy.tsx
+++ src/components/ActiveVideo/Lazy.tsx
@@ -3,15 +3,31 @@ import { localised } from '~locales'
 import { asyncComponent } from '~utils/components'
 import Spinner from 'components/Spinner'
 
-import type { StepComponentProps } from '~types/routers'
+import type { RenderFallbackProp, StepComponentProps } from '~types/routers'
+import FallbackButton from 'components/Button/FallbackButton'
 
 const ActiveVideo = asyncComponent(
   () => import(/* webpackChunkName: "activeVideo" */ './ActiveVideo'),
   Spinner
 )
 
-const LazyActiveVideo = (props: StepComponentProps) => (
-  <ActiveVideo {...props} />
-)
+const LazyActiveVideo = (props: StepComponentProps) => {
+  const { changeFlowTo } = props
+  const renderCrossDeviceFallback: RenderFallbackProp = ({ text }) => {
+    return (
+      <FallbackButton
+        text={text}
+        onClick={() => changeFlowTo('crossDeviceSteps')}
+      />
+    )
+  }
+
+  return (
+    <ActiveVideo
+      renderCrossDeviceFallback={renderCrossDeviceFallback}
+      {...props}
+    />
+  )
+}
 
 export default localised(LazyActiveVideo)
