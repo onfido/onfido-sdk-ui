2397e5ec26bd8a389d9e8eec71c262e9d2ad229e
Danny van der Jagt <dannyvanderjagt@gmail.com>
2022-08-16 17:33:28 +0200

    Update integration test for empty document due to api change

--- src/components/utils/__integrations__/onfidoApi/document.integration.js
+++ src/components/utils/__integrations__/onfidoApi/document.integration.js
@@ -97,7 +97,7 @@ describe('API uploadDocument endpoint', () => {
       try {
         expect(error.status).toBe(422)
         expect(error.response.error.type).toBe('validation_error')
-        expect(error.response.error.fields).toHaveProperty('file')
+        expect(error.response.error.fields).toHaveProperty('content_type')
         done()
       } catch (err) {
         done(err)
